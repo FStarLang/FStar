dd99847f43f9f9cb4e3582801034ac486dbfbf20
Pierre-Yves Strub <pierre-yves@strub.nu>
2014-04-27 10:28:35 +0200

    Cache width in formatter

--- src/format/format.fs
+++ src/format/format.fs
@@ -2,7 +2,9 @@
 module FSharp.OCaml.Format
 
 (* -------------------------------------------------------------------- *)
-type doc =
+type doc = { node: node; width: int; }
+
+and node =
 | Text   of string
 | Concat of doc * doc
 | Group  of doc
@@ -11,37 +13,31 @@ type doc =
 type width = int
 
 let (&.) (s : string) : doc =
-    Text s
+    { node = Text s; width = String.length s; }
 
 let (+.) (d1 : doc) (d2 : doc) : doc =
-    Concat (d1, d2)
+    { node  = Concat (d1, d2);
+      width = d1.width + d2.width; }
 
 let endl : doc =
-    Line
+    { node = Line; width = 1; }
 
 let group (d : doc) =
-    Group d
-
-(* -------------------------------------------------------------------- *)
-let rec width = function
-    | Text   x        -> String.length x
-    | Line            -> 1
-    | Concat (d1, d2) -> width d1 + width d2
-    | Group  d        -> width d
+    { node = Group d; width = d.width; }
 
 (* -------------------------------------------------------------------- *)
 let tostring (w : width) (doc : doc) =
-    let rec format (fit, wleft) = function
-    | Text s ->
-        (s, wleft - String.length s)
-    | Line ->
-        if fit then (" " , wleft-1) else ("\r\n", w)
-    | Group d ->
-        format (fit || width d <= wleft, wleft) d
-    | Concat (d1, d2) ->
-        let (s1, wleft1) = format (fit, wleft ) d1
-        let (s2, wleft2) = format (fit, wleft1) d2
-        (s1 + s2, wleft2)
+    let rec format (fit, wleft) doc =
+        match doc.node with
+        | Text s ->
+            (s, wleft - String.length s)
+        | Line ->
+            if fit then (" " , wleft-1) else ("\r\n", w)
+        | Group d ->
+            format (fit || d.width <= wleft, wleft) d
+        | Concat (d1, d2) ->
+            let (s1, wleft1) = format (fit, wleft ) d1
+            let (s2, wleft2) = format (fit, wleft1) d2
+            (s1 + s2, wleft2)
 
     fst (format (false, w) doc)
-
