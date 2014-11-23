b6b3cc0d9699f90154934c7d923c59f4e2da1444
Antoine Delignat-Lavaud <antoine@delignat-lavaud.fr>
2014-11-23 18:00:44 +0100

    Tune the OCaml GC

--- src/fstarml.ml
+++ src/fstarml.ml
@@ -108,6 +108,7 @@
       
     let x =
       Printexc.record_backtrace true;
+      Gc.set { (Gc.get()) with Gc.minor_heap_size = 1048576; Gc.major_heap_increment = 4194304; Gc.space_overhead = 150; };
       Fstar.Support.Prims.try_with
         (fun _20069 ->
            match _20069 with
