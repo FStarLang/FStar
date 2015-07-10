a18f14d6c479351c6449f72eb96199f915d2e6d5
Abhishek Anand (@ MSR desktop) <t-aban@microsoft.com>
2015-07-10 12:54:46 -0700

    towards testing extraction of inductives. Defined the undefined parts getting in the way of testing.

--- src/backend/ocaml-asttrans2.fs
+++ src/backend/ocaml-asttrans2.fs
@@ -29,11 +29,17 @@ let unknownType : mlty = MLTY_Var  ("Obj.t", 0)
 let convRange (r:Range.range) : int = 0 (*FIX!!*)
 let convIdent (id:ident) : mlident = (id.idText,(convRange id.idRange))
     
-(*assuming that btvar includes the name of the variable*)
-type context = env
-(*is there an F# library of associative lists?*)
-let contains (c:context) (b:btvar) = failwith "contains is undefined"
+(* This is all the context is needed for extracting F* types to OCaml.
+   In particular, the definition of a type constant is not needed when translating references to it.
+   However, enough info is needed to determine whether a type constant is a type scheme. add that.
+*)
+type context = {
+    tyVars : list<ident>;
+    tyConstants : list<lident>
+}
 
+(*is there an F# library of associative lists?*)
+let contains (c:context) (b:btvar) = List.contains b.v.realname (*why not ppname?*) c.tyVars
 
 let deltaUnfold (i : lident) (c: context) : (option<typ>) = None (*FIX!!*)
 
@@ -43,7 +49,7 @@ let deltaUnfold (i : lident) (c: context) : (option<typ>) = None (*FIX!!*)
       *)
 let isTypeScheme (i : lident) (c: context) : bool = true  (* FIX!! *)
 
-let liftType' (t:typ') : typ = failwith "liftType is undefined"
+(* let liftType' (t:typ') : typ = failwith "liftType is undefined" *)
 
 let lident2mlpath (l:lident) : mlpath =
   (List.map (fun x -> x.idText) l.ns, l.ident.idText)
@@ -51,11 +57,21 @@ let lident2mlpath (l:lident) : mlpath =
 let rec extractType' (c:context) (ft:typ') : mlty = 
 (* first \beta, \iota and \zeta reduces ft. Since F* does not have SN, one has to be more careful for the termination argument.
     Why not \delta? I guess the reason is that unfolding definitions will make the resultant OCaml code less readable.
-    However in the Typ_app case,  \delta reduction is done as the second-last resort, just before giving up and returing unknownType
+    However in the Typ_app case,  \delta reduction is done as the second-last resort, just before giving up and returing unknownType;
+        a bloated type is atleast as good as unknownType?
 *)
 match ft with
-  | Typ_btvar _ -> extractType' c (Typ_app (liftType' ft, []))
-  | Typ_const _ -> extractType' c (Typ_app (liftType' ft, []))
+(*The next 2 cases duplicate a lot of code in the Type_app case. It will nice to share the common computations.*)
+  | Typ_btvar btv -> (if (contains c btv) then MLTY_Var (convIdent btv.v.realname) else unknownType)
+  | Typ_const ftv -> 
+            (match  (isTypeScheme ftv.v c)  with
+             | true -> MLTY_Named ([ (* FIX!! *)],(lident2mlpath ftv.v))
+             | false -> 
+                 (match  (deltaUnfold ftv.v c) with
+                 | Some tyu ->  extractTyp c tyu
+                 | None -> unknownType
+                 )
+            )
   | Typ_fun (bs, codomain) -> 
         (let codomainML = (extractComp  c codomain) in 
         if  (codomainML = erasedContent) 
@@ -112,10 +128,8 @@ match bn with
 | (Inr bvv,_) -> bvv.v.realname
 
 (* these vars will have type Type, irrespective of that types they had in F*. This is to match the limitations of  OCaml*)
-let extendContextWithTyVars (c:context) (idents : list<ident>) : context = failwith "extendContextWithTyVars is not implemented"
-
-let lookupSigelt (c:context) (ctorname: lident) : sigelt 
-    = failwith "lookupSigelt not implemented"
+let extendContext (c:context) (tyVars : list<ident>) (tyConsts : list<lident>) : context = 
+    { tyVars = List.append c.tyVars tyVars; tyConstants = List.append c.tyConstants tyConsts}
 
 let mlsymbolOfLident (id : lident) : mlsymbol =
   id.ident.idText
@@ -123,12 +137,12 @@ let mlsymbolOfLident (id : lident) : mlsymbol =
 
 (*just enough info to generate OCaml code; add more info as needed*)
 type inductiveConstructor = {
-  cname: string;
+  cname: lident;
   ctype : typ;
   cargs : binders (*will this list always be empty? it has always been empty: tested nat, list, vec*)
 }
 type inductiveTypeFam = {
-  tyName: string;
+  tyName: lident;
   k : knd;
   tyBinders: binders;
   constructors : list<inductiveConstructor>
@@ -138,7 +152,7 @@ type inductiveTypeFam = {
 let parseInductiveConstructor (sigs : list<sigelt>) : (inductiveConstructor * list<sigelt>) =
 match sigs with
 | (Sig_datacon (l, (*codomain*) t ,(_,cargs:binders,_),_,_,_))::tlsig ->
-     ({ cname = l.ident.idText ; ctype = t; cargs =[] }, tlsig)
+     ({ cname = l ; ctype = t; cargs =[] }, tlsig)
 | _ -> failwith "incorrect sigelt provided to parseInductiveConstructor"
 
 let rec parseInductiveConstructors (sigs : list<sigelt>) (n: int) : (list<inductiveConstructor> * list<sigelt>) =
@@ -156,14 +170,14 @@ let parseInductiveTypeFromSigBundle
 match sigs with
 | (Sig_tycon (l,bs,kk,_,constrs,_,_))::tlsig -> 
     let (indConstrs:list<inductiveConstructor>, ttlsig) = parseInductiveConstructors tlsig (List.length constrs) in
-     ({tyName = l.ident.idText; k = kk; tyBinders=bs; constructors=indConstrs}, ttlsig)
-| _ -> failwith "incorrect sif bundle provided to parseInductiveTypeFromSigBundle"
+     ({tyName = l; k = kk; tyBinders=bs; constructors=indConstrs}, ttlsig)
+| _ -> failwith "incorrect input provided to parseInductiveTypeFromSigBundle"
 
 let parseFirstInductiveType
     (s : sigelt)  : inductiveTypeFam  =
 match s with
 | Sig_bundle (sigs, _, _, _) -> fst (parseInductiveTypeFromSigBundle sigs)
-| _ -> failwith "incorrect sif bundle provided to parseInductiveTypeFromSigBundle"
+| _ -> failwith "incorrect input provided to parseInductiveTypeFromSigBundle"
 
 
 let rec argTypes  (t: mlty) : list<mlty> =
@@ -171,29 +185,32 @@ match t with
 | MLTY_Fun (a,b) -> a::(argTypes b)
 | _ -> []
  
+let lident2mlsymbol (l:lident) : mlsymbol = l.ident.idText
 
 let extractCtor (c:context) (ctor: inductiveConstructor):  (mlsymbol * list<mlty>) =
    if (0< List.length ctor.cargs)
    then (failwith "cargs is unexpectedly non-empty. This is a design-flaw, please report.")
    else 
         (let mlt = extractTyp c ctor.ctype in
-        (ctor.cname, argTypes mlt))
+        (lident2mlsymbol ctor.cname, argTypes mlt))
  
 (*similar to the definition of the second part of \hat{\epsilon} in page 110*)
-let extractSigElt (c:context) (s:sigelt) : option<mlsig1> =
+(* \pi_1 of returned value is the exported constant*)
+let extractSigElt (c:context) (s:sigelt) :  option<lident * mlsig1> =
 match s with
 | Sig_typ_abbrev (l,bs,_,t,_,_) -> 
     let idents = List.map binderIdent bs in
-    let newContext = (extendContextWithTyVars c idents) in
+    let newContext = (extendContext c idents [l] ) in
     let tyDecBody = MLTD_Abbrev (extractTyp newContext t) in
-     Some (MLS_Ty [(mlsymbolOfLident l, List.map convIdent idents , Some tyDecBody)])
+     Some (l, MLS_Ty [(mlsymbolOfLident l, List.map convIdent idents , Some tyDecBody)])
      (*type l idents = tyDecBody*)
+
 | Sig_bundle _ -> 
     let ind = parseFirstInductiveType s in
     let idents = List.map binderIdent ind.tyBinders in
-    let newContext = (extendContextWithTyVars c idents) in
+    let newContext = (extendContext c idents [ind.tyName]) in
     let tyDecBody = MLTD_DType (List.map (extractCtor newContext) ind.constructors) in
-          Some (MLS_Ty [(ind.tyName, List.map convIdent idents , Some tyDecBody)])
+          Some (ind.tyName, MLS_Ty [(lident2mlsymbol ind.tyName, List.map convIdent idents , Some tyDecBody)])
      (*type l idents = tyDecBody*)
 | _ -> None
 
