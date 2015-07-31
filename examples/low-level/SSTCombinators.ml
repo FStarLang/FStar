1c131935f52ad5daad01ff7dfa796b0da17d0a37
Abhishek Anand (@ MSR desktop) <t-aban@microsoft.com>
2015-07-31 13:40:12 -0700

    added the extract of SST and factorial. There are some unnececcary magics in Factorial.ml . Many magics are necessary, because of how the non-OCaml inductive type aref gets extracted. Up Next : will try to provide a simple implementation of undefined functions in SST.ml, and try to run the factorial program.

--- examples/low-level/SSTCombinators.ml
+++ examples/low-level/SSTCombinators.ml
@@ -1,20 +1,20 @@
 
-let withNewScope = (fun ( mods ) ( body ) -> (let _10_35 = (SST.pushStackFrame ())
+let withNewScope = (fun ( mods ) ( body ) -> (let _11_35 = (SST.pushStackFrame ())
 in (let v = (body ())
-in (let _10_38 = (SST.popStackFrame ())
+in (let _11_38 = (SST.popStackFrame ())
 in v))))
 
 let rec scopedWhile = (fun ( wg ) ( mods ) ( bd ) -> (match ((wg ())) with
 | true -> begin
-(let _10_77 = (withNewScope mods (Obj.magic bd))
+(let _11_77 = (withNewScope mods (Obj.magic bd))
 in (scopedWhile wg mods bd))
 end
 | false -> begin
 ()
 end))
 
-let scopedWhile1 = (fun ( r ) ( lc ) ( loopInv ) ( mods ) ( bd ) -> (scopedWhile (Obj.magic (fun ( u ) -> (let _10_4195 = (SST.memread r)
-in (lc _10_4195)))) mods bd))
+let scopedWhile1 = (fun ( r ) ( lc ) ( loopInv ) ( mods ) ( bd ) -> (scopedWhile (Obj.magic (fun ( u ) -> (let _12_3640 = (SST.memread r)
+in (lc _12_3640)))) mods bd))
 
 
 
