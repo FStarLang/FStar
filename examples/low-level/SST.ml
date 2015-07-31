9b72d50a3bcae3d8897069b6d90f59988177ed27
Abhishek Anand (@ MSR desktop) <t-aban@microsoft.com>
2015-07-31 14:13:17 -0700

    simple implementation of SST. tried to run factorial (in Test.ml). It fails with a "not yet implemented" exception. extraction will now include names of the unimplemented lidents in such messages.

--- examples/low-level/SST.ml
+++ examples/low-level/SST.ml
@@ -1,15 +1,15 @@
 
-let halloc = (fun ( init ) -> (failwith ("Not yet implemented")))
+let halloc = (fun ( init ) -> ref init)
 
-let salloc = (fun ( init ) -> (failwith ("Not yet implemented")))
+let salloc = (fun ( init ) ->  ref init)
 
-let memread = (fun ( r ) -> (failwith ("Not yet implemented")))
+let memread = (fun ( r ) -> !r )
 
-let memwrite = (fun ( r ) ( v ) -> (failwith ("Not yet implemented")))
+let memwrite = (fun ( r ) ( v ) -> r := v )
 
-let pushStackFrame = (fun ( _ ) -> (failwith ("Not yet implemented")))
+let pushStackFrame = (fun ( _ ) -> ())
 
-let popStackFrame = (fun ( _ ) -> (failwith ("Not yet implemented")))
+let popStackFrame = (fun ( _ ) -> ())
 
 
 
