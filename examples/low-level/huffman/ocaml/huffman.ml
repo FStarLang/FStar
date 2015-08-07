d7269a4c31d4a7742564bdeefd59975d1dde5910
Michael Hicks <mwh@cs.umd.edu>
2015-08-06 17:38:48 -0700

    Fixed bug in C version (was printing too much). Cleaned up the Huffman encoding code, and test.

--- examples/low-level/huffman/ocaml/huffman.ml
+++ examples/low-level/huffman/ocaml/huffman.ml
@@ -1,19 +1,25 @@
 type symbol_type = int
 type code_type = char
 
-let num_symbol_values = 257;;
+(* maximum value in input stream *)
+let symbol_value_bound = 257;;
 
+(* bits needed to represent symbols up to the bound *)
 let bits_per_symbol =
   let rec aux n b =
     let n' = n lsr 1 in
     if n' = 0 then b
     else aux n' (b+1) in
-  aux (num_symbol_values-1) 1
+  aux (symbol_value_bound-1) 1
 
+(* bytes needed to represent symbols up to the bound *)
 let bytes_per_symbol = 
   if (bits_per_symbol mod 8) = 0 then bits_per_symbol / 8 
   else (bits_per_symbol / 8) + 1
 
+(* main data structure. A node is part of a Huffman tree, and it's what is in the histogram.
+   The algorithm will construct a histogram of nodes, then construct a tree out of these, 
+   and using this tree will set the "code" of each node, which is used in the final encoding. *)
 type node =
     { mutable frequency: int; 
       mutable next: node;
@@ -22,6 +28,8 @@ type node =
       symbol: symbol_type;
       mutable code: string;
     };;
+
+(* dummy "null" node, so that we don't have to use option types. *)
 
 let rec null_node = 
   { frequency = -1;
@@ -36,6 +44,8 @@ let is_null n =
       { frequency = -1 } -> true
     | _ -> false 
       
+(* Maintains a mutable list of nodes, sorted by the node.frequency field *)
+
 module type NODE_LIST =
   sig
     type node_list
@@ -44,8 +54,8 @@ module type NODE_LIST =
     val is_singleton : node_list -> bool
     val pop_two : node_list -> node*node
     val contents : node_list -> node
-    val print_list_nodes : node_list -> bool -> unit
     val insert_in_ordered_list : node -> node_list -> unit
+    val print_list_nodes : node_list -> bool -> unit
   end
 
 module NodeList : NODE_LIST = 
@@ -54,16 +64,7 @@ module NodeList : NODE_LIST =
     let is_empty l = is_null !l
     let new_list () = ref null_node
     let contents l = !l
-    let print_list_nodes l flag =
-      let rec aux tree flag =
-	if flag then
-	  Printf.printf "Symbol Histogram: \n";
-	if is_null tree then 
-	  print_endline ""
-	else
-	  (Printf.printf "%04x: %d  " tree.symbol tree.frequency;
-	   aux tree.next false) in
-      aux !l flag
+    (* Insert by mutation. The order is according to the frequency field. *)
     let rec insert_in_ordered_list (the_node:node) (the_list:node_list) =
       if is_null !the_list then 
 	the_list := the_node
@@ -84,21 +85,28 @@ module NodeList : NODE_LIST =
 		    aux l.next in
 	     aux !the_list)
     let is_singleton l = (not (is_empty l)) && (is_null (!l).next)
+    (* removes the first two elements of the list (with the smallest frequencies) *)
     let pop_two l =
       let res = (!l,(!l).next) in
       l := (!l).next.next;
       res
+    (* DEBUG: prints list of nodes (for histogram) *)
+    let print_list_nodes l flag =
+      let rec aux tree flag =
+	if flag then
+	  Printf.printf "Symbol Histogram: \n";
+	if is_null tree then 
+	  print_endline ""
+	else
+	  (Printf.printf "%04x: %d  " tree.symbol tree.frequency;
+	   aux tree.next false) in
+      aux !l flag
   end
 
-let rec count_leaves (tree:node) :int =
-  let ndzero = tree.zero_child in
-  let ndone = tree.one_child in
-  if is_null ndzero then 1
-  else 
-    let x = count_leaves ndzero in
-    let y = count_leaves ndone in 
-    x+y
-
+(* Computes a histogram (modifying the [histogram] argument in place) of 
+   the symbol_type values (integers) appearing in [symbol_stream]. Assumes that 
+   no symbol_type value exceeds [symbol_value_bound] (a global) and that 
+   [histogram] is at least that size. *)
 let compute_histogram
     (symbol_stream: symbol_type array)
     (histogram: node option array) : unit =
@@ -121,7 +129,7 @@ let compute_histogram
   done;
   ()
 
-(* recursively computes and stores the codes in each leaf of the tree *)
+(* called by build_huffman_tree: recursively computes and stores the codes in each leaf of the tree *)
 let rec compute_code_strings 
     (tree:node)
     (code_string:bytes)
@@ -137,11 +145,17 @@ let rec compute_code_strings
      compute_code_strings one_nd code_string (code_string_pos+1))
 ;;
 
+(* Builds a huffman tree. Does this by making a sorted list of all of the elements in
+   the histogram, according to frequency. Then it iteratively builds up the tree by
+   pulling pairs of elements from the front of the list, until none remain. At the end,
+   it uses the tree to set the code fields of the leaves of the tree, which are the nodes
+   that were already in the histogram. That way, when we go through the histogram 
+   later, the nodes will have the codes we need. *)
 let build_huffman_tree
     (histogram:node option array): node =
   (* make ordered list, sorted by frequency *)
   let tree = NodeList.new_list () in
-  for i = 0 to (num_symbol_values-1) do
+  for i = 0 to (symbol_value_bound-1) do
     match histogram.(i) with
 	Some nd -> NodeList.insert_in_ordered_list nd tree
       | None -> ()
@@ -162,11 +176,11 @@ let build_huffman_tree
   done;
   (* compute codes *)
   let tree' = NodeList.contents tree in
-  let temp_code = Bytes.create num_symbol_values in
+  let temp_code = Bytes.create symbol_value_bound in
   compute_code_strings tree' temp_code 0;
   tree'
 
-(* The symbol stream is the input to encode;
+(* Encodes/compresses the input stream using the given histogram.The symbol stream is the input to encode;
    The histogram contains the frequencies of the symbols in the stream;
    The encoded_stream is modified in place; it should be at least as large as the input stream;
    Returns the number of bytes written to the encoded stream *)
@@ -202,43 +216,7 @@ let encode_stream
 	  aux (sym_idx+1) ofs' mask') in
   aux 0 0 1
 
-let rec pack_tree_iter (tree:node) (packed_tree:bytes) (ofs:int) : int =
-  let rec write_sym sym nb sofs =
-    if nb = 0 then sofs
-    else
-      (let the_sym = sym land 0xFF in
-       Bytes.set packed_tree sofs (Char.chr the_sym);
-       write_sym (sym lsr 8) (nb-1) (sofs+1)) in
-  (* leaf tag *)
-  if is_null tree.zero_child then 
-    (Bytes.set packed_tree ofs (Char.chr 1);
-     write_sym tree.symbol bytes_per_symbol (ofs+1))
-  (* node tag *)
-  else 
-    (Bytes.set packed_tree ofs (Char.chr 0);
-     let ofs' = pack_tree_iter tree.zero_child packed_tree (ofs+1) in
-     pack_tree_iter tree.one_child packed_tree ofs')
-
-let pack_huffman_tree (tree:node) : bytes*int =
-  let num_leaves = count_leaves tree in
-  let packed_tree_sz = 2*num_leaves+bytes_per_symbol*num_leaves-1 in
-  let packed_tree = Bytes.create packed_tree_sz in
-  let len = pack_tree_iter tree packed_tree 0 in
-  packed_tree,len
-
-(*
-let huffman_encode 
-    (symbol_stream:symbol_type array)
-    (encoded_stream:bytes) =
-  (* XXX return packed tree too *)
-  let histogram = Array.make num_symbol_values None in
-  compute_histogram symbol_stream histogram;
-  let tree = build_huffman_tree histogram in
-  Printf.printf "leaves in tree = %d\n" (count_leaves tree);
-  let encoded_len = encode_stream symbol_stream histogram encoded_stream in
-  (encoded_len,tree,histogram)
-*)
-
+(* DEBUG: Prints the stream encoded with the encode_stream function *)
 let print_stream 
     (encoded_stream:bytes)
     (stream_len:int) =
@@ -257,6 +235,46 @@ let print_stream
       aux (i+1) in
   aux 0
 
+(* Takes the given Huffman [tree], and packs it into the [packed_tree] bytearray
+   (which should be preallocated with sufficient space), starting at offset [ofs]. *)
+let rec pack_tree_iter (tree:node) (packed_tree:bytes) (ofs:int) : int =
+  let rec write_sym sym nb sofs =
+    if nb = 0 then sofs
+    else
+      (let the_sym = sym land 0xFF in
+       Bytes.set packed_tree sofs (Char.chr the_sym);
+       write_sym (sym lsr 8) (nb-1) (sofs+1)) in
+  (* leaf tag *)
+  if is_null tree.zero_child then 
+    (Bytes.set packed_tree ofs (Char.chr 1);
+     write_sym tree.symbol bytes_per_symbol (ofs+1))
+  (* node tag *)
+  else 
+    (Bytes.set packed_tree ofs (Char.chr 0);
+     let ofs' = pack_tree_iter tree.zero_child packed_tree (ofs+1) in
+     pack_tree_iter tree.one_child packed_tree ofs')
+
+(* Determines the "size" of the [tree], by counting its leaves. *)
+let rec count_leaves (tree:node) :int =
+  let ndzero = tree.zero_child in
+  let ndone = tree.one_child in
+  if is_null ndzero then 1
+  else 
+    let x = count_leaves ndzero in
+    let y = count_leaves ndone in 
+    x+y
+
+(* Packs the given Huffman [tree] into a bytearray, which is returned,
+   along with the length (which might be shorter than the physical
+   length of the bytearray. *)
+let pack_huffman_tree (tree:node) : bytes*int =
+  let num_leaves = count_leaves tree in
+  let packed_tree_sz = 2*num_leaves+bytes_per_symbol*num_leaves-1 in
+  let packed_tree = Bytes.create packed_tree_sz in
+  let len = pack_tree_iter tree packed_tree 0 in
+  packed_tree,len
+
+(* DEBUG: prints the packed huffman tree *)
 let print_tree (packed_tree:bytes) (tree_size:int) =
   Printf.printf "\nTree (%d): " tree_size;
   let rec aux i =
@@ -273,40 +291,32 @@ let print_tree (packed_tree:bytes) (tree_size:int) =
 	aux (i+3)) in
   aux 0
 
+(* Top-level routine to perform huffman coding:
+   [symbol_stream] is an array of symbols that are to be compressed;
+   [encoded_stream] is a bytearray in which to store the compressed stream
+     (should be at least the same length as symbol_stream, in case there is
+     no compression)
+   Returns a triple containing the packed huffman tree (as a bytearray), 
+     the length of valid data in the packed tree array,
+     and the length of the valid data in the encoded stream
+*)
+let huffman_encode 
+    (symbol_stream:symbol_type array)
+    (encoded_stream:bytes) : bytes*int*int =
+  let histogram = Array.make symbol_value_bound None in
+  compute_histogram symbol_stream histogram;
+  let tree = build_huffman_tree histogram in
+  Printf.printf "leaves in tree = %d\n" (count_leaves tree);
+  let encoded_len = encode_stream symbol_stream histogram encoded_stream in
+  let packed_tree,packed_len = pack_huffman_tree tree in
+  packed_tree,packed_len,encoded_len
+;;
+
+(* TEST *)
+
 let test_inp = [| 1;1;2;2;3;1;5;4;1;8 |];;
 let test_oup = Bytes.create 10;;
-let test_hist = Array.make num_symbol_values None;;
-compute_histogram test_inp test_hist;;
-let tree = build_huffman_tree test_hist;;
-Printf.printf "leaves in tree = %d\n" (count_leaves tree);;
-let encoded_len = encode_stream test_inp test_hist test_oup;;
+let packed_tree,packed_len,encoded_len = huffman_encode test_inp test_oup;;
 print_stream test_oup encoded_len;;
-let packed_tree,packed_len = pack_huffman_tree tree;;
 print_tree packed_tree packed_len;;
-
-
-(*    
-int huffman_encode (SymbolType *symbol_stream, int symbol_stream_length, Byte **packed_huffman_tree, int *huffman_tree_size, CodeType *encoded_stream)
-                            
-                           
-                                         /* to be malloced */
-                         
-                                         /* already malloced */
-  {
-  struct node *histogram[NUM_SYMBOL_VALUES];
-  struct node *huffman_tree;
-  int encoded_stream_length;
-  register int i;
-  
-  /* initialize histogram */
-  for (i=0; i<NUM_SYMBOL_VALUES; i++)  histogram[i] = (struct node * ) NULL;
-  
-  compute_histogram(symbol_stream, symbol_stream_length, histogram);
-  build_huffman_tree(histogram, &huffman_tree);
-  encoded_stream_length = 
-    encode_stream(symbol_stream, symbol_stream_length, histogram, encoded_stream);
-  *huffman_tree_size = pack_huffman_tree(huffman_tree, packed_huffman_tree);
-  free_tree_nodes(huffman_tree);
-  return (encoded_stream_length);
-  }
-					     *)
+print_endline "";;
