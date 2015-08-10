f56019c85bdeb63162c066920433506c8e988d69
Abhishek Anand (on VAIO laptop) <t-aban@microsoft.com>
2015-08-10 07:14:42 +0100

    pushed a frame in the beginning, based on Mike's suggestion.
    Reduced printing to just one int,
    in hopes to reduce any variability caused by IO.
    Also, does OCaml try to do GC while waiting for IO (e.g. printing)?
    
    Run times on laptop are all over the place.

--- examples/low-level/TestSieveInLoop.ml
+++ examples/low-level/TestSieveInLoop.ml
@@ -6,6 +6,15 @@ let rec printMaxPrimes (upto : int) : unit =
 	else ((print_string  (string_of_int (sieveJustMax upto)));
 			printMaxPrimes (upto -1))
 
+let rec sumMaxPrimes (upto : int) : int = 
+	if upto<=0 
+	then 0
+	else ((((sieveJustMax upto)))+
+			sumMaxPrimes (upto -1))
+
 let _ =
 let max = int_of_string (Sys.argv.(1)) in
-	printMaxPrimes max;;
+	SST.pushStackFrame ();
+	(* printMaxPrimes max; *)
+	print_int (sumMaxPrimes max);
+	SST.popStackFrame ();;
