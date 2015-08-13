0b6a4bf4b785ce17ae7db19c9aa41cf04c73d5a6
Abhishek Anand (@ MSR desktop) <t-aban@microsoft.com>
2015-08-13 14:23:06 -0700

    added the node d/s in Huffman.ml to the extraction test suite.

--- examples/extraction/Test.ml
+++ examples/extraction/Test.ml
@@ -422,6 +422,11 @@ let ___OddSEven____1 = (fun ( _0 ) ( projectee ) -> (match (projectee) with
 _8_152
 end))
 
+type node =
+{frequency : int; next : node; zero_child : node Support.ST.ref; one_child : node; symbol : int; code : string}
+
+let is_Mknode = (fun ( _ ) -> (Support.All.failwith "Not yet implemented:is\x5fMknode"))
+
 let ev2 = EvSOdd (S (O), OddSEven (O, Ev0))
 
 type someLemmaStatement =
