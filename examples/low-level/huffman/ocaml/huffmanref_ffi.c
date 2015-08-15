8f5653603f456549a25765c558ea7dee0a0e71b4
Michael Hicks <mwh@cs.umd.edu>
2015-08-14 21:49:10 -0700

    Version of huffman that uses references rather than mutable record fields. However, this version doesn't work sometimes, and I haven't figured out why yet. Hopefully it's enough to get started porting to FST.

--- examples/low-level/huffman/ocaml/huffmanref_ffi.c
+++ examples/low-level/huffman/ocaml/huffmanref_ffi.c
@@ -3,19 +3,24 @@
 #include "camlstack.h"
 #include "stack.h"
 
+extern value stack_mkref_noscan(value v); // in camlstack.c
+extern value stack_mkref(value v);
+
 static value empty_string = (value)0;
 
 CAMLprim value stack_mknode(value v1, value v2, value v3, value v4, value v5)
 {
   CAMLparam5 (v1, v2, v3, v4, v5);
-  int mask[3];
+  int mask[2];
   int nbits = 0;
+  value v1ref, v2ref, v6ref;
   if (empty_string == (value)0)
     empty_string = stack_caml_alloc_string(1);
-  if (!(Is_long(v2) || is_stack_pointer((void *)v2))) {
-    mask[0] = 2;
-    nbits++;
-  }
+  v1ref = stack_mkref_noscan(v1);
+  if (is_stack_pointer((void *)v2))
+    v2ref = stack_mkref_noscan(v2);
+  else
+    v2ref = stack_mkref(v2);
   if (!(Is_long(v3) || is_stack_pointer((void *)v3))) {
     mask[nbits] = 3;
     nbits++;
@@ -24,16 +29,17 @@ CAMLprim value stack_mknode(value v1, value v2, value v3, value v4, value v5)
     mask[nbits] = 4;
     nbits++;
   }
+  v6ref = stack_mkref_noscan(empty_string);
   value tuple = stack_caml_alloc_tuple(6,nbits,mask);
   if (tuple == (value)0)
     caml_failwith ("Huffman.mknode");
   else {
-    Field(tuple, 0) = v1;
-    Field(tuple, 1) = v2;
+    Field(tuple, 0) = v1ref;
+    Field(tuple, 1) = v2ref;
     Field(tuple, 2) = v3;
     Field(tuple, 3) = v4;
     Field(tuple, 4) = v5;
-    Field(tuple, 5) = empty_string;
+    Field(tuple, 5) = v6ref;
     CAMLreturn(tuple);
   }
 }
