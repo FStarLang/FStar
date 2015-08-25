802985b6182cbcea2a42506c5dba7c9e8a6fd000
nikswamy <nikswamy@users.noreply.github.com>
2015-08-25 14:46:56 -0700

    removing old files

--- src/support/ocaml/fstar-lib/src/map.ml
+++
@@ -1,8 +0,0 @@
-type ('a, 'b) t =  ('a, 'b) BatMap.t
-let sel m k = BatMap.find k m
-let upd m k v = BatMap.add k v m
-let const x = BatMap.empty
-let concat = BatMap.union
-let equal x y =
-  (BatMap.is_empty x && BatMap.is_empty y) || (BatMap.is_empty (BatMap.filter (fun k v -> try BatMap.find k x<>v with Not_found -> true) y))
-
