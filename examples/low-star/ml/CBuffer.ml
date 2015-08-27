5efa95d563b9a40bb242c11ddf81e9e2c3d84c8b
jkz <jkzinzindohoue@gmail.com>
2015-08-27 16:39:33 +0100

    low-star example, running make works on my side

--- examples/low-star/ml/CBuffer.ml
+++ examples/low-star/ml/CBuffer.ml
@@ -1,3 +1,10 @@
-
-(* Change content for base ptr *)
-type buffer = { content: char array; start_idx:int; length:int}
+(*
+  This type corresponds to the iovec C structure :
+  strut iovec{
+    void *iov_base;
+    size_t iov_len;
+  };
+*)
+type buffer = { content: char array;
+                start_idx:int;
+                length:int}
--- examples/low-star/ml/CSystem.ml
+++ examples/low-star/ml/CSystem.ml
@@ -6,20 +6,21 @@ type buffer = { content : bytes; start_idx : int; length : int;}
 external ocaml_writev: fd -> buffer array -> int -> int = "ocaml_writev"
 external ocaml_readv: fd -> buffer array -> int -> int = "ocaml_readv"
 
-let char_array_to_bytes a =
-  let b = Bytes.create (Array.length a) in
-  for i = 0 to Array.length a - 1 do
-    Bytes.set b i a.(i)
-  done;
-  b
+let char_array_to_bytes a b idx len =
+  for i = 0 to len - 1 do
+    Bytes.set b i a.(i+idx)
+  done
 
-let bytes_to_char_array a b =
-  for i = 0 to Array.length a - 1 do
-    a.(i) <- Bytes.get b i
+let bytes_to_char_array a b idx len =
+  for i = 0 to len - 1 do
+    a.(i+idx) <- Bytes.get b i
   done
 
 let cbuffer_to_buffer (cbuff:CBuffer.buffer) =
-  let (buff:buffer) = { content = char_array_to_bytes cbuff.content;
+  let tot_len = Array.length cbuff.content in
+  let c = Bytes.create tot_len in
+  char_array_to_bytes cbuff.content c 0 tot_len;
+  let (buff:buffer) = { content = c;
                         start_idx = cbuff.start_idx;
                         length = cbuff.length; } in
   buff
@@ -40,36 +41,37 @@ let map_flag f =
   | CREATE -> Unix.O_CREAT
   | _ -> failwith "Unhandled flag"
 
+(* Hack : converts (array char) ~> bytes and back *)
+(* TODO : better hack with bigarray ? *)
 let read f (b:CBuffer.buffer) idx len =
-  let buff = char_array_to_bytes b.content in
-  let n = Unix.read f buff idx len in
-  print_string ("Content read : " ^ (Bytes.sub_string buff 0 n) ^ "\n");
-  bytes_to_char_array b.content buff;
+  let buff = Bytes.create len in
+  let n = Unix.read f buff 0 len in
+  bytes_to_char_array b.content buff b.start_idx b.length;
   n
 
+(* TODO: same as read so that it works *)
 let write = Unix.write
+
 let openfile f l i = Unix.openfile f (List.map map_flag l) i
 let close = Unix.close
 
+(* TODO : test *)
 let readv fd buffers =
   let buffs = Array.of_list buffers in
   let buff_len = Array.length buffs in
+  let buffs = Array.map cbuffer_to_buffer buffs in
   ocaml_readv fd buffs buff_len
 
+(* Conversions and call to C writev function *)
 let writev fd (buffers:CBuffer.buffer list) =
   let buffs = Array.of_list buffers in
-  for i = 0 to Array.length buffs - 1 do
-    print_string "Buffer content :\n";
-    for j = buffs.(i).start_idx to buffs.(i).start_idx + buffs.(i).length do
-      print_char buffs.(i).content.(j) ;
-    done;
-    print_string "\n";
-  done;
   let buff_len = Array.length buffs in
   let buffs = Array.map cbuffer_to_buffer buffs in
   ocaml_writev fd buffs buff_len
 
+
 (*
+TODO : implement these functions 
 let get_inet_tcp_socket = (fun ( _ ) -> (Support.All.failwith "Not yet implemented:get_inet_tcp_socket"))
 
 let accept = (fun ( f ) -> (Support.All.failwith "Not yet implemented:accept"))
@@ -77,46 +79,4 @@ let accept = (fun ( f ) -> (Support.All.failwith "Not yet implemented:accept"))
 let bind = (fun ( _ ) ( _ ) ( _ ) -> (Support.All.failwith "Not yet implemented:bind"))
 
 let listen = (fun ( _ ) ( _ ) -> (Support.All.failwith "Not yet implemented:listen"))
-
-
-let is_O_RDONLY = (fun ( _discr_ ) -> (match (_discr_) with
-| Support.System.O_RDONLY -> begin
-true
-end
-| _ -> begin
-false
-end))
-
-let is_O_WRONLY = (fun ( _discr_ ) -> (match (_discr_) with
-| Support.System.O_WRONLY -> begin
-true
-end
-| _ -> begin
-false
-end))
-
-let is_O_RDWR = (fun ( _discr_ ) -> (match (_discr_) with
-| Support.System.O_RDWR -> begin
-true
-end
-| _ -> begin
-false
-end))
-
-let is_O_APPEND = (fun ( _discr_ ) -> (match (_discr_) with
-| Support.System.O_APPEND -> begin
-true
-end
-| _ -> begin
-false
-end))
-
-let is_O_CREAT = (fun ( _discr_ ) -> (match (_discr_) with
-| Support.System.O_CREAT -> begin
-true
-end
-| _ -> begin
-false
-end))
-
 *)
