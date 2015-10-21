2bd3bf66ed80f7efe84243fcab3c8c8685e9ae46
nikswamy <nikswamy@users.noreply.github.com>
2015-10-20 22:13:53 -0700

    basic implementation of strongly reducing krivine abstract machine with Camlstack

--- examples/low-level/kam/kam.ml
+++ examples/low-level/kam/kam.ml
@@ -1,12 +1,58 @@
 let mk_list n =
   Camlstack.push_frame ();
-  let out = Camlstack.mk_ref [] in
+  let out = Camlstack.mkref [] in
   for i = 0 to n do
     out := Camlstack.cons i !out;
   done;
   Camlstack.pop_frame()
 
-let _ = mk_list 1000
+;; let _ = mk_list 1000
 
-  
 
+type tm =
+  | Var of int
+  | Abs of int * tm
+  | App of tm  * tm 
+
+type env = (int * closure) list
+and closure = 
+  | Clos of env * tm
+  | Open
+
+let null : closure = Obj.magic 0
+
+type stack = tm list
+
+let rec find env x : closure = match env with 
+  | (y, k)::tl -> 
+    if y=x then k else find tl x
+  | [] -> raise Not_found
+
+open Camlstack 
+let rec norm (env:env) (stack:stack) (tm:tm) : tm = match tm with 
+  | Abs(x, body) -> 
+    begin match stack with 
+      | [] -> Abs (x, norm (cons (mkpair x Open) env) stack body)
+      | hd::tl -> norm (cons (mkpair x (Clos(env, hd))) env) tl body 
+    end
+
+  | App(t1, t2) -> 
+    norm env (cons t2 stack) t1
+
+  | Var x -> 
+    let k = find env x in
+    match k with 
+      | Open -> rebuild env tm stack
+      | Clos(env', k) -> norm env' stack k
+
+and rebuild env head stack = match stack with 
+  | [] -> head
+  | hd::tl -> rebuild env (App(head, norm env [] hd)) tl
+
+let norm e = 
+  push_frame();
+  let x = norm [] [] e in 
+  pop_frame(); 
+  x
+
+let _ = norm (Abs(0, App(Abs(1, Var 1), Var 0)))
