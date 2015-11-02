630765f380989ea0ef1123f8562777a65314af5f
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2015-11-02 13:39:13 +0100

    OCaml wrapper for Uint16 F* module

--- contrib/Platform/ml/uintegers.ml
+++ contrib/Platform/ml/uintegers.ml
@@ -30,6 +30,38 @@ let compare x y = Stdint.Uint8.compare x y
 
 end
 
+module Uint16 = struct
+  
+let zero = Stdint.Uint16.zero
+let one = Stdint.Uint16.one
+let max_int = Stdint.Uint16.max_int
+let min_int = Stdint.Uint16.min_int
+let bits = Stdint.Uint16.bits
+
+let add x y = Stdint.Uint16.add x y
+let sub x y = Stdint.Uint16.sub x y
+let mul x y = Stdint.Uint16.mul x y
+let div x y = Stdint.Uint16.div x y
+let rem x y = Stdint.Uint16.rem x y
+let succ x = Stdint.Uint16.succ x
+let pred x = Stdint.Uint16.pred x
+let abs x = Stdint.Uint16.abs x
+let neg x = Stdint.Uint16.neg x
+let logand x y = Stdint.Uint16.logand x y
+let logor x y = Stdint.Uint16.logor x y
+let logxor x y = Stdint.Uint16.logxor x y
+let lognot x = Stdint.Uint16.lognot x
+let shift_left x n = Stdint.Uint16.shift_left x n
+let shift_right x n = Stdint.Uint16.shift_right x n
+let shift_right_logical x n = Stdint.Uint16.shift_right_logical x n
+let of_int n = Stdint.Uint16.of_int n
+let to_int n = Stdint.Uint16.to_int n
+let of_string s = Stdint.Uint16.of_string s
+let to_string x = Stdint.Uint16.to_string x
+let compare x y = Stdint.Uint16.compare x y
+
+end
+                 
 module Uint32 = struct
   
 let zero = Stdint.Uint32.zero
