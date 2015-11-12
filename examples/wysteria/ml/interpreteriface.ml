9d7b0313b9042eca9b45153481ff60f712a39c31
Aseem Rastogi <aseem@cs.umd.edu>
2015-11-12 14:39:09 -0500

    running psi with new design

--- examples/wysteria/ml/interpreteriface.ml
+++ examples/wysteria/ml/interpreteriface.ml
@@ -2,6 +2,24 @@ open Prins
 open AST
 open Prog
 
+exception Interpreteriface_error of string
+
+let cast x = Obj.magic x
+
+let project_value_content (dv:dvalue) :Obj.t =
+  let D_v (_, v) = dv in
+  let content =
+    match v with
+      | V_prin p -> cast p
+      | V_eprins eps -> cast eps
+      | V_unit -> cast ()
+      | V_bool b -> cast b
+      | V_opaque (_, v, _, _, _, _) -> cast v
+      | _ -> raise (Interpreteriface_error ("Interpreteriface does not handle this value type"))
+  in
+  content
+
+
 let init_env =
   fun x ->
     if name_of_var x = "alice" then Some (D_v (const_meta, V_prin Alice))
