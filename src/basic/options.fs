6ee0d64624a9c343d236b656f5438f80dd96e143
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2016-02-05 22:03:46 +0100

    Delete trailing whitespace in src/basic/options.fs

--- src/basic/options.fs
+++ src/basic/options.fs
@@ -106,7 +106,7 @@ let check_cardinality () = match !cardinality with
     | _ -> false
 let dep = ref None
 let explicit_deps = Util.mk_ref false
-let init_options () = 
+let init_options () =
     show_signatures := [];
     norm_then_print := true;
     z3_exe := Platform.exe "z3";
@@ -178,7 +178,7 @@ let get_include_path () =
    * installation (into /usr/local for instance) *)
   let h = get_fstar_home () in
   !_include_path @ ["."; h ^ "/lib"; h ^ "/lib/fstar"; h ^ "/stdlib" ; h ^ "/stdlib/fstar"]
-    
+
 let find_file filename =
     let search_path = get_include_path () in
     try
@@ -198,11 +198,11 @@ let find_file filename =
                 Some path
               else
                 None))
-    with _ -> 
+    with _ ->
       None
 
 let prims () = match !prims_ref with
-  | None -> 
+  | None ->
     (let filen = "prims.fst" in
     match find_file filen with
     | Some result ->
@@ -301,7 +301,7 @@ and parse_codegen s =
      (Util.print_string "Wrong argument to codegen flag\n";
       display_usage (specs ()); exit 1)
 and validate_cardinality x = match x with
-    | "warn" 
+    | "warn"
     | "check"
     | "off" -> x
     | _ ->   (Util.print_string "Wrong argument to cardinality flag\n";
@@ -320,12 +320,12 @@ let should_verify m =
 
 let dont_gen_projectors m = List.contains m (!__temp_no_proj)
 
-let should_print_message m = 
-    should_verify m 
-    && not (List.contains m !admit_fsi) 
+let should_print_message m =
+    should_verify m
+    && not (List.contains m !admit_fsi)
     && m <> "Prims"
 
-let set_options = 
+let set_options =
     //The smt option is a security concern
     //only allow it to be set from the command line, not from the build-config
     let no_smt_specs = specs() |> List.filter (fun (_, name, _, _) -> name <> "smt") in
