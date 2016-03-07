f1f2f245f71bae300ff8333570527bd0cc6830b1
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2016-03-07 19:57:14 +0100

    New fun returning random as string (Not exposed to .fst)

--- contrib/CoreCrypto/ml/CoreCrypto.ml
+++ contrib/CoreCrypto/ml/CoreCrypto.ml
@@ -226,6 +226,11 @@ let random i =
     else if (not (ocaml_rand_status())) then failwith "random number generator not ready"
     else bytes_of_string (ocaml_rand_bytes i)
 
+let random_string i =
+    if (i < 0) then invalid_arg "input to random must be non-negative"
+    else if (not (ocaml_rand_status())) then failwith "random number generator not ready"
+    else ocaml_rand_bytes i
+
 (* -------------------------------------------------------------------- *)
 type rsa
 
