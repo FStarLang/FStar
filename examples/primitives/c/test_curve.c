a4a7a24eafdced24aba8a7008abc7079172a477e
Jean Karim <jkzinzindohoue@gmail.com>
2016-03-17 18:31:35 +0100

    Proper test file

--- examples/primitives/c/test_curve.c
+++ examples/primitives/c/test_curve.c
@@ -10,91 +10,12 @@
 #define Bignum_Bigint_bigint_wide uint128_t*
 #define Bignum_Bigint_bytes uint8*
 
-typedef uint64_t limb;
-typedef limb felem[5];
-
 typedef struct Curve_Point_point Curve_Point_point;
 struct Curve_Point_point{
 Bignum_Bigint_bigint x;
 Bignum_Bigint_bigint y;
 Bignum_Bigint_bigint z;
 };
-
-
-void fmul(felem output, const felem in2, const felem in) {
-  uint128_t t[5];
-  limb r0,r1,r2,r3,r4,s0,s1,s2,s3,s4,c;
-
-  r0 = in[0];
-  r1 = in[1];
-  r2 = in[2];
-  r3 = in[3];
-  r4 = in[4];
-
-  s0 = in2[0];
-  s1 = in2[1];
-  s2 = in2[2];
-  s3 = in2[3];
-  s4 = in2[4];
-
-  t[0]  =  ((uint128_t) r0) * s0;
-  t[1]  =  ((uint128_t) r0) * s1 + ((uint128_t) r1) * s0;
-  t[2]  =  ((uint128_t) r0) * s2 + ((uint128_t) r2) * s0 + ((uint128_t) r1) * s1;
-  t[3]  =  ((uint128_t) r0) * s3 + ((uint128_t) r3) * s0 + ((uint128_t) r1) * s2 + ((uint128_t) r2) * s1;
-  t[4]  =  ((uint128_t) r0) * s4 + ((uint128_t) r4) * s0 + ((uint128_t) r3) * s1 + ((uint128_t) r1) * s3 + ((uint128_t) r2) * s2;
-
-  r4 *= 19;
-  r1 *= 19;
-  r2 *= 19;
-  r3 *= 19;
-
-  t[0] += ((uint128_t) r4) * s1 + ((uint128_t) r1) * s4 + ((uint128_t) r2) * s3 + ((uint128_t) r3) * s2;
-  t[1] += ((uint128_t) r4) * s2 + ((uint128_t) r2) * s4 + ((uint128_t) r3) * s3;
-  t[2] += ((uint128_t) r4) * s3 + ((uint128_t) r3) * s4;
-  t[3] += ((uint128_t) r4) * s4;
-
-                  r0 = (limb)t[0] & 0x7ffffffffffff; c = (limb)(t[0] >> 51);
-  t[1] += c;      r1 = (limb)t[1] & 0x7ffffffffffff; c = (limb)(t[1] >> 51);
-  t[2] += c;      r2 = (limb)t[2] & 0x7ffffffffffff; c = (limb)(t[2] >> 51);
-  t[3] += c;      r3 = (limb)t[3] & 0x7ffffffffffff; c = (limb)(t[3] >> 51);
-  t[4] += c;      r4 = (limb)t[4] & 0x7ffffffffffff; c = (limb)(t[4] >> 51);
-  r0 +=   c * 19; c = r0 >> 51; r0 = r0 & 0x7ffffffffffff;
-  r1 +=   c;      c = r1 >> 51; r1 = r1 & 0x7ffffffffffff;
-  r2 +=   c;
-
-  output[0] = r0;
-  output[1] = r1;
-  output[2] = r2;
-  output[3] = r3;
-  output[4] = r4;
-}
-
-void fmul2(uint128_t t[9], const felem in2, const felem in) {
-  limb r0,r1,r2,r3,r4,s0,s1,s2,s3,s4,c;
-
-  r0 = in[0];
-  r1 = in[1];
-  r2 = in[2];
-  r3 = in[3];
-  r4 = in[4];
-
-  s0 = in2[0];
-  s1 = in2[1];
-  s2 = in2[2];
-  s3 = in2[3];
-  s4 = in2[4];
-
-  t[0]  =  ((uint128_t) r0) * s0;
-  t[1]  =  ((uint128_t) r0) * s1 + ((uint128_t) r1) * s0;
-  t[2]  =  ((uint128_t) r0) * s2 + ((uint128_t) r2) * s0 + ((uint128_t) r1) * s1;
-  t[3]  =  ((uint128_t) r0) * s3 + ((uint128_t) r3) * s0 + ((uint128_t) r1) * s2 + ((uint128_t) r2) * s1;
-  t[4]  =  ((uint128_t) r0) * s4 + ((uint128_t) r4) * s0 + ((uint128_t) r3) * s1 + ((uint128_t) r1) * s3 + ((uint128_t) r2) * s2;
-
-  t[5] = ((uint128_t) r4) * s1 + ((uint128_t) r1) * s4 + ((uint128_t) r2) * s3 + ((uint128_t) r3) * s2;
-  t[6] = ((uint128_t) r4) * s2 + ((uint128_t) r2) * s4 + ((uint128_t) r3) * s3;
-  t[7] = ((uint128_t) r4) * s3 + ((uint128_t) r3) * s4;
-  t[8] = ((uint128_t) r4) * s4;
-}
 
 
 void decode_scalar(uint8 scalar[32]){
@@ -143,27 +64,6 @@ void print_bigint(uint64 z[5]){
   }
 }
 
-void aux(Bignum_Bigint_bigint_wide res , Bignum_Bigint_bigint a , uint64 s , int ctr ) {
-  switch (ctr) { 
-  case 0 : 
-    ;  ; 
-    break;
-  default:;  
-    int _23_76 ;
-    _23_76 = ctr ; 
-    int i;
-    i = (ctr - 1 ) ; 
-    uint64 ai;
-    ai = a[i] ; 
-    uint128_t resi;
-    resi = (((uint128_t) ai ) *  s ) ; 
-    res [ i ] =  resi  ; 
-    aux (res, a, s, i)  ; 
-    return   ;  
-    break;
-  }
-}
-
 void test(){
   uint64 output[9], rx[9] = {0}, ry[9] = {0}, rz[9] = {0}, qx[9] = {0}, qy[9] = {0}, qz[9] = {1}, zrecip[9] = {0};
   uint64 axx[9] = {0}, ayy[9] = {0}, azz[9] = {0}, axxx[9] = {0}, ayyy[9] = {0}, azzz[9] = {0};
@@ -175,7 +75,7 @@ void test(){
 		    0x35, 0x94, 0xc1, 0xa4, 0x24, 0xb1, 0x5f, 0x7c,
 		    0x72, 0x66, 0x24, 0xec, 0x26, 0xb3, 0x35, 0x3b,
 		    0x10, 0xa9, 0x03, 0xa6, 0xd0, 0xab, 0x1c, 0x4c};
-
+  char* expected = "c3da55379de9c6908e94ea4df28d084f32eccf03491c71f754b4075577a28552";
   decode_scalar(scalar);
   decode_input(qx, input);
 
@@ -183,13 +83,15 @@ void test(){
   Curve_Point_point res = (Curve_Point_point) {rx, ry, rz};
 
   int i;
+  printf(" Running 1000 computations of Curve25519 test vector ...\n");
   for (i=0;i<1000;i++){
     Curve_Ladder_montgomery_ladder(res, scalar, basepoint);
   }
-  print_array(res.x, 5);
+
   Bignum_Core_crecip_prime(zrecip, res.z);
-  print_array(zrecip, 5);
   Bignum_Core_fmul(output, res.x, zrecip);
+  
+  printf("Expected:\n %s\nGot:\n", expected);
   print_bigint(output);
 }
 
