6c94020d6df27954a788d1b76b24533ad2ead53d
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2016-04-01 16:11:37 +0200

    Bind UInt64 to Stdint

--- examples/primitives/ml/FStar_UInt64.ml
+++ examples/primitives/ml/FStar_UInt64.ml
@@ -1,33 +1,36 @@
-type uint64 = int
+type uint64 = Stdint.uint64
 type native_int = uint64
 
-let (zero:uint64) = 0
-let (one:uint64) = 1
-let (ones:uint64) = -1
+let (zero:uint64) = Stdint.Uint64.zero
+let (one:uint64) = Stdint.Uint64.one
 
 let bits = 64
 
 (* Standard operators *)
-let add a b = a + b
-let add_mod a b = a + b
-let sub a b = a - b
-let sub_mod a b = a - b
-let mul a b = a * b
-let mul_mod a b = a * b
-let div a b = a / b
-let rem a b = a mod b
+let add_mod a b = Stdint.Uint64.add a b
+(* let add a b = a + b *)
+let sub_mod a b = Stdint.Uint64.sub a b
+(* let sub a b = a - b *)
+let mul_mod a b = Stdint.Uint64.mul a b
+(* let mul a b = a * b *)
+let div a b = Stdint.Uint64.div a b
+let rem a b = Stdint.Uint64.rem a b
 
-let logand a b = a land b
-let logxor a b = a lxor b
-let logor a b = a lor b
-let lognot a = lnot a
+let logand a b = Stdint.Uint64.logand a b
+let logxor a b = Stdint.Uint64.logxor a b
+let logor a b = Stdint.Uint64.logor a b
+let lognot a = Stdint.Uint64.lognot a
 
-let shift_left a s = a lsl s
-let shift_right a s = a lsr s
+let shift_left a s = Stdint.Uint64.shift_left a s
+let shift_right a s = Stdint.Uint64.shift_right a s
+let shift_right_logical a s = Stdint.Uint64.shift_right_logical a s
 
-let rotate_left a s = (a lsl s) + (a lsr (64-s))
-let rotate_right a s = (a lsl (64-s)) + (a lsr s)
-
+let rotate_left a s =
+  logor (shift_left a s)
+        (shift_right_logical a (64 - s))
+let rotate_right a s =
+  logor (shift_right_logical a s)
+        (shift_left a (64 - s))
 (*
 val to_uint64: sint -> Tot uint64
 let to_uint64 s = to_usint n s
@@ -42,6 +45,7 @@ let op_Hat_Greater_Greater = shift_right
 let op_Hat_Amp = logand
 let op_Hat_Bar = logor
 let op_Hat_Hat = logxor
+let (ones:uint64) = lognot zero
 
 
 let of_uint32 s = s
