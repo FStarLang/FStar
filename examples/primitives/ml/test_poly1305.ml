338055dc7dd20d981feab22cd2967e510ac69a59
Jean Karim <jkzinzindohoue@gmail.com>
2016-04-07 00:52:11 +0200

    added performance test to poly

--- examples/primitives/ml/test_poly1305.ml
+++ examples/primitives/ml/test_poly1305.ml
@@ -16,6 +16,11 @@ let from_bytestring s =
     upd b i (int_of_string ("0x" ^ (String.sub s (2*i) 2)))
   done;
   b
+
+let time f x s =
+  let t = Sys.time() in
+  let _ = f x in
+  Printf.printf "Ellapsed time for %s : %fs\n" s (Sys.time() -. t)
   
 let key = from_bytestring "85d6be7857556d337f4452fe42d506a80103808afb0db2fd4abff6af4149f51b"
 let msg = from_string "Cryptographic Forum Research Group"
@@ -25,7 +30,7 @@ let _ =
   (* To store the hash *)
   let hash = create (0) 16 in
   (* Run hash computation *)
-  poly1305_mac hash msg 34 key;
+  time (fun () -> for i = 1 to 1000000 do poly1305_mac hash msg 34 key done) () "1.000.000 iterations of poly1305";
   (* Output result *)
   print_string ("Expected " ^ expected ^ "\n");
   print_string "Got hash:\n";
