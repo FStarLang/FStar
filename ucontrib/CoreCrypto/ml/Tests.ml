40cc76b4afbfac487e65ba67798a55a4f6248e2a
Santiago Zanella-Beguelin <santiago@microsoft.com>
2016-06-02 19:26:08 +0100

    Simplify tests

--- ucontrib/CoreCrypto/ml/Tests.ml
+++ ucontrib/CoreCrypto/ml/Tests.ml
@@ -1207,22 +1207,6 @@ module TestEcdhke = struct
     string_of_bytes shared1 = string_of_bytes shared2
 end
 
-module TestCertLoad = struct
-  let test () =
-    match load_chain "test_chain.pem" with
-    | None -> false
-    | Some chain ->
-      begin
-      (*Printf.printf "Parsed a chain of %d elements.\n" (List.length chain);*)
-      match load_key "test_client.key" with
-      | None -> false
-      | Some k ->
-        let tbs = bytes_of_string "hello world" in
-        let sigv = maybe_hash_and_sign k (Some SHA256) tbs in
-        verify_signature k (Some SHA256) tbs sigv
-      end
-end
-
 module TestCertAndSign = struct
   let test mode () =
     match load_chain ("pki/" ^ mode ^ "/certificates/" ^ mode ^ ".cert.mitls.org.crt") with
@@ -1249,7 +1233,7 @@ module TestCert = struct
      let c2 = "MIIETTCCAzWgAwIBAgILBAAAAAABRE7wNjEwDQYJKoZIhvcNAQELBQAwVzELMAkGA1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2ExEDAOBgNVBAsTB1Jvb3QgQ0ExGzAZBgNVBAMTEkdsb2JhbFNpZ24gUm9vdCBDQTAeFw0xNDAyMjAxMDAwMDBaFw0yNDAyMjAxMDAwMDBaMEwxCzAJBgNVBAYTAkJFMRkwFwYDVQQKExBHbG9iYWxTaWduIG52LXNhMSIwIAYDVQQDExlBbHBoYVNTTCBDQSAtIFNIQTI1NiAtIEcyMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2gHs5OxzYPt+j2q3xhfjkmQy1KwA2aIPue3ua4qGypJn2XTXXUcCPI9A1p5tFM3D2ik5pw8FCmiiZhoexLKLdljlq10dj0CzOYvvHoN9ItDjqQAu7FPPYhmFRChMwCfLew7sEGQAEKQFzKByvkFsMVtI5LHsuSPrVU3QfWJKpbSlpFmFxSWRpv6mCZ8GEG2PgQxkQF5zAJrgLmWYVBAAcJjI4e00X9icxw3A1iNZRfz+VXqG7pRgIvGu0eZVRvaZxRsIdF+ssGSEj4k4HKGnkCFPAm694GFn1PhChw8K98kEbSqpL+9Cpd/do1PbmB6B+Zpye1reTz5/olig4hetZwIDAQABo4IBIzCCAR8wDgYDVR0PAQH/BAQDAgEGMBIGA1UdEwEB/wQIMAYBAf8CAQAwHQYDVR0OBBYEFPXN1TwIUPlqTzq3l9pWg+Zp0mj3MEUGA1UdIAQ+MDwwOgYEVR0gADAyMDAGCCsGAQUFBwIBFiRodHRwczovL3d3dy5hbHBoYXNzbC5jb20vcmVwb3NpdG9yeS8wMwYDVR0fBCwwKjAooCagJIYiaHR0cDovL2NybC5nbG9iYWxzaWduLm5ldC9yb290LmNybDA9BggrBgEFBQcBAQQxMC8wLQYIKwYBBQUHMAGGIWh0dHA6Ly9vY3NwLmdsb2JhbHNpZ24uY29tL3Jvb3RyMTAfBgNVHSMEGDAWgBRge2YaRQ2XyolQL30EzTSo//z9SzANBgkqhkiG9w0BAQsFAAOCAQEAYEBoFkfnFo3bXKFWKsv0XJuwHqJL9csCP/gLofKnQtS3TOvjZoDzJUN4LhsXVgdSGMvRqOzm+3M+pGKMgLTSxRJzo9P6Aji+Yz2EuJnB8br3n8NA0VgYU8Fi3a8YQn80TsVD1XGwMADH45CuP1eGl87qDBKOInDjZqdUfy4oy9RU0LMeYmcI+Sfhy+NmuCQbiWqJRGXy2UzSWByMTsCVodTvZy84IOgu/5ZR8LrYPZJwR2UcnnNytGAMXOLRc3bgr07i5TelRS+KIz6HxzDmMTh89N1SyvNTBCVXVmaU6Avu5gMUTu79bZRknl7OedSyps9AsUSoPocZXun4IRZZUw" in
      let tob x = bytes_of_string (BatBase64.str_decode x) in
      let chain = List.map tob [c1; c2] in
-     validate_chain chain true (Some "test.ht.vc") "CAFile.pem"  
+     validate_chain chain true (Some "test.ht.vc") "pki/CAFile.pem"
 end                 
 
 module TestECDSACert = struct
@@ -1332,5 +1316,4 @@ let _ =
   simple_test "Certificate signature verify (ECDSA)  " TestECDSACert.test;
   simple_test "Certificate signature verify (DSA)    " TestDSACert.test;
   simple_test "Certificate signature verify (RSA)    " TestRSACert.test;
-  simple_test "Certificate load from PEM chain/key   " TestCertLoad.test;
   ()
