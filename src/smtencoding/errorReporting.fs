FStarLang/FStar

commit 0646bb9ba808d2ce0edcd9ea589850e5cb83ec3b

killing some warnings in the regression suite

ulib/prims.fst
@@ -112,10 +112,10 @@ inline let pure_return (a:Type) (x:a) (p:pure_post a) =
 inline let pure_bind_wp (r1:range) (a:Type) (b:Type)
                    (wp1:pure_wp a) (wp2: (a -> GTot (pure_wp b)))
                    (p : pure_post b) =
-     labeled r1 "push" unit 
-     /\ wp1 (fun (x:a) -> 
-             labeled r1 "pop" unit 
-	     /\ wp2 x p)
+     (* labeled r1 "push" unit  *)
+     (* /\  *)wp1 (fun (x:a) -> 
+             (* labeled r1 "pop" unit  *)
+	     (* /\ *) wp2 x p)
 inline let pure_if_then_else (a:Type) (p:Type) (wp_then:pure_wp a) (wp_else:pure_wp a) (post:pure_post a) =
      l_ITE p (wp_then post) (wp_else post)
 

examples/metatheory/StlcCbvDbParSubst.fst
@@ -38,7 +38,7 @@ let extend_gen x t g y = if y < x then g y
                          else if y = x then Some t
                          else g (y-1)
 
-opaque val weakening : n:nat -> #g:env -> #e:exp -> #t:typ -> t':typ ->
+irreducible val weakening : n:nat -> #g:env -> #e:exp -> #t:typ -> t':typ ->
       h:typing g e t -> Tot (typing (extend_gen n t' g) (shift_up_above n e) t)
       (decreases h)
 let rec weakening n #g #v #t t' h =
@@ -79,7 +79,7 @@ let rec progress #e #t h =
 (* Typing extensional follows directly from functional extensionality
    (it's also a special case of context invariance below) *)
 
-opaque val typing_extensional : #e:exp -> #g:env -> #t:typ ->
+irreducible val typing_extensional : #e:exp -> #g:env -> #t:typ ->
       h:(typing g e t) -> g':env{feq g g'} -> Tot (typing g' e t)
 let typing_extensional #e #g #t h _ = h
 
@@ -238,8 +238,8 @@ let subst_gen_elam x v t_y e' =
 
 val substitution_preserves_typing :
        x:var -> #e:exp -> #v:exp -> #t_x:typ -> #t:typ -> #g:env ->
-      =h1:typing empty v t_x ->
-      =h2:typing (extend_gen x t_x g) e t ->
+      $h1:typing empty v t_x ->
+      $h2:typing (extend_gen x t_x g) e t ->
        Tot (typing g (subst (sub_beta_gen x v) e) t) (decreases e)
 let rec substitution_preserves_typing x #e #v #t_x #t #g h1 h2 =
   match h2 with

examples/unit-tests/universes/NegativeTests.ShortCircuiting.fst
@@ -1,6 +1,6 @@
 module NegativeTests.ShortCircuiting
 
-type bad_p : bool -> Type
+assume type bad_p : bool -> Type
 val bad : x:int -> Tot (b:bool{bad_p b})
 let rec bad x = true || bad (x-1)
 

ulib/FStar.SquashProperties.fst
@@ -105,7 +105,7 @@ type u = p:Type -> Tot (squash (pow p))
 (* NS: FAILS TO CHECK BEYOND HERE ... TODO, revisit *)
 
 (* Bijection between U and (pow U) *)
-val f : u -> Tot (squash (pow u))
+assume val f : u -> Tot (squash (pow u))
 #set-options "--print_universes"
 (* let f x = x u  *) //fails here without a means of denoting universes
 

commit 8733a7ee5c07c4f6e277184e3095f6304836ccba

inadvertently broke error reporting in 0646bb9ba808d2ce0ed; restoring

src/smtencoding/errorReporting.fs
@@ -53,7 +53,12 @@ let fresh_label : ranges -> term -> labels -> term * labels =
 
       Returns the labeled query and the label terms that were added
 *)
-let label_goals use_env_msg r q : term * labels * ranges = 
+let label_goals use_env_msg  //when present, provides an alternate error message, usually "could not check implicit argument", or something like that
+                r            //the source range in which this query was asked
+                q : term     //the query, decorated with labels
+                  * labels   //the labels themselves
+                  * ranges   //not-relevant at the top-level; recursively this range represents the stack of source locations being visited
+                  = 
     let flag, msg_prefix = match use_env_msg with 
         | None -> false, ""
         | Some f -> true, f() in

ulib/prims.fst
@@ -112,10 +112,10 @@ inline let pure_return (a:Type) (x:a) (p:pure_post a) =
 inline let pure_bind_wp (r1:range) (a:Type) (b:Type)
                    (wp1:pure_wp a) (wp2: (a -> GTot (pure_wp b)))
                    (p : pure_post b) =
-     (* labeled r1 "push" unit  *)
-     (* /\  *)wp1 (fun (x:a) -> 
-             (* labeled r1 "pop" unit  *)
-	     (* /\ *) wp2 x p)
+     labeled r1 "push" unit
+     /\ wp1 (fun (x:a) -> 
+             labeled r1 "pop" unit
+	     /\ wp2 x p)
 inline let pure_if_then_else (a:Type) (p:Type) (wp_then:pure_wp a) (wp_else:pure_wp a) (post:pure_post a) =
      l_ITE p (wp_then post) (wp_else post)
 