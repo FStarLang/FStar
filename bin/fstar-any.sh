72f3ae4e45a3d01d7b25e1986d6a98b336394fb8
Daniel Fabian <daniel.fabian@integral-it.ch>
2016-07-22 14:12:17 +0100

    Adjust the Makefile according to #584. This together with the checked… (#591)
    * Adjust the Makefile according to #584. This together with the checked in parser.fsi makes the Linux build work out-of-the-box for the OCaml bootstrap.
    
    * Source rather than execute the fstar-any.sh wrapper.

---
+++ bin/fstar-any.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FSTAR=$(which fstar.exe)
+if (( $? != 0 )); then
+  echo "fstar.exe not found"
+elif file $FSTAR | grep Mono >/dev/null 2>&1; then
+ mono --debug $FSTAR "$@"
+else
+ $FSTAR "$@"
+fi
