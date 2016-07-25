2dc03dd67c60b0ac663d7e7fc31c7b4fd0dbb2d5
Nikhil Swamy <nikswamy@users.noreply.github.com>
2016-07-25 16:09:58 -0700

    adding hints files

--- src/gmake/fstar.mk
+++ src/gmake/fstar.mk
@@ -2,5 +2,7 @@ ifndef FSTAR_HOME
    $(error "Please define the `FSTAR_HOME` variable before including this makefile.")
 endif
 
-FSTAR_ALWAYS=$(shell cd $(FSTAR_HOME) && pwd)/bin/fstar.exe $(OTHERFLAGS)
+HINTS_ENABLED?=--use_hints
+
+FSTAR_ALWAYS=$(shell cd $(FSTAR_HOME) && pwd)/bin/fstar.exe $(OTHERFLAGS) $(HINTS_ENABLED)
 FSTAR=$(FSTAR_ALWAYS)
