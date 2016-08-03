02e67824c6d88e2fdb1435f8007719db8a5573a8
Jonathan Protzenko <protz@microsoft.com>
2016-08-03 15:05:41 -0700

    Blind fix for the build

--- bin/fstar-any.sh
+++ bin/fstar-any.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-FSTAR=$(dirname $(stat -f $0))/fstar.exe
-if (( $? != 0 )); then
+if which greadlink >/dev/null 2>&1; then
+  READLINK=greadlink
+else
+  READLINK=readlink
+fi
+FSTAR=$(dirname $($READLINK -f $0))/fstar.exe
+if [ ! -f "$FSTAR" ]; then
   echo "fstar.exe not found"
   exit 1
-elif file $FSTAR | grep Mono >/dev/null 2>&1; then
-  mono --debug $FSTAR "$@" || exit 1
+elif file "$FSTAR" | grep Mono >/dev/null 2>&1; then
+  mono --debug "$FSTAR" "$@" || exit 1
 else
-  $FSTAR "$@" || exit 1
+  "$FSTAR" "$@" || exit 1
 fi
