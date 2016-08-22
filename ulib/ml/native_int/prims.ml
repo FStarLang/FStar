6429b62aec3af2e8562bb7173ba19be2448735ce
Markulf Kohlweiss <markulf@microsoft.com>
2016-08-22 14:20:42 +0100

    adding (mod) to native_int/prims.ml

--- ulib/ml/native_int/prims.ml
+++ ulib/ml/native_int/prims.ml
@@ -9,7 +9,9 @@ include MkPrims.Make(struct
   let ( >= ) = ( >= )
   let ( < ) = ( < )
   let ( > ) = ( > )
+  let ( mod )     = ( mod )
   let op_Minus x = - x
+  let ( ~- )  = op_Minus
   let parse_int  = int_of_string
   let to_string = string_of_int
                     
