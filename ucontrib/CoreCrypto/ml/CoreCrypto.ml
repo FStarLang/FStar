c49bd9eed5fa000f6e69fd26162ffd2352725f48
Antoine Delignat-Lavaud <antdl@microsoft.com>
2016-10-12 11:47:13 +0100

    Clean up the build flags

--- ucontrib/CoreCrypto/ml/CoreCrypto.ml
+++ ucontrib/CoreCrypto/ml/CoreCrypto.ml
@@ -89,7 +89,7 @@ external ocaml_EVP_MD_sha384 : unit -> md = "ocaml_EVP_MD_sha384"
 external ocaml_EVP_MD_sha512 : unit -> md = "ocaml_EVP_MD_sha512"
 external ocaml_EVP_MD_block_size : md -> int = "ocaml_EVP_MD_block_size"
 external ocaml_EVP_MD_size : md -> int = "ocaml_EVP_MD_size"
-external ocaml_EVP_MD_CTX_create : md -> md_ctx = "ocaml_EVP_MD_CTX_create"
+external ocaml_EVP_MD_CTX_new : md -> md_ctx = "ocaml_EVP_MD_CTX_new"
 external ocaml_EVP_MD_CTX_fini   : md_ctx -> unit = "ocaml_EVP_MD_CTX_fini"
 external ocaml_EVP_MD_CTX_update : md_ctx -> string -> unit = "ocaml_EVP_MD_CTX_update"
 external ocaml_EVP_MD_CTX_final  : md_ctx -> string = "ocaml_EVP_MD_CTX_final"
@@ -104,7 +104,7 @@ let md_of_hash_alg h = match h with
 
 let hash (h:hash_alg) (b:bytes) =
   let md = md_of_hash_alg h in
-  let ctx = ocaml_EVP_MD_CTX_create(md) in
+  let ctx = ocaml_EVP_MD_CTX_new(md) in
   ocaml_EVP_MD_CTX_update ctx (string_of_bytes b);
   let h = ocaml_EVP_MD_CTX_final(ctx) in
   ocaml_EVP_MD_CTX_fini(ctx);
@@ -115,7 +115,7 @@ type hash_ctx = md_ctx (* exported name *)
 
 let digest_create (h:hash_alg) : hash_ctx = 
   let md = md_of_hash_alg h in
-  let ctx = ocaml_EVP_MD_CTX_create md in
+  let ctx = ocaml_EVP_MD_CTX_new md in
   ctx 
   
 let digest_update (ctx:md_ctx) (b:bytes) : unit = 
