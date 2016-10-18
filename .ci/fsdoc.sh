a5ad33aeb9eb6b0edccb934bc65c1a42824c09d2
Samin Ishtiaq <sishtiaq@microsoft.com>
2016-10-18 13:22:16 +0100

    md link.

--- .ci/fsdoc.sh
+++ .ci/fsdoc.sh
@@ -33,10 +33,12 @@ do
 done
 ## index.md 
 ### Add html to the module names. 
-sed -i '/[A-Za-z0-9]/s/\(.*\)/\1.html/' index.md
+sed -i '/[A-Za-z0-9]/s/\(.*\)/[\1](\1.html)/' index.md
 ### convert index.md to index.html.  
 pandoc index.md -f markdown -t html -o index.html
 popd
+
+exit 
 
 # push fstarlang.github.io with latest html.
 # The flow is described at https://github.com/blog/1270-easier-builds-and-deployments-using-git-over-https-and-oauth;
