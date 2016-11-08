3e1ab5f41c684a8516197b43205a3a699505d36d
Jean Karim <jkzinzindohoue@gmail.com>
2016-11-08 12:55:31 +0100

    openssl tests

--- examples/low-level/crypto/test_perf.c
+++ examples/low-level/crypto/test_perf.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include "testutils.h"
 #include "Crypto_AEAD.h"
+#include <openssl/evp.h>
 
 #define PLAINLEN (16*1024)
 #define AADLEN (12)
@@ -54,6 +55,69 @@ uint8_t ivBuffer[IVLEN] = {
 
 #define ROUNDS 1000
 
+#define AES_GCM 0
+#define CHACHA_POLY 1
+
+int openssl_aead_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *aad,
+                         int aad_len, unsigned char *key, unsigned char *iv,
+                         unsigned char *ciphertext, unsigned char *tag, int cipher){
+  EVP_CIPHER_CTX *ctx;
+  int len;
+  int ciphertext_len;
+  /* Create and initialise the context */
+  if(!(ctx = EVP_CIPHER_CTX_new())) return EXIT_FAILURE;
+  /* Initialise the encryption operation. */
+  if (cipher == AES_GCM) {
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL))
+      return EXIT_FAILURE;
+  } else {
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_chacha20_poly1305(), NULL, NULL, NULL))
+      return EXIT_FAILURE;
+  }
+
+  clock_t c1, c2;
+  double t1, t2;
+  unsigned long long a,b,d1,d2;
+  c1 = clock();
+  a = rdtsc();
+  for (int j = 0; j < ROUNDS; j++){
+    /* Set IV length if default 12 bytes (96 bits) is not appropriate */
+    if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, 16, NULL))
+      return EXIT_FAILURE;
+    /* Initialise key and IV */
+    if(1 != EVP_EncryptInit_ex(ctx, NULL, NULL, key, iv)) return EXIT_FAILURE;
+    /* Provide any AAD data. This can be called zero or more times as
+     * required
+     */
+    if(1 != EVP_EncryptUpdate(ctx, NULL, &len, aad, aad_len))
+      return EXIT_FAILURE;
+    /* Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+      return EXIT_FAILURE;
+    ciphertext_len = len;
+    /* Finalise the encryption. Normally ciphertext bytes may be written at
+     * this stage, but this does not occur in GCM mode
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) return EXIT_FAILURE;
+    ciphertext_len += len;
+    /* Get the tag */
+    if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, tag))
+      return EXIT_FAILURE;
+  }
+  b = rdtsc();
+  c2 = clock();
+  d1 = b - a;
+  printf("[openssl-%s] No of cycles for %d 2^14 bytes AEAD blocks: %llu (%llucycles/byte)\n", cipher==AES_GCM ? "aes-gcm" : "chacha-poly", ROUNDS, d1, d1/PLAINLEN/ROUNDS);
+  t1 = ((double)c2 - c1)/CLOCKS_PER_SEC;
+  printf("[openssl-%s] User time for %d 2^14 bytes AEAD blocks: %f (%fns/byte)\n", cipher==AES_GCM ? "aes-gcm" : "chacha-poly", ROUNDS, t1, t1*1000000/PLAINLEN/ROUNDS);
+  /* Clean up */
+  EVP_CIPHER_CTX_free(ctx);
+  return ciphertext_len;
+}
+
+
 void test_crypto_aead(){
   void *plain = malloc(PLAINLEN), *cipher = malloc(PLAINLEN+16);
   uint8_t mac[16];
@@ -91,7 +155,7 @@ void test_crypto_aead(){
     ak = (Prims_option__uint8_t_ ){ .tag = Prims_option__uint8_t__None, { .case_None = {  } } };
   Crypto_AEAD_Invariant_state_______
   st0 = { .x00 = FStar_HyperHeap_root, .x01 = log, .x02 = prf, .x03 = ak };
-  
+
   c1 = clock();
   a = rdtsc();
   for (int j = 0; j < ROUNDS; j++) Crypto_AEAD_encrypt(i, st0, iv, AADLEN, aad, PLAINLEN, plain, cipher);
@@ -101,6 +165,10 @@ void test_crypto_aead(){
   printf("No of cycles for %d 2^14 bytes AEAD blocks: %llu (%llucycles/byte)\n", ROUNDS, d1, d1/PLAINLEN/ROUNDS);
   t1 = ((double)c2 - c1)/CLOCKS_PER_SEC;
   printf("User time for %d 2^14 bytes AEAD blocks: %f (%fns/byte)\n", ROUNDS, t1, t1*1000000/PLAINLEN/ROUNDS);
+
+  openssl_aead_encrypt(plain, PLAINLEN, aad, AADLEN, key, ivBuffer, cipher, mac, AES_GCM);
+  openssl_aead_encrypt(plain, PLAINLEN, aad, AADLEN, key, ivBuffer, cipher, mac, CHACHA_POLY);
+
 }
 
 
