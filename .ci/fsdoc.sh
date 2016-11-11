81aa0d28532e98db8b0e4199665ddf1865505426
Samin Ishtiaq <sishtiaq@microsoft.com>
2016-11-11 10:24:58 +0000

    Add -s and --css flags to pandoc call.

--- .ci/fsdoc.sh
+++ .ci/fsdoc.sh
@@ -29,15 +29,15 @@ do
     f="${fe%.*}"     #
     md="${f}.md"
     html="${f}.html"
-    pandoc $md -f markdown -t html -o $html
+    pandoc $md -s --css=style.ccs -f markdown -t html -o $html
 done
 ## index.md 
 ### Add html to the module names. 
 sed -i '/[A-Za-z0-9]/s/\(.*\)/[\1](\1.html)/' index.md
 ### convert index.md to index.html.  
-pandoc index.md -f markdown -t html -s -o index.html
+pandoc index.md -s --css=style.ccs -f markdown -t html -o index.html
 popd
-
+exit
 # push fstarlang.github.io with latest html.
 # The flow is described at https://github.com/blog/1270-easier-builds-and-deployments-using-git-over-https-and-oauth;
 # $PAT is stored in the Build Defn. 
