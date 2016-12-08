9b55ac14ac7c5e19b9f46ad451faaffd2cb97fde
Jonathan Protzenko <protz@microsoft.com>
2016-12-07 16:12:29 -0800

    Adjust number of parameters following conversation with Bryan...

--- examples/low-level/crypto/spartan_stub.c
+++ examples/low-level/crypto/spartan_stub.c
@@ -6,16 +6,16 @@
 #define STDCALL
 #endif
 
-extern void STDCALL KeyExpansionStdcall(const void *key_ptr, void *expanded_key_ptr);
+extern void STDCALL KeyExpansionStdcall(const void *key_ptr, void *expanded_key_ptr, void *placeholder);
 extern void STDCALL AES128EncryptOneBlockStdcall(void *output_ptr, const void *input_ptr, const void *expanded_key_ptr);
 
 void Spartan_keyExpansion(uint8_t *k, uint8_t *w, uint8_t *sb)
 {
-  KeyExpansionStdcall(k, w);
+  KeyExpansionStdcall(k, w, sb);
 }
 
 void Spartan_cipher(uint8_t *out, uint8_t *in, uint8_t *w, uint8_t *sb)
 {
-  AES128EncryptOneBlockStdcall(out, in, w);
+  AES128EncryptOneBlockStdcall(out, in, w, sb);
 }
 
