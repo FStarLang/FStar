e0bc763a1ff7c846420b9ebdb81735d4913d9ba4
Christoph M. Wintersteiger <cwinter@microsoft.com>
2017-03-08 18:10:50 +0000

    Add #ifdef to set a custom stack size for managed F*

--- src/fstar/FStar.Top.fs
+++ src/fstar/FStar.Top.fs
@@ -1,7 +1,12 @@
 #light "off"
 module FStar.Top
 open FStar.All
-let _ = 
+let _ =
+#if CUSTOM_STACK_SIZE
+    let t = (new System.Threading.Thread(FStar.Main.main, 1048576 * 32)) in
+    t.Start() ; t.Join() ;
+#else
     let _ = FStar.Main.main () in
+#endif
     printfn "done"
 
