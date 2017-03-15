698c948c6048ae3dfe564e6d791c651ba806c46d
Samin Ishtiaq <sishtiaq@microsoft.com>
2017-03-15 18:15:49 +0000

    FST_FILES: use GLOBIGNORE to exclude broken files.

--- .ci/fsdoc.sh
+++ .ci/fsdoc.sh
@@ -11,7 +11,7 @@ echo Running fsdoc in `pwd`
 
 # SI: we assume F* has been built and we are in it. 
 
-# un-set this if you won't push docs to fstarlang.
+# un-set this if you won't push the htmls to fstarlang.
 # Useful for local testing. 
 FSTARLANG_PUSH=true
 
@@ -23,88 +23,13 @@ mkdir -p "$FSDOC_ODIR"
 
 # fsdoc ulib to the output dir.  
 pushd ulib
-# Get fst, fsti files (files are name-sorted by default). 
-# SI: Used to be this:
-#FST_FILES=(*.fst *.fsti)
-#     but FStar.IndefiniteDescription.fst fails to parse;
-#         prims.fst has a lower-case filename but an upper-case modulename
-#           (pandoc looks for Prims.md but there's only a prims.md). 
-FST_FILES=(
-FStar.All.fst \
-FStar.Array.fst \
-FStar.Axiomatic.Array.fst \
-FStar.BitVector.fst \
-FStar.Buffer.fst \
-FStar.Buffer.Quantifiers.fst \
-FStar.Bytes.fst \
-FStar.Classical.fst \
-FStar.Constructive.fst \
-FStar.Crypto.fst \
-FStar.DependentMap.fst \
-FStar.ErasedLogic.fst \
-FStar.Fin.fst \
-FStar.FunctionalExtensionality.fst \
-FStar.Ghost.fst \
-FStar.Heap.fst \
-FStar.HyperHeap.fst \
-FStar.HyperStack.fst \
-FStar.Int.Cast.fst \
-FStar.Int.fst \
-FStar.Int128.fst \
-FStar.Int16.fst \
-FStar.Int31.fst \
-FStar.Int32.fst \
-FStar.Int63.fst \
-FStar.Int64.fst \
-FStar.Int8.fst \
-FStar.Integers.fst \
-FStar.List.fst \
-FStar.List.Tot.Base.fst \
-FStar.List.Tot.fst \
-FStar.List.Tot.Properties.fst \
-FStar.Map.fst \
-FStar.MarkovsPrinciple.fst \
-FStar.Math.Lemmas.fst \
-FStar.Math.Lib.fst \
-FStar.Monotonic.RRef.fst \
-FStar.Monotonic.Seq.fst \
-FStar.MRef.fst \
-FStar.Mul.fst \
-FStar.OrdMap.fst \
-FStar.OrdMapProps.fst \
-FStar.OrdSet.fst \
-FStar.OrdSetProps.fst \
-FStar.PredicateExtensionality.fst \
-FStar.PropositionalExtensionality.fst \
-FStar.Reader.fst \
-FStar.Relational.Comp.fst \
-FStar.Relational.Relational.fst \
-FStar.Relational.State.fst \
-FStar.Seq.Base.fst \
-FStar.Seq.fst \
-FStar.Seq.Properties.fst \
-FStar.Set.fst \
-FStar.Squash.fst \
-FStar.SquashEffect.fst \
-FStar.SquashProperties.fst \
-FStar.ST.fst \
-FStar.StrongExcludedMiddle.fst \
-FStar.Struct.fst \
-FStar.StructNG.fst \
-FStar.Tcp.fst \
-FStar.TSet.fst \
-FStar.TwoLevelHeap.fst \
-FStar.UInt.fst \
-FStar.UInt128.fst \
-FStar.UInt16.fst \
-FStar.UInt31.fst \
-FStar.UInt32.fst \
-FStar.UInt63.fst \
-FStar.UInt64.fst \
-FStar.UInt8.fst \
-FStar.Universe.fst \
-FStar.Util.fst \
-)
+# Get fst, fsti files (files are name-sorted by default) except: 
+#  FStar.IndefiniteDescription.fst fails to parse;
+#  prims.fst has a lower-case filename but an upper-case modulename
+#    (pandoc looks for Prims.md but there's only a prims.md).
+GLOBIGNORE="prims.fst:FStar.IndefiniteDescription.fst"
+FST_FILES=(*.fst *.fsti)
+unset GLOBIGNORE
 
 ../bin/fstar-any.sh --odir "../$FSDOC_ODIR" --doc ${FST_FILES[*]} 
 popd
