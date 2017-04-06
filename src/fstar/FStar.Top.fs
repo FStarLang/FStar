4aa60c5f09cfeb1983769ef4dd8c18b07b35c3cd
Christoph M. Wintersteiger <cwinter@microsoft.com>
2017-04-06 17:04:23 +0100

    Removed #if to set a custom stack size by default (in .NET)

--- src/fstar/FStar.Top.fs
+++ src/fstar/FStar.Top.fs
@@ -2,11 +2,6 @@
 module FStar.Top
 open FStar.All
 let _ =
-#if CUSTOM_STACK_SIZE
-    let t = (new System.Threading.Thread(FStar.Main.main, 1048576 * 32)) in
+    let t = (new System.Threading.Thread(FStar.Main.main, 1024 * 1024 * 32)) in
     t.Start() ; t.Join() ;
-#else
-    let _ = FStar.Main.main () in
-#endif
     printfn "done"
-
