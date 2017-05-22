e969ad24a5ce53dc089ddfd0b86fa16a8fb12a41
Aseem Rastogi <aseemr@microsoft.com>
2017-05-22 15:01:59 +0530

    ***NO_CI*** cp

--- src/ocaml-output/FStar_Pervasives.ml
+++ src/ocaml-output/FStar_Pervasives.ml
@@ -1,16 +1,30 @@
 open Prims
+type 'Aheap st_pre_h = 'Aheap -> Obj.t
+type ('Aheap,'Aa) st_post_h = 'Aa -> 'Aheap -> Obj.t
+type ('Aheap,'Aa) st_wp_h = Prims.unit -> 'Aheap st_pre_h
+type ('Aheap,'Aa,'Ax,'Ap,'Auu____49) st_return = 'Ap
+type ('Aheap,'Ar1,'Aa,'Ab,'Awp1,'Awp2,'Ap,'Ah0) st_bind_wp = 'Awp1
+type ('Aheap,'Aa,'Ap,'Awp_then,'Awp_else,'Apost,'Ah0) st_if_then_else =
+  Prims.unit
+type ('Aheap,'Aa,'Awp,'Apost,'Ah0) st_ite_wp = Prims.unit
+type ('Aheap,'Aa,'Awp1,'Awp2) st_stronger = Prims.unit
+type ('Aheap,'Aa,'Ab,'Awp,'Ap,'Ah) st_close_wp = Prims.unit
+type ('Aheap,'Aa,'Ap,'Awp,'Aq,'Ah) st_assert_p = Prims.unit
+type ('Aheap,'Aa,'Ap,'Awp,'Aq,'Ah) st_assume_p = Prims.unit
+type ('Aheap,'Aa,'Ap,'Ah) st_null_wp = Prims.unit
+type ('Aheap,'Aa,'Awp) st_trivial = Prims.unit
 type 'Aa result =
   | V of 'Aa
   | E of Prims.exn
   | Err of Prims.string
 let uu___is_V projectee =
-  match projectee with | V v -> true | uu____27 -> false
+  match projectee with | V v -> true | uu____378 -> false
 let __proj__V__item__v projectee = match projectee with | V v -> v
 let uu___is_E projectee =
-  match projectee with | E e -> true | uu____52 -> false
+  match projectee with | E e -> true | uu____403 -> false
 let __proj__E__item__e projectee = match projectee with | E e -> e
 let uu___is_Err projectee =
-  match projectee with | Err msg -> true | uu____77 -> false
+  match projectee with | Err msg -> true | uu____428 -> false
 let __proj__Err__item__msg projectee = match projectee with | Err msg -> msg
 type ex_pre = Obj.t
 type 'Aa ex_post = 'Aa result -> Obj.t
@@ -26,12 +40,12 @@ type ('Aa,'Aq,'Awp,'Ap) ex_assume_p = Prims.unit
 type ('Aa,'Ap) ex_null_wp = Prims.unit
 type ('Aa,'Awp) ex_trivial = 'Awp
 type ('Aa,'Awp,'Ap) lift_div_exn = 'Awp
-let raise uu____349 = failwith "Not yet implemented:raise"
+let raise uu____700 = failwith "Not yet implemented:raise"
 type 'Ah all_pre_h = 'Ah -> Obj.t
 type ('Ah,'Aa) all_post_h = 'Aa result -> 'Ah -> Obj.t
 type ('Ah,'Aa) all_wp_h = Prims.unit -> 'Ah all_pre_h
 type ('Aheap,'Aa,'Awp,'Apost,'Ah0) all_ite_wp = Prims.unit
-type ('Aheap,'Aa,'Ax,'Ap,'Auu____454) all_return = 'Ap
+type ('Aheap,'Aa,'Ax,'Ap,'Auu____805) all_return = 'Ap
 type ('Aheap,'Ar1,'Aa,'Ab,'Awp1,'Awp2,'Ap,'Ah0) all_bind_wp = 'Awp1
 type ('Aheap,'Aa,'Ap,'Awp_then,'Awp_else,'Apost,'Ah0) all_if_then_else =
   Prims.unit
@@ -43,15 +57,15 @@ type ('Aheap,'Aa,'Ap,'Ah0) all_null_wp = Prims.unit
 type ('Aheap,'Aa,'Awp) all_trivial = Prims.unit
 type 'Aa inversion = Prims.unit
 let allow_inversion = ()
-let invertOption uu____714 = ()
+let invertOption uu____1065 = ()
 type ('a,'b) either =
   | Inl of 'a
   | Inr of 'b
 let uu___is_Inl projectee =
-  match projectee with | Inl v -> true | uu____746 -> false
+  match projectee with | Inl v -> true | uu____1097 -> false
 let __proj__Inl__item__v projectee = match projectee with | Inl v -> v
 let uu___is_Inr projectee =
-  match projectee with | Inr v -> true | uu____780 -> false
+  match projectee with | Inr v -> true | uu____1131 -> false
 let __proj__Inr__item__v projectee = match projectee with | Inr v -> v
 type ('a,'b,'c) tuple3 =
   | Mktuple3 of 'a* 'b* 'c
