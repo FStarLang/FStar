31e818b1772eb36757a2ac1a56123e0235158313
Christoph M. Wintersteiger <cwinter@microsoft.com>
2017-06-09 18:57:21 +0100

    Fix for git commit/file timestamps in z3_nightly.py

--- .scripts/z3_nightly.py
+++ .scripts/z3_nightly.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import io
 import subprocess
 import sys
 import time
@@ -13,10 +14,11 @@ import zipfile
 import platform
 
 PLATFORMS = [ "x64-osx", "x86-ubuntu", "x64-ubuntu", "x64-debian", "x86-win", "x64-win" ]
+REQUIRE_ALL_PLATFORMS = True
 
 FSTAR_BIN_URL = "https://github.com/FStarLang/binaries.git"
 FSTAR_BIN_LOCAL = os.path.join("nightly", "fstar-binaries")
-FSTAR_BIN_SUBDIR = "z3-latest"
+FSTAR_BIN_SUBDIR = "z3-tested"
 FSTAR_BIN_RBRANCH = "origin/master"
 
 Z3_BIN_URL = "https://github.com/Z3Prover/bin.git"
@@ -59,7 +61,7 @@ def get_platform():
     return z3bn, z3s, z3a
 
 def mk_args(cmd):
-    css = cmd.split(' ')
+    css = cmd.split(" ")
     in_string = False
     cs = []
     cur = ""
@@ -90,17 +92,28 @@ def call_logged(cmd, log, checked=True):
         log.write("Error code: %d\n" % ec)        
         raise Z3NightlyException("Command failed.")
 
+def call_with_output(cmd):
+    cs = mk_args(cmd)
+    # log.write(">>>>> " + " ".join(cs) + " <<<<<\n")
+    p = subprocess.Popen(cs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if err is None or err == "":
+        return out
+    else:
+        return out + " " + err
+
 def update_git(url, branch, dir, subdir, log, quiet=False):
     q = "--quiet" if quiet else ""
-    if not os.path.isdir(dir):        
+    v = "--verbose" if not quiet else ""
+    if not os.path.isdir(dir):                
         call_logged("git clone %s %s %s" % (q, url, dir), log)
     else:
         prev_wd = os.getcwd()
         os.chdir(dir)
         call_logged("git reset %s --hard %s" % (q, branch), log)
         call_logged("git clean %s -f" % (q), log)
-        q = "--quiet" if quiet else "--verbose"
-        call_logged("git pull %s -s recursive -Xtheirs" % (q), log)
+        call_logged("git pull %s -s recursive -Xtheirs" % (v), log)
+        call_logged("git reset %s --hard %s" % (q, branch), log)
         os.chdir(prev_wd)
     sp = os.path.join(dir, subdir)
     if not os.path.isdir(sp):
@@ -112,23 +125,37 @@ def find_latest_binary(dir, pattern, log):
         m = pattern.match(f)
         if m is not None:
             fp = os.path.join(dir, f)
-            mt = os.path.getmtime(fp)
+            mt = call_with_output("git log -n 1 --date-order --date=raw --pretty=format:\"%cd\"").strip("\"").split(" ")[0]
             if best_offer == None or mt > best_offer[1]:
-                version, githash, bitness, platform, pversion = m.groups()
-                best_offer = (fp, mt, version, githash, bitness, platform, pversion)
+                version, git_hash, bitness, platform, pversion = m.groups()
+                best_offer = (fp, mt, version, git_hash, bitness, platform, pversion)
     return best_offer
+
+def find_specific_binary(dir, pattern, version, git_hash, log):
+    for f in os.listdir(dir):
+        m = pattern.match(f)
+        if m is not None:
+            fp = os.path.join(dir, f)
+            fversion, fgit_hash, bitness, platform, pversion = m.groups()
+            if (version == None or fversion == version) and fgit_hash == git_hash:
+                return (fp, None, version, git_hash, bitness, platform, pversion)
+    return None
+
+def get_platform_files(from_path, version, git_hash, platforms):
+    res = []
+    for pf in platforms:
+        fnpat = "z3-" + version + "." + git_hash + "-" + pf + "*.zip"
+        pp = os.path.join(from_path, fnpat)
+        matching_files = glob.glob(pp)
+        if REQUIRE_ALL_PLATFORMS == True and len(matching_files) == 0:
+            raise Z3NightlyException("No platform files for '%s' with version=%s and git_hash=%s." % (pf, version, git_hash))
+        elif len(matching_files) > 0:
+            res.append(matching_files[0])
+    return res
 
 def pick_better(old, new, from_path, pattern, platforms):
     if (old is None and new is not None) or (new[3] != old[3] and new[1] > old[1]):
-        files = []
-        for pf in platforms:
-            fnpat = "z3-" + new[2] + "." + new[3] + "-" + pf + "*.zip"
-            pp = os.path.join(from_path, fnpat)
-            matching_files = glob.glob(pp)
-            if len(matching_files) == 0:
-                return None
-            files.append(matching_files[0])
-        return files
+        return get_platform_files(from_path, new[2], new[3], platforms)
     return None
         
 def wipe_old_pkgs(to_repo, to_subdir, pattern, log):
@@ -151,27 +178,6 @@ def add_new_pkgs(files, to_repo, to_subdir, pattern, log):
     call_logged('git push -v --force', log)
     os.chdir(prev_dir)
 
-def push_latest(log):
-    wd = os.getcwd()
-    try:
-        update_git(FSTAR_BIN_URL, FSTAR_BIN_RBRANCH, FSTAR_BIN_LOCAL, FSTAR_BIN_SUBDIR, log)
-        update_git(Z3_BIN_URL, Z3_BIN_RBRANCH, Z3_BIN_LOCAL, Z3_BIN_SUBDIR, log)
-        old_bin_path = os.path.join(FSTAR_BIN_LOCAL, FSTAR_BIN_SUBDIR)
-        new_bin_path = os.path.join(Z3_BIN_LOCAL, Z3_BIN_SUBDIR)
-        best_old = find_latest_binary(old_bin_path, Z3_PKG_NAME_PAT, log)
-        best_new = find_latest_binary(new_bin_path, Z3_PKG_NAME_PAT, log)
-        better = pick_better(best_old, best_new, new_bin_path, Z3_PKG_NAME_PAT, PLATFORMS)
-        if better is not None:
-            wipe_old_pkgs(FSTAR_BIN_LOCAL, FSTAR_BIN_SUBDIR, Z3_PKG_NAME_PAT, log)
-            add_new_pkgs(better, FSTAR_BIN_LOCAL, FSTAR_BIN_SUBDIR, Z3_PKG_NAME_PAT, log)
-        os.chdir(wd)
-        return 0
-    except Exception as ex:
-        os.chdir(wd)
-        traceback.print_exc(log)
-        log.close()
-        return 1
-
 def empty(d):
     if not os.path.isdir(d):
         os.mkdir(d)
@@ -183,7 +189,39 @@ def empty(d):
         else:
             os.remove(ofp)
 
-def get(log, binary_name, platform, bitness, Tested=True):
+def push(version, git_hash, log):
+    wd = os.getcwd()
+    try:
+        old_bin_path = os.path.join(FSTAR_BIN_LOCAL, FSTAR_BIN_SUBDIR)
+        new_bin_path = os.path.join(Z3_BIN_LOCAL, Z3_BIN_SUBDIR)
+        update_git(FSTAR_BIN_URL, FSTAR_BIN_RBRANCH, FSTAR_BIN_LOCAL, FSTAR_BIN_SUBDIR, log)
+
+        if git_hash == None:            
+            update_git(Z3_BIN_URL, Z3_BIN_RBRANCH, Z3_BIN_LOCAL, Z3_BIN_SUBDIR, log)
+            best_old = find_latest_binary(old_bin_path, Z3_PKG_NAME_PAT, log)
+            best_new = find_latest_binary(new_bin_path, Z3_PKG_NAME_PAT, log)
+            better = pick_better(best_old, best_new, new_bin_path, Z3_PKG_NAME_PAT, PLATFORMS)
+            if better is not None:
+                wipe_old_pkgs(FSTAR_BIN_LOCAL, FSTAR_BIN_SUBDIR, Z3_PKG_NAME_PAT, log)
+                add_new_pkgs(better, FSTAR_BIN_LOCAL, FSTAR_BIN_SUBDIR, Z3_PKG_NAME_PAT, log)
+        else:
+            sb = find_specific_binary(new_bin_path, Z3_PKG_NAME_PAT, version, git_hash, log)
+            if sb == None:
+                raise Z3NightlyException("Z3 packages with git hash '%s' not found." % git_hash)
+            else:
+                pfiles = get_platform_files(new_bin_path, version, git_hash, PLATFORMS)
+                wipe_old_pkgs(FSTAR_BIN_LOCAL, FSTAR_BIN_SUBDIR, Z3_PKG_NAME_PAT, log)
+                add_new_pkgs(pfiles, FSTAR_BIN_LOCAL, FSTAR_BIN_SUBDIR, Z3_PKG_NAME_PAT, log)
+            pass
+        os.chdir(wd)
+        return 0
+    except Exception as ex:
+        os.chdir(wd)
+        traceback.print_exc(log)
+        log.close()
+        return 1
+
+def get(binary_name, platform, bitness, log, Tested=True):
     wd = os.getcwd()
     try:
         if Tested:
@@ -198,7 +236,7 @@ def get(log, binary_name, platform, bitness, Tested=True):
             m = Z3_PKG_NAME_PAT.match(f)
             if m is not None:
                 fp = os.path.join(bsdir, f)
-                version, githash, fbitness, fplatform, pversion = m.groups()
+                version, git_hash, fbitness, fplatform, pversion = m.groups()
                 if fplatform == platform and fbitness == bitness:
                     zfn = os.path.join(bsdir, f)
                     # print("Extracting Z3 from %s" % zfn)
@@ -226,7 +264,7 @@ def get(log, binary_name, platform, bitness, Tested=True):
         return 1
 
 def print_help():
-    print("Usage: %s (get-tested|get-latest|push-latest)" % sys.argv[0])
+    print("Usage: %s (get-tested|get-latest|push)" % sys.argv[0])
 
 if  __name__ =='__main__':    
     if len(sys.argv) < 2:
@@ -242,16 +280,22 @@ if  __name__ =='__main__':
                 pfm = sys.argv[2]
             if len(sys.argv) >= 4:
                 bits = sys.argv[3]
-            r = get(log, bn, pfm, bits, Tested=True)
+            r = get(bn, pfm, bits, log, Tested=True)
         elif op == "get-latest":
             bn, pfm, bits = get_platform()
             if len(sys.argv) >= 3:
                 pfm = sys.argv[2]
             if len(sys.argv) >= 4:
                 bits = sys.argv[3]
-            r = get(log, bn, pfm, bits, Tested=False)
-        elif op == "push-latest":
-            r = push_latest()
+            r = get(bn, pfm, bits, log, Tested=False)
+        elif op == "push":
+            version = None
+            git_hash = None
+            if len(sys.argv) >= 3:
+                version = sys.argv[2]
+            if len(sys.argv) >= 4:
+                git_hash = sys.argv[3]
+            r = push(version, git_hash, log)
         else:
             print("Error: Unknown operation '" + op + "'")
             print_help()
