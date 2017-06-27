2b77ddee1bd09c840316b66a2f944a263d61191e
Jonathan Protzenko <protz@microsoft.com>
2017-06-27 14:45:21 -0700

    Trim down the number of OCaml-version specific files

--- src/ocaml-output/check_version.ml
+++
@@ -1,6 +0,0 @@
-let _ =
-  let versions = [ "4.04.1"; "4.02.3" ] in
-  if not (List.mem Sys.ocaml_version versions) then begin
-    Printf.printf "The only supported OCaml versions are: %s\nPlease upgrade!\n" (String.concat ", " versions);
-    exit 1
-  end
