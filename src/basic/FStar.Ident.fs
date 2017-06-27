d44cdef59a352d07713a23beaf28eda191cdee2f
Jonathan Protzenko <protz@microsoft.com>
2017-06-27 15:30:55 -0700

    Lexer hack to make the compiler sources valid F\#

--- src/basic/FStar.Ident.fs
+++ src/basic/FStar.Ident.fs
@@ -5,17 +5,17 @@ open FStar.ST
 open FStar.All
 open FStar.Range
 
-[@ PpxDeriving ]
+///[@ PpxDeriving ]
 type ident = {idText:string;
               idRange:Range.range}
 
-[@ PpxDeriving ]
+///[@ PpxDeriving ]
 type lident = {ns:list<ident>; //["FStar"; "Basic"]
                ident:ident;    //"lident"
                nsstr:string; // Cached version of the namespace
                str:string} // Cached version of string_of_lid
 
-[@ PpxDeriving ]
+///[@ PpxDeriving ]
 type lid = lident
 
 let mk_ident (text,range) = {idText=text; idRange=range}
