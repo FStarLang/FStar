9d280af3288b19351173aec3f468554008fa5ff5
Guido Martínez <mtzguido@gmail.com>
2017-06-27 11:29:19 -0700

    parser: fix strcat lid

--- src/parser/FStar.Parser.Const.fs
+++ src/parser/FStar.Parser.Const.fs
@@ -112,7 +112,7 @@ let assert_lid      = pconst  "_assert"
 let list_append_lid = p2l ["FStar"; "List"; "append"]
 (* list_tot_append_lid is used to desugar @ everywhere else *)
 let list_tot_append_lid = p2l ["FStar"; "List"; "Tot"; "Base"; "append"]
-let strcat_lid      = p2l ["Prims"; "strcat"]
+let strcat_lid      = p2l ["FStar"; "String"; "strcat"]
 let let_in_typ      = p2l ["Prims"; "Let"]
 let string_of_int_lid = p2l ["Prims"; "string_of_int"]
 let string_of_bool_lid = p2l ["Prims"; "string_of_bool"]
