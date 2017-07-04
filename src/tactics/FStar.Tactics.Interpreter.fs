eae4e7b449a5e2dd54318772680bd3fe10f08f15
Guido Martínez <mtzguido@gmail.com>
2017-07-04 14:23:21 -0700

    tactics: fix hook bug
    wrong environment being used, and allowed to prove false..

--- src/tactics/FStar.Tactics.Interpreter.fs
+++ src/tactics/FStar.Tactics.Interpreter.fs
@@ -284,7 +284,7 @@ let rec traverse (f: pol -> Env.env -> term -> term * list<goal>) (pol:pol) (e:E
 
         | Tm_app ({ n = Tm_fvar fv }, [(p,_); (q,_)]) when S.fv_eq_lid fv PC.imp_lid ->
                let x = S.new_bv None p in
-               let (p', gs1) = traverse f (flip pol) (Env.push_bv e x) p in
+               let (p', gs1) = traverse f (flip pol)  e                p in
                let (q', gs2) = traverse f       pol  (Env.push_bv e x) q in
                ((U.mk_imp p' q').n, gs1@gs2)
 
