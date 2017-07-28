4c9a8561f392774aaae3cf2dd661f4eed3b5a358
Guido Martínez <mtzguido@gmail.com>
2017-07-27 21:14:28 -0300

    fix silly mistake

--- src/basic/FStar.Order.fs
+++ src/basic/FStar.Order.fs
@@ -5,44 +5,30 @@ module FStar.Order
 type order = | Lt | Eq | Gt
 
 // Some derived checks
-val ge : order -> bool
-let ge o = o <> Lt
-
-val le : order -> bool
-let le o = o <> Gt
-
-val ne : order -> bool
-let ne o = o <> Eq
+let ge (o : order) : bool = o <> Lt
+let le (o : order) : bool = o <> Gt
+let ne (o : order) : bool = o <> Eq
 
 // Just for completeness and consistency...
-val gt : order -> bool
-let gt o = o = Gt
-
-val lt : order -> bool
-let lt o = o = Lt
-
-val eq : order -> bool
-let eq o = o = Eq
+let gt (o : order) : bool = o = Gt
+let lt (o : order) : bool = o = Lt
+let eq (o : order) : bool = o = Eq
 
 // Lexicographical combination, thunked to be lazy
-val lex : order -> (unit -> order) -> order
-let lex o1 o2 =
+let lex (o1 : order) (o2 : unit -> order) : order =
     match o1, o2 with
     | Lt, _ -> Lt
     | Eq, _ -> o2 ()
     | Gt, _ -> Gt
 
-val order_from_int : int -> order
-let order_from_int i =
+let order_from_int (i : int) : order =
     if i < 0 then Lt
     else if i = 0 then Eq
     else Gt
 
-val compare_int : int -> int -> order
-let compare_int i j = order_from_int (i - j)
+let compare_int (i : int) (j : int) : order = order_from_int (i - j)
 
-val compare_list : ('a -> 'a -> order) -> list 'a -> list 'a -> order
-let rec compare_list f l1 l2 =
+let rec compare_list (f : 'a -> 'a -> order) (l1 : list<'a>) (l2 : list<'a>) : order =
     match l1, l2 with
     | [], [] -> Eq
     | [], _ -> Lt
