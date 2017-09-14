c8142496acb447dedd99a9275a702d3ef42d4b51
Guido Martínez <mtzguido@gmail.com>
2017-09-14 09:26:28 -0300

    tactics: laxer check on `pointwise`

--- src/tactics/FStar.Tactics.Basic.fs
+++ src/tactics/FStar.Tactics.Basic.fs
@@ -750,8 +750,8 @@ let rec tac_bottom_fold_env (f : env -> term -> tac<term>) (env : env) (t : term
  *)
 let pointwise_rec (ps : proofstate) (tau : tac<unit>) opts (env : Env.env) (t : term) : tac<term> =
     let t, lcomp, g = TcTerm.tc_term env t in
-    if not (U.is_total_lcomp lcomp) || not (Rel.is_trivial g) then
-        ret t // Don't do anything for possibly impure terms
+    if (* not (U.is_total_lcomp lcomp) || *) not (Rel.is_trivial g) then
+        ret t
     else
         let typ = lcomp.res_typ in
         bind (new_uvar env typ) (fun ut ->
