463f96c380821d0a03bccf153d8459935a0d9182
Nikhil Swamy <nikswamy@users.noreply.github.com>
2017-09-29 10:59:01 -0700

    turn on --use_hint_hashes by default in our makefiles

--- src/gmake/fstar.mk
+++ src/gmake/fstar.mk
@@ -2,7 +2,7 @@ ifndef FSTAR_HOME
    $(error "Please define the `FSTAR_HOME` variable before including this makefile.")
 endif
 
-HINTS_ENABLED?=--use_hints
+HINTS_ENABLED?=--use_hints --use_hint_hashes
 
 ifdef Z3
 OTHERFLAGS+=--smt $(Z3)
