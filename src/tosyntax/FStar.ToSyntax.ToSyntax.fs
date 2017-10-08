cc93e01421ce5859df2e2b4f98b4b724463bb437
Ash <ashermanning@yahoo.co.uk>
2017-10-08 15:36:56 +0300

    forgot to add FStar.ToSyntax.ToSyntax.fs

--- src/tosyntax/FStar.ToSyntax.ToSyntax.fs
+++ src/tosyntax/FStar.ToSyntax.ToSyntax.fs
@@ -636,8 +636,6 @@ and desugar_typ env e : S.term =
     desugar_term_maybe_top false env e
 
 and desugar_machine_integer env repr (signedness, width) range =
-  let lower, upper = FStar.Const.bounds signedness width in
-  let value = FStar.Util.int_of_string (FStar.Util.ensure_decimal repr) in
   let tnm = "FStar." ^
     (match signedness with | Unsigned -> "U" | Signed -> "") ^ "Int" ^
     (match width with | Int8 -> "8" | Int16 -> "16" | Int32 -> "32" | Int64 -> "64")
@@ -646,7 +644,7 @@ and desugar_machine_integer env repr (signedness, width) range =
   //and coerce them to the appropriate type using the internal coercion
   // __uint_to_t or __int_to_t
   //Rather than relying on a verification condition to check this trivial property
-  if not (lower <= value && value <= upper)
+  if not (FStar.Util.within_bounds repr signedness width)
   then raise (Error(BU.format2 "%s is not in the expected range for %s"
                                repr tnm,
                     range));
