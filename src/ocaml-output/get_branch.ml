5e0b9506158a6ddcfa823a6a998d7bc4b287a02d
Jonathan Protzenko <protz@microsoft.com>
2017-10-23 11:11:15 -0700

    Fix for non-Windows OSes ... make get_branch return a deterministic line ending.

--- src/ocaml-output/get_branch.ml
+++ src/ocaml-output/get_branch.ml
@@ -2,4 +2,4 @@ let _ =
   let branch = String.sub Sys.ocaml_version 0 (String.rindex Sys.ocaml_version '.') ^ ".X" in
   if not (List.mem branch ["4.02.X"; "4.04.X"; "4.05.X"]) then
     failwith ("Unsupported version of OCaml:" ^ Sys.ocaml_version);
-  print_endline branch
+  Printf.printf "%s\n" branch
