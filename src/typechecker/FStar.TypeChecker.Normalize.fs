b7b66abda4e1b2d52c5399b5c6dd852f16d77041
Nikhil Swamy <nikswamy@users.noreply.github.com>
2017-12-27 09:45:02 -0800

    ***NO_CI*** reconcile non-informative type change with erasure of universes in extraction

--- src/typechecker/FStar.TypeChecker.Normalize.fs
+++ src/typechecker/FStar.TypeChecker.Normalize.fs
@@ -1698,7 +1698,7 @@ and norm_comp : cfg -> env -> comp -> comp =
 *)
 and ghost_to_pure_aux cfg env c =
     let norm t =
-        norm ({cfg with steps=[Eager_unfolding; UnfoldUntil Delta_constant; AllowUnboundUniverses];
+        norm ({cfg with steps=[Eager_unfolding; UnfoldUntil Delta_constant; AllowUnboundUniverses; EraseUniverses];
                         delta_level=[Unfold Delta_constant]}) env [] t in
     let non_info t = non_informative (norm t) in
     match c.n with
