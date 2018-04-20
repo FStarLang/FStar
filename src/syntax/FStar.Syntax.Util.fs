773e9594d5ba22f8a12cc271db600312471a5a2d
Aseem Rastogi <aseemr@microsoft.com>
2018-04-20 17:15:59 +0530

    making a thunk for the comp

--- src/syntax/FStar.Syntax.Util.fs
+++ src/syntax/FStar.Syntax.Util.fs
@@ -380,8 +380,7 @@ let set_result_typ c t = match c.n with
 
 let set_result_typ_lc (lc:lcomp) (t:typ) :lcomp =
   let c = set_result_typ (lcomp_comp lc) t in
-  lc.comp_thunk := Inr c;  //AR: do we really need a thunk here?
-  { lc with res_typ = t }
+  mk_lcomp lc.eff_name t lc.cflags (fun _ -> c)
 
 let is_trivial_wp c =
   comp_flags c |> U.for_some (function TOTAL | RETURN -> true | _ -> false)
