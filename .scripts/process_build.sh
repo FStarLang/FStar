e3f363990327531a48d824c7534a6d6e4b8e4066
Guido Martínez <mtzguido@gmail.com>
2018-05-01 11:04:41 -0300

    .scripts: process_build.sh: some more color

--- .scripts/process_build.sh
+++ .scripts/process_build.sh
@@ -44,12 +44,17 @@ function cleanup_files () {
 
 # Constants for showing color in output window
 RED='\033[0;31m'
+YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+diag () {
+	echo -e "${YELLOW}$1${NC}"
+}
+
 CURRENT_VERSION=$(head -n 1 version.txt | tr -d '\r')
 
-echo "*** Clean up log files ***"
+diag "*** Clean up log files ***"
 if [[ -f src/ocaml-output/fstar/HelloOcamlOutput.log ]]; then
   rm src/ocaml-output/fstar/HelloOcamlOutput.log
 fi
@@ -57,12 +62,12 @@ if [[ -f src/ocaml-output/fstar/AllExamples.log ]]; then
   rm src/ocaml-output/fstar/AllExamples.log
 fi
 
-echo "*** Make package (clean build directory first) ***"
+diag "*** Make package (clean build directory first) ***"
 cd src/ocaml-output
 make package
 
 # 'make package' makes the package using the major version from version.txt. This script is a weekly process to make minor versions so use timestamp in file name instead of major version
-echo "*** Unzip and verify the Package  ***"
+diag "*** Unzip and verify the Package  ***"
 TIME_STAMP=$(date +%Y%m%d%H%M)
 
 TYPE="_Windows_x64.zip"
@@ -84,14 +89,14 @@ else
   fi
 fi
 
-echo "*** Make the examples ***"
+diag "*** Make the examples ***"
 cd fstar
 make -C examples/hello ocaml | tee HelloOcamlOutput.log
 make -j6 -C examples | tee AllExamples.log
 
-echo "-- Verify hello ocaml -- should output Hello F*! *"
-if ! egrep 'F*!' HelloOcamlOutput.log; then
-  echo -e "* ${RED}FAIL!${NC} for examples/hello ocaml - F*! was not found in HelloOcamlOutput.log"
+diag "-- Verify hello ocaml -- should output Hello F*! *"
+if ! egrep 'Hello F\*!' HelloOcamlOutput.log; then
+  echo -e "* ${RED}FAIL!${NC} for examples/hello ocaml - 'Hello F*!' was not found in HelloOcamlOutput.log"
   exit 1
 else
   echo -e "* ${GREEN}PASSED!${NC} for examples/hello ocaml"
@@ -106,7 +111,7 @@ else
 fi
 
 # Got to this point, so know it passed - copy minor version out
-echo "*** Upload the minor version of the package. Will only keep the most recent 4 packages ***"
+diag "*** Upload the minor version of the package. Will only keep the most recent 4 packages ***"
 BN_BINARYSPATH_ROOT=~/binaries
 BN_BINARYSPATH=$BN_BINARYSPATH_ROOT/weekly
 BN_FILESTOKEEP=4
@@ -120,7 +125,7 @@ git fetch
 git checkout master
 git reset --hard origin/master
 
-echo "-- copy files and add to Github --"
+diag "-- copy files and add to Github --"
 if [[ -f $FSTAR_HOME/src/ocaml-output/$MINOR_ZIP_FILE ]]; then
   cp_to_binaries $MINOR_ZIP_FILE
 fi
@@ -129,13 +134,13 @@ if [[ -f $FSTAR_HOME/src/ocaml-output/$MINOR_TAR_FILE ]]; then
 fi
 
 # Now that latest package is added, remove the oldest one because only keeping most recent 4 packages
-echo "-- Delete oldest ZIP file if more than 4 exist --"
+diag "-- Delete oldest ZIP file if more than 4 exist --"
 cleanup_files "zip"
-echo "-- Delete oldest TAR file if more than 4 exist --"
+diag "-- Delete oldest TAR file if more than 4 exist --"
 cleanup_files "gz"
 
 # Commit and push - adding a new one and removing the oldest - commit with amend to keep history limited
-echo "--- now commit it but keep history truncated ... then push --- "
+diag "--- now commit it but keep history truncated ... then push --- "
 git commit --amend -m "Adding new build package and removing oldest."
 git push git@github.com:FStarLang/binaries.git master --force
 
