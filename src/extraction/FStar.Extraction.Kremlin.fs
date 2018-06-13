863c7e9e3a876ca629b6d703f9652b3df01cb5c0
Tahina Ramananandro <taramana@microsoft.com>
2018-06-13 11:11:39 -0700

    Extract LowStar.BufferOps.blit to KreMLin

--- src/extraction/FStar.Extraction.Kremlin.fs
+++ src/extraction/FStar.Extraction.Kremlin.fs
@@ -754,7 +754,10 @@ and translate_expr env e: expr =
       EPushFrame
   | MLE_App ({ expr = MLE_Name p }, [ _ ]) when (string_of_mlpath p = "FStar.HyperStack.ST.pop_frame") ->
       EPopFrame
-  | MLE_App ({ expr = MLE_TApp({ expr = MLE_Name p }, _) }, [ e1; e2; e3; e4; e5 ]) when (string_of_mlpath p = "FStar.Buffer.blit") ->
+  | MLE_App ({ expr = MLE_TApp({ expr = MLE_Name p }, _) }, [ e1; e2; e3; e4; e5 ]) when (
+      string_of_mlpath p = "FStar.Buffer.blit" ||
+      string_of_mlpath p = "LowStar.BufferOps.blit"
+    ) ->
       EBufBlit (translate_expr env e1, translate_expr env e2, translate_expr env e3, translate_expr env e4, translate_expr env e5)
   | MLE_App ({ expr = MLE_TApp({ expr = MLE_Name p }, _) }, [ e1; e2; e3 ]) when (string_of_mlpath p = "FStar.Buffer.fill") ->
       EBufFill (translate_expr env e1, translate_expr env e2, translate_expr env e3)
