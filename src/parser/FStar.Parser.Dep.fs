fdb6111c50d7aeca36daa2cd337b8868d73820d9
Guido Martínez <mtzguido@gmail.com>
2018-07-17 14:24:01 -0700

    parser: dep: follow quotes and antiquotes
    Fixes #1496

--- src/parser/FStar.Parser.Dep.fs
+++ src/parser/FStar.Parser.Dep.fs
@@ -656,10 +656,10 @@ let collect_one
     | Ensures (t, _)
     | Labeled (t, _, _) ->
         collect_term t
+    | Quote (t, _)
+    | Antiquote t
     | VQuote t ->
         collect_term t
-    | Quote _
-    | Antiquote _ -> ()
     | Attributes cattributes  ->
         List.iter collect_term cattributes
 
