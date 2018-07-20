303d297e5835f543baa884a4ab6c5069caa58082
Gustavo Varo <gustavo.varo@microsoft.com>
2018-07-20 14:34:20 -0400

    backport some changes form the ci script

--- .docker/build/linux/build_fstar.sh
+++ .docker/build/linux/build_fstar.sh
@@ -157,12 +157,24 @@ function build_fstar () {
             cd vale
             if [[ "$OS" == "Windows_NT" ]]; then
                 timeout 480 ./scons_cygwin.sh -j $threads --FSTAR-MY-VERSION --MIN_TEST
+                ## This hack for determining the success of a vale run is needed
+                ## because somehow scons is not returning the error code properly
+                timeout 480 ./scons_cygwin.sh -j 4 --FSTAR-MY-VERSION --MIN_TEST |& tee vale_output
+                
+                ## adds "min-test (Vale)" to the ORANGE_FILE
+                ##      if this string vvvv is present in vale_output
+                ! grep -qi 'scons: building terminated because of errors.' vale_output || \
+                {
+                    { echo " - min-test (Vale)" >> $ORANGE_FILE ; }
+                    echo true > $orange_status_file
+                }
             else
-                timeout 480 scons -j $threads --FSTAR-MY-VERSION --MIN_TEST
-            fi || {
-                { echo " - min-test (Vale)" >> $ORANGE_FILE ; }
-                echo true > $orange_status_file
-            }
+                timeout 480 scons -j $threads --FSTAR-MY-VERSION --MIN_TEST || \
+                {
+                    { echo " - min-test (Vale)" >> $ORANGE_FILE ; }
+                    echo true > $orange_status_file
+                }
+            fi
             cd ..
         } &
 
