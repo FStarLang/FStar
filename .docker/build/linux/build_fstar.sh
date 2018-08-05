0f08c51949ab4c53a8ce52f8c56b41557b893681
Gustavo Varo <gustavo.varo@microsoft.com>
2018-08-05 10:49:30 -0400

    Small fix on how we replace the branch name to refresh hints.

--- .docker/build/linux/build_fstar.sh
+++ .docker/build/linux/build_fstar.sh
@@ -114,7 +114,7 @@ function refresh_hints() {
     local hints_dir="$4"
 
     # Figure out the branch
-    CI_BRANCH=${$branchname##refs/heads/}
+    CI_BRANCH=${branchname##refs/heads/}
     echo "Current branch_name=$CI_BRANCH"
 
     # Add all the hints, even those not under version control
