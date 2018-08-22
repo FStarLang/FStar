8a8bef869be7391070534d2758eccae54f0903ff
Aseem Rastogi <aseemr@microsoft.com>
2018-08-22 18:13:27 +0530

    one more instance of HST.mreference

--- ulib/ml/FStar_HyperStack_ST.ml
+++ ulib/ml/FStar_HyperStack_ST.ml
@@ -56,7 +56,7 @@ type erid = rid
 
 type 'a ref = 'a FStar_HyperStack.reference
 type ('a, 'b) mreference = 'a ref
-type 'a reference = 'a mreference
+type 'a reference = 'a ref
 let alloc = salloc
 type ('a, 'b) mref = 'a ref
 type ('a, 'b, 'c) m_rref = 'b ref
