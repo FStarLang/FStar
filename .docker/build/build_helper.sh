4a2aae54acc8d3b35b328bd77e21fd31d0c0562c
Gustavo Varo <gustavo.varo@microsoft.com>
2018-08-30 19:06:52 -0400

    Initial Windows DockerFile

--- .docker/build/build_helper.sh
+++ .docker/build/build_helper.sh
@@ -5,7 +5,16 @@ out_file=$2
 threads=$3
 branchname=$4
 
+# Add ssh identity
+eval $(ssh-agent)
+ssh-add .ssh/id_rsa
+
+eval $(opam config env)
+
 tail -f $out_file &
 tail_pd=$!
 { { { { { { stdbuf -e0 -o0 ./build.sh "$@" ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDERR]!' ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDOUT]!' ; } 2>&1 ; } >> $out_file
 kill $tail_pd
+
+eval $(ssh-agent)
+ssh-add -D
