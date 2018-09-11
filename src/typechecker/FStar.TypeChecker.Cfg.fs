068d53bb67b01f70b83484bb922f089709376fce
Guido Martínez <mtzguido@gmail.com>
2018-09-11 20:33:54 +0200

    fix warning

--- src/typechecker/FStar.TypeChecker.Cfg.fs
+++ src/typechecker/FStar.TypeChecker.Cfg.fs
@@ -726,6 +726,7 @@ let built_in_primitive_steps : BU.psmap<primitive_step> =
           | "UInt32" -> Z.of_hex "ffffffff"
           | "UInt64" -> Z.of_hex "ffffffffffffffff"
           | "UInt128" -> Z.of_hex "ffffffffffffffffffffffffffffffff"
+          | _ -> failwith (BU.format1 "Impossible: bad string on mask: %s\n" m)
         in
         let bitwise =
           bounded_unsigned_int_types
