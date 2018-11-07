127ce7c1c8b818aa6175987e0855d998955a371b
Aseem Rastogi <aseemr@microsoft.com>
2018-11-07 10:03:57 +0530

    ml implementation of Set.equal

--- ulib/ml/FStar_Set.ml
+++ ulib/ml/FStar_Set.ml
@@ -5,3 +5,9 @@ let union = BatSet.union
 let intersect = BatSet.intersect
 let complement x = BatSet.empty
 let mem = BatSet.mem
+
+(*
+ * F* should not extract Set.equal
+ * We should fix it, adding the following in the meantime
+ *)
+type ('A, 'B, 'C) equal = unit
