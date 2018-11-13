518dde40c8e9bd33aef63ab000136ba35342faa7
Gustavo Varo <gustavo.varo@microsoft.com>
2018-11-12 21:06:53 -0500

    Improve how we export home path to enhance debug.

--- .docker/build/build.sh
+++ .docker/build/build.sh
@@ -8,10 +8,21 @@ threads=$3
 branchname=$4
 
 function export_home() {
+    local home_path=""
     if command -v cygpath >/dev/null 2>&1; then
-        export $1_HOME=$(cygpath -m "$2")
+        home_path=$(cygpath -m "$2")
     else
-        export $1_HOME="$2"
+        home_path="$2"
+    fi
+
+    export $1_HOME=$home_path
+
+    # Update .bashrc file
+    token=$1_HOME=
+    if grep -q "$token" ~/.bashrc; then
+        sed -i -E "s/$token.*/$token$home_path/" ~/.bashrc
+    else
+        echo "export $1_HOME=$home_path" >> ~/.bashrc
     fi
 }
 
