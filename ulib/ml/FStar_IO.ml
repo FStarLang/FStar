1b853ab7747537ae81649f22830623239a38d9e4
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2018-11-23 14:43:14 +0100

    Further fixing FStar_IO.ml printing of Machine Integers

--- ulib/ml/FStar_IO.ml
+++ ulib/ml/FStar_IO.ml
@@ -7,12 +7,13 @@ let fpr = Printf.fprintf
 
 let print_newline = print_newline
 let print_string s = pr "%s" s; flush stdout
-let print_uint8 s = pr "%.02x" (FStar_UInt8.to_int s); flush stdout
-let print_uint8_dec s = print_int (FStar_UInt8.to_int s); flush stdout
-let print_uint32 s = pr "%.04x" (FStar_UInt32.to_int s); flush stdout
-let print_uint32_dec s = print_int (FStar_UInt32.to_int s); flush stdout
-let print_uint64 s = pr "%.08x" (FStar_UInt64.to_int s); flush stdout
-let print_uint64_dec s = print_int (FStar_UInt64.to_int s); flush stdout
+let print_uint8 s = pr "%x" (FStar_UInt8.to_int s); flush stdout
+let print_uint8_dec s = pr "%s" (FStar_UInt8.to_string s); flush stdout
+let print_uint32 s = pr "%x" (FStar_UInt32.to_int s); flush stdout
+let print_uint32_dec s = pr "%s" (FStar_UInt32.to_string s); flush stdout
+let print_uint64 s = pr "%s" (let fs = FStar_UInt64.to_string_hex s in
+                              String.sub fs 2 (String.length fs - 2)); flush stdout
+let print_uint64_dec s = pr "%s" (FStar_UInt64.to_string s); flush stdout
 let print_any s = output_value stdout s; flush stdout
 let input_line = read_line
 let input_int () = Z.of_int (read_int ())
