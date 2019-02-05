b5c380884453769dcdebbd884da016414d9d0869
Nikhil Swamy <nikswamy@users.noreply.github.com>
2019-02-04 23:49:48 -0800

    enable protect_top_level_axioms again

--- ulib/gmake/fstar.mk
+++ ulib/gmake/fstar.mk
@@ -1,5 +1,5 @@
 HINTS_ENABLED?=--use_hints --use_hint_hashes
-PROTECT_TOP_LEVEL_AXIOMS?=--protect_top_level_axioms false
+PROTECT_TOP_LEVEL_AXIOMS?=--protect_top_level_axioms true
 WARN_ERROR=--warn_error +9 --warn_error +16 --warn_error +19
 OTHERFLAGS+=$(PROTECT_TOP_LEVEL_AXIOMS) $(WARN_ERROR)
 
