533b691958e088277bcdc1b16d75aaa5e29bcec9
Nikhil Swamy <nikswamy@users.noreply.github.com>
2019-02-05 08:15:05 -0800

    setting a soft timeout to prevent cancelled builds

--- ulib/gmake/fstar.mk
+++ ulib/gmake/fstar.mk
@@ -1,7 +1,7 @@
 HINTS_ENABLED?=--use_hints --use_hint_hashes
 PROTECT_TOP_LEVEL_AXIOMS?=--protect_top_level_axioms true
 WARN_ERROR=--warn_error +9 --warn_error +16 --warn_error +19
-OTHERFLAGS+=$(PROTECT_TOP_LEVEL_AXIOMS) $(WARN_ERROR)
+OTHERFLAGS+=$(PROTECT_TOP_LEVEL_AXIOMS) $(WARN_ERROR) --z3cliopt 'timeout=180000'
 
 ifdef Z3
 OTHERFLAGS+=--smt $(Z3)
