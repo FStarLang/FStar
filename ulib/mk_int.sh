1c343324b397e1f2798fafb079e549fb80ba3e71
Santiago Zanella-Beguelin <santiago@microsoft.com>
2019-03-26 11:05:25 +0000

    Modify mk_int script to add copyright notice and regenerate FStar.Int/UInt files

--- ulib/mk_int.sh
+++ ulib/mk_int.sh
@@ -3,6 +3,21 @@
 for i in 8 16 31 32 63 64 128; do
   f=FStar.Int$i.fst
   cat > $f <<EOF
+(*
+   Copyright 2008-2019 Microsoft Research
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*)
 module FStar.Int$i
 (* This module generated automatically using [mk_int.sh] *)
 
@@ -27,6 +42,21 @@ done
 for i in 8 16 31 32 63 64; do
   f=FStar.UInt$i.fst
   cat > $f <<EOF
+(*
+   Copyright 2008-2019 Microsoft Research
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*)
 module FStar.UInt$i
 (* This module generated automatically using [mk_int.sh] *)
 
