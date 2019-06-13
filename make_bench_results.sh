7a865c883f4d7b0a3832b0f4db96205d16d791e7
Tom Kelly <ctk21@cl.cam.ac.uk>
2019-06-13 16:59:28 +0100

    Build ulib before benchmarking. Use new ulib benchmark target. Try not to nuke ulib when doing the extraction benchmark.

--- make_bench_results.sh
+++ make_bench_results.sh
@@ -15,22 +15,39 @@ mkdir -p ${OUTDIR}
 make clean
 make -C src clean_boot
 make -C src clean
+git checkout -- src/ocaml-output
+
+# build fstar compiler bootstrap
 T0=`date +'%Y%m%d_%H%M%S'`
 echo "Starting fstar compiler bootstrap ${T0}"
-make -C src ocaml-fstar-ocaml 2>&1 | tee ${OUTDIR}/build.log
+make -C src ocaml-fstar-ocaml 2>&1 | tee ${OUTDIR}/build_fstar.log
 T1=`date +'%Y%m%d_%H%M%S'`
 echo "Finished fstar compiler boostrap ${T1} (started at ${T0})"
 
-# benchmark runs
-for i in examples/micro-benchmarks ulib; do
-	NME=${i##*/}
-	${TASKSET_WRAP} make -C $i ORUN_FSTAR=true OTHERFLAGS="${FSTAR_OTHERFLAGS}" 2>&1 | tee ${OUTDIR}/${NME}.log
-	cat ${i}/*.bench > ${OUTDIR}/${NME}.bench
-done
+# verify ulib and install
+T0=`date +'%Y%m%d_%H%M%S'`
+echo "Starting fstarlib build ${T0}"
+make -C src fstarlib 2>&1 | tee ${OUTDIR}/build_fstarlib.log
+T1=`date +'%Y%m%d_%H%M%S'`
+echo "Finished fstar compiler boostrap ${T1} (started at ${T0})"
+
+ls -ltr ulib >> ${OUTDIR}/build_fstarlib.log
+
+# benchmark examples/micro-benchmarks
+BENCH_DIR=examples/micro-benchmarks; NME=micro-benchmarks
+rm -f ${BENCH_DIR}/*.bench
+${TASKSET_WRAP} make -C ${BENCH_DIR} ORUN_FSTAR=true OTHERFLAGS="${FSTAR_OTHERFLAGS}" 2>&1 | tee ${OUTDIR}/${NME}.log
+cat ${BENCH_DIR}/*.bench > ${OUTDIR}/${NME}.bench
+
+# benchmark ulib
+BENCH_DIR=ulib; NME=ulib
+rm -f ${BENCH_DIR}/*.bench
+${TASKSET_WRAP} make -C ${BENCH_DIR} benchmark ORUN_FSTAR=true OTHERFLAGS="${FSTAR_OTHERFLAGS}" 2>&1 | tee ${OUTDIR}/${NME}.log
+cat ${BENCH_DIR}/*.bench > ${OUTDIR}/${NME}.bench
 
 # ocaml_extract: make -C src ocaml
-make -C src clean
 make -C src clean_boot
+#make -C src clean # will do a clean-ocaml as well
 ${TASKSET_WRAP} make -C src ocaml ORUN_FSTAR=true OTHERFLAGS="${FSTAR_OTHERFLAGS}" 2>&1 | tee ${OUTDIR}/ocaml_extract.log
 cat src/ocaml-output/*.bench > ${OUTDIR}/ocaml_extract.bench
 
