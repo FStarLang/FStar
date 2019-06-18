1d8fe20e615f5c526cd2893962e04acbbdd987ef
Catalin Hritcu <catalin.hritcu@gmail.com>
2019-06-19 00:06:27 +0200

    Print versions in .scripts/process_build.sh

--- .scripts/process_build.sh
+++ .scripts/process_build.sh
@@ -103,6 +103,10 @@ cd fstar
 # to export it from here.
 export FSTAR_HOME="$PWD"
 
+diag "-- Versions --"
+fstar.exe --version
+z3 --version
+
 diag "-- Verify micro benchmarks --"
 make -C examples/micro-benchmarks
 if [ $? -ne 0 ]; then
