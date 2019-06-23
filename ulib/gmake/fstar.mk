2198a89e094be0232a4684d9ae8fbb8d31c509bd
Aseem Rastogi <aseemr@microsoft.com>
2019-06-23 18:05:50 +0530

    suppressing 271

--- ulib/gmake/fstar.mk
+++ ulib/gmake/fstar.mk
@@ -1,5 +1,6 @@
 HINTS_ENABLED?=--use_hints
-WARN_ERROR=
+# 271: theory symbols in smt patters
+WARN_ERROR=--warn_error -271
 VALIDITY_AXIOMS?=--smtencoding.valid_intro true --smtencoding.valid_elim true
 OTHERFLAGS+=$(WARN_ERROR) --z3cliopt 'timeout=600000' $(VALIDITY_AXIOMS)
 
