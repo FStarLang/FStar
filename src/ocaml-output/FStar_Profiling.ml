a4088c35bc8f76a71c9430d471a29e51ed4821ec
Nikhil Swamy <nikswamy@users.noreply.github.com>
2019-08-06 11:45:28 -0700

    missed file

---
+++ src/ocaml-output/FStar_Profiling.ml
@@ -0,0 +1,108 @@
+open Prims
+type counter =
+  {
+  cid: Prims.string ;
+  total_time: Prims.int FStar_ST.ref ;
+  running: Prims.bool FStar_ST.ref ;
+  undercount: Prims.bool FStar_ST.ref }
+let (__proj__Mkcounter__item__cid : counter -> Prims.string) =
+  fun projectee  ->
+    match projectee with | { cid; total_time; running; undercount;_} -> cid
+  
+let (__proj__Mkcounter__item__total_time : counter -> Prims.int FStar_ST.ref)
+  =
+  fun projectee  ->
+    match projectee with
+    | { cid; total_time; running; undercount;_} -> total_time
+  
+let (__proj__Mkcounter__item__running : counter -> Prims.bool FStar_ST.ref) =
+  fun projectee  ->
+    match projectee with
+    | { cid; total_time; running; undercount;_} -> running
+  
+let (__proj__Mkcounter__item__undercount :
+  counter -> Prims.bool FStar_ST.ref) =
+  fun projectee  ->
+    match projectee with
+    | { cid; total_time; running; undercount;_} -> undercount
+  
+let (new_counter : Prims.string -> counter) =
+  fun cid  ->
+    let uu____171 = FStar_Util.mk_ref Prims.int_zero  in
+    let uu____177 = FStar_Util.mk_ref false  in
+    let uu____183 = FStar_Util.mk_ref false  in
+    {
+      cid;
+      total_time = uu____171;
+      running = uu____177;
+      undercount = uu____183
+    }
+  
+let (all_counters : counter FStar_Util.smap) =
+  FStar_Util.smap_create (Prims.of_int (20)) 
+let (create_or_lookup_counter : Prims.string -> counter) =
+  fun cid  ->
+    let uu____201 = FStar_Util.smap_try_find all_counters cid  in
+    match uu____201 with
+    | FStar_Pervasives_Native.Some c -> c
+    | FStar_Pervasives_Native.None  ->
+        let c = new_counter cid  in
+        (FStar_Util.smap_add all_counters cid c; c)
+  
+let profile : 'a . (unit -> 'a) -> Prims.string -> Prims.string -> 'a =
+  fun f  ->
+    fun module_name  ->
+      fun cid  ->
+        let uu____241 = FStar_Options.profile_enabled module_name cid  in
+        if uu____241
+        then
+          let c = create_or_lookup_counter cid  in
+          let uu____245 = FStar_ST.op_Bang c.running  in
+          (if uu____245
+           then f ()
+           else
+             (try
+                (fun uu___31_278  ->
+                   match () with
+                   | () ->
+                       (FStar_ST.op_Colon_Equals c.running true;
+                        (let uu____302 = FStar_Util.record_time f  in
+                         match uu____302 with
+                         | (res,elapsed) ->
+                             ((let uu____313 =
+                                 let uu____315 =
+                                   FStar_ST.op_Bang c.total_time  in
+                                 uu____315 + elapsed  in
+                               FStar_ST.op_Colon_Equals c.total_time
+                                 uu____313);
+                              FStar_ST.op_Colon_Equals c.running false;
+                              res)))) ()
+              with
+              | uu___30_385 ->
+                  (FStar_ST.op_Colon_Equals c.running false;
+                   FStar_ST.op_Colon_Equals c.undercount true;
+                   FStar_Exn.raise uu___30_385)))
+        else f ()
+  
+let (report_and_clear : unit -> unit) =
+  fun uu____439  ->
+    FStar_Util.smap_iter all_counters
+      (fun uu____445  ->
+         fun c  ->
+           let warn =
+             let uu____450 = FStar_ST.op_Bang c.running  in
+             if uu____450
+             then " (Warning, this counter is still running)"
+             else
+               (let uu____478 = FStar_ST.op_Bang c.undercount  in
+                if uu____478
+                then
+                  " (Warning, some operations raised exceptions and we not accounted for)"
+                else "")
+              in
+           let uu____507 =
+             let uu____509 = FStar_ST.op_Bang c.total_time  in
+             FStar_Util.string_of_int uu____509  in
+           FStar_Util.print3 "Profiled %s:\t %s ms%s\n" c.cid uu____507 warn);
+    FStar_Util.smap_clear all_counters
+  
