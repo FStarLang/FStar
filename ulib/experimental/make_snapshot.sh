913d285fe626ebd6ad5ce0f015bee72f35ba481a
Denis Merigoux <denis.merigoux@gmail.com>
2020-03-03 23:07:21 -0800

    Forgot some files

--- ulib/experimental/make_snapshot.sh
+++ ulib/experimental/make_snapshot.sh
@@ -3,5 +3,5 @@
 commit=$(git log --pretty=format:'%h' -n 1)
 cp -f README.md README.md.backup
 sed -i "s/???/$commit/g" README.md
-tar -czvf steelcore-icfp2020-supplementary-material.tar.gz Steel.*.fst Steel.*.fsti MParIndex.fst README.md
+tar -czvf steelcore-icfp2020-supplementary-material.tar.gz MST.fst NMST.fst Steel.*.fst Steel.*.fsti MParIndex.fst README.md
 cp -f README.md.backup README.md
