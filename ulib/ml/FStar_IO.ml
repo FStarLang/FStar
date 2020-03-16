b846bc077cae342f209c33bf1f4dd111da68983d
Guido Martínez <mtzguido@gmail.com>
2020-03-16 15:20:17 -0300

    integers: to_string_hex does not pad, introduce to_string_hex_pad
    Fix FStar.IO while at it.

--- ulib/ml/FStar_IO.ml
+++ ulib/ml/FStar_IO.ml
@@ -17,82 +17,24 @@ let print_string s = pr "%s" s; flush stdout
  *   pr "%u" s;
  *   flush stdout *)
 
+let print_via (f:'a -> string) (x:'a) : unit =
+    pr "%s" (f x);
+    flush stdout
 
-let print_uint8 s =
-  let s' = FStar_UInt8.to_int s in
-  pr "%x" s';
-  flush stdout
+let print_uint8      = print_via FStar_UInt8.to_string_hex
+let print_uint16     = print_via FStar_UInt16.to_string_hex
+let print_uint32     = print_via FStar_UInt32.to_string_hex
+let print_uint64     = print_via FStar_UInt64.to_string_hex
 
-let print_uint8_dec s =
-  pr "%s" (FStar_UInt8.to_string s);
-  flush stdout
+let print_uint8_dec  = print_via FStar_UInt8.to_string
+let print_uint16_dec = print_via FStar_UInt16.to_string
+let print_uint32_dec = print_via FStar_UInt32.to_string
+let print_uint64_dec = print_via FStar_UInt64.to_string
 
-let print_uint32 s =
-  pr "%s" (FStar_UInt32.to_string_hex s);
-  flush stdout
-
-let print_uint32_dec s =
-  pr "%s" (FStar_UInt32.to_string s);
-  flush stdout
-
-let print_uint64 s =
-  let fs = FStar_UInt64.to_string_hex s in
-  let s' = String.sub fs 2 (String.length fs - 2) in
-  pr "%s" s';
-  flush stdout
-
-let print_uint64_dec s =
-  pr "%s" (FStar_UInt64.to_string s);
-  flush stdout
-
-
-let print_uint8_hex_pad s =
-  let n = FStar_Int_Cast.uint8_to_uint64 s in
-  let fs = FStar_UInt64.to_string_hex n in
-  let s' = String.sub fs 2 (String.length fs - 2) in
-  let l = 2 - String.length s' in
-  let r = (String.make l '0') ^ s' in
-  pr "%s" r;
-  flush stdout
-
-let print_uint8_dec_pad s =
-  let s' = FStar_UInt8.to_string s in
-  let l = 2 - String.length s' in
-  let r = (String.make l '0') ^ s' in
-  pr "%s" r;
-  flush stdout
-
-let print_uint32_hex_pad s =
-  let n = FStar_Int_Cast.uint32_to_uint64 s in
-  let fs = FStar_UInt64.to_string_hex n in
-  let s' = String.sub fs 2 (String.length fs - 2) in
-  let l = 8 - String.length s' in
-  let r = (String.make l '0') ^ s' in
-  pr "%s" r;
-  flush stdout
-
-let print_uint32_dec_pad s =
-  let s' = FStar_UInt32.to_string s in
-  let l = 8 - String.length s' in
-  let r = (String.make l '0') ^ s' in
-  pr "%s" r;
-  flush stdout
-
-let print_uint64_hex_pad s =
-  let fs = FStar_UInt64.to_string_hex s in
-  let s' = String.sub fs 2 (String.length fs - 2) in
-  let l = 16 - String.length s' in
-  let r = (String.make l '0') ^ s' in
-  pr "%s" r;
-  flush stdout
-
-let print_uint64_dec_pad s =
-  let s' = FStar_UInt64.to_string s in
-  let l = 16 - String.length s' in
-  let r = (String.make l '0') ^ s' in
-  pr "%s" r;
-  flush stdout
-
+let print_uint8_hex_pad  = print_via FStar_UInt8.to_string_hex_pad
+let print_uint16_hex_pad = print_via FStar_UInt16.to_string_hex_pad
+let print_uint32_hex_pad = print_via FStar_UInt32.to_string_hex_pad
+let print_uint64_hex_pad = print_via FStar_UInt64.to_string_hex_pad
 
 let print_any s = output_value stdout s; flush stdout
 let input_line = read_line
