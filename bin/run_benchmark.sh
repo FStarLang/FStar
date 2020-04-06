946d125e7b466930ed477b09d6de2bf1da9d5e1d
Guido Martínez <mtzguido@gmail.com>
2020-04-06 08:13:53 -0300

    nit

--- bin/run_benchmark.sh
+++ bin/run_benchmark.sh
@@ -98,9 +98,8 @@ write_csv() {
     IN=${1}
     OUT=${1}.csv
 
-    FIELDS=('name', 'time_secs', 'user_time_secs', 'sys_time_secs', 'maxrss_kB')
-    # Other fields which could be relevant (not used currently)
-    # 'gc.allocated_words', 'gc.minor_words', 'gc.promoted_words', 'gc.major_words', 'gc.minor_collections', 'gc.major_collections', 'gc.heap_words', 'gc.heap_chunks', 'gc.top_heap_words', 'gc.compactions')
+    FIELDS=('name', 'time_secs', 'user_time_secs', 'sys_time_secs', 'maxrss_kB', 'gc.allocated_words', 'gc.minor_words', 'gc.promoted_words', 'gc.major_words', 'gc.minor_collections', 'gc.major_collections', 'gc.heap_words', 'gc.heap_chunks', 'gc.top_heap_words', 'gc.compactions')
+
     HEADER=$(printf "%s" ${FIELDS[@]})
     JQ_ARGS=$(printf ".%s" ${FIELDS[@]})
 
