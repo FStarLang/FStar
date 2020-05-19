f26c54dab3339d8a614c6998ba829e420c9ed53b
Nikhil Swamy <nikswamy@users.noreply.github.com>
2020-05-19 11:51:37 -0700

    Revise the erasability check to account for normalization

--- src/typechecker/FStar.TypeChecker.Util.fs
+++ src/typechecker/FStar.TypeChecker.Util.fs
@@ -2716,15 +2716,16 @@ let check_sigelt_quals (env:FStar.TypeChecker.Env.env) se =
             () (* just ignore it, the member ses have the attribute too *)
             
           | Sig_let((false, [lb]), _) ->
-            if not (Env.non_informative env lb.lbdef)
+            let _, body, _ = U.abs_formals lb.lbdef in
+            if not (N.non_info_norm env body)
             then raise_error
                    (Errors.Fatal_QulifierListNotPermitted,
                     BU.format1
                     "Illegal attribute: \
                      the `erasable` attribute is only permitted on inductive type definitions \
                      and abbreviations for non-informative types. %s is considered informative."
-                     (Print.term_to_string lb.lbdef))
-                    lb.lbdef.pos
+                     (Print.term_to_string body))
+                    body.pos
           
           | _ ->
             raise_error
