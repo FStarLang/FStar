30bc93c8b005c5f1a42a9db40c74ade4ec09e6b0
Guido Martínez <mtzguido@gmail.com>
2020-10-19 12:08:17 -0300

    Revert "normalizer: make memoization valid only for a given cfg"
    This reverts commit 4504873d3609253f9a22656dd5c55ae1f7f3b9d6.

--- src/typechecker/FStar.TypeChecker.Normalize.fs
+++ src/typechecker/FStar.TypeChecker.Normalize.fs
@@ -60,20 +60,8 @@ let cases f d = function
   | Some x -> f x
   | None -> d
 
-(* We memoize the normal form of variables in the environment, in
- * order to implement call-by-need and avoid an exponential explosion,
- * but we take care to only reuse memoized values when the cfg has not
- * changed. The main reason is normalization requests, which can "grow"
- * the set of allowed computations steps, and hence we may memoize
- * something during the request that is used outside of it. This will
- * essentially make it invalid. See issue #2155 in Github.
- *
- * We compare the cfg with physical equality, so it has to be the
- * exact same object in memory. See read_memo and set_memo below. *)
-type cfg_memo<'a> = memo<(Cfg.cfg * 'a)>
-
 type closure =
-  | Clos of env * term * cfg_memo<(env * term)> * bool //memo for lazy evaluation; bool marks whether or not this is a fixpoint
+  | Clos of env * term * memo<(env * term)> * bool //memo for lazy evaluation; bool marks whether or not this is a fixpoint
   | Univ of universe                               //universe terms do not have free variables
   | Dummy                                          //Dummy is a placeholder for a binder when doing strong reduction
 and env = list<(option<binder>*closure)>
@@ -85,7 +73,7 @@ type branches = list<(pat * option<term> * term)>
 type stack_elt =
  | Arg      of closure * aqual * Range.range
  | UnivArgs of list<universe> * Range.range
- | MemoLazy of cfg_memo<(env * term)>
+ | MemoLazy of memo<(env * term)>
  | Match    of env * branches * cfg * Range.range
  | Abs      of env * binders * env * option<residual_comp> * Range.range //the second env is the first one extended with the binders, for reducing the option<lcomp>
  | App      of env * term * aqual * Range.range
@@ -98,20 +86,11 @@ type stack = list<stack_elt>
 
 let head_of t = let hd, _ = U.head_and_args' t in hd
 
-let read_memo cfg (r:memo<(Cfg.cfg * 'a)>) : option<'a> =
-  match !r with
-  | Some (cfg', a) when BU.physical_equality cfg cfg' ->
-    Some a
-  | _ -> None
-
-let set_memo cfg (r:memo<(Cfg.cfg * 'a)>) (t:'a) : unit =
-  if cfg.memoize_lazy then begin
-    (* We do this only as a sanity check. The only situation where we
-     * should set a memo again is when the cfg has changed. *)
-    if Option.isSome (read_memo cfg r) then
-      failwith "Unexpected set_memo: thunk already evaluated";
-    r := Some (cfg, t)
-  end
+let set_memo cfg (r:memo<'a>) (t:'a) =
+  if cfg.memoize_lazy then
+    match !r with
+    | Some _ -> failwith "Unexpected set_memo: thunk already evaluated"
+    | None -> r := Some t
 
 let closure_to_string = function
     | Clos (env, t, _, _) -> BU.format2 "(env=%s elts; %s)" (List.length env |> string_of_int) (Print.term_to_string t)
@@ -1203,7 +1182,7 @@ let rec norm : cfg -> env -> stack -> term -> term =
                    if not fix
                    || cfg.steps.zeta
                    || cfg.steps.zeta_full
-                   then match read_memo cfg r with
+                   then match !r with
                         | Some (env, t') ->
                             log cfg  (fun () -> BU.print2 "Lazy hit: %s cached to %s\n" (Print.term_to_string t) (Print.term_to_string t'));
                             if maybe_weakly_reduced t'
@@ -1309,7 +1288,7 @@ let rec norm : cfg -> env -> stack -> term -> term =
                    let stack =
                      stack |>
                      List.fold_right (fun (a, aq) stack ->
-                       Arg (Clos(env, a, BU.mk_ref (Some (cfg, ([], a))), false),aq,t.pos)::stack)
+                       Arg (Clos(env, a, BU.mk_ref (Some ([], a)), false),aq,t.pos)::stack)
                      norm_args
                    in
                    log cfg  (fun () -> BU.print1 "\tPushed %s arguments\n" (string_of_int <| List.length args));
@@ -1495,7 +1474,7 @@ let rec norm : cfg -> env -> stack -> term -> term =
                     let memo = BU.mk_ref None in
                     let rec_env = (None, Clos(env, fix_f_i, memo, true))::rec_env in
                     rec_env, memo::memos, i + 1) (snd lbs) (env, [], 0) in
-            let _ = List.map2 (fun lb memo -> memo := Some (cfg, (rec_env, lb.lbdef))) (snd lbs) memos in //tying the knot
+            let _ = List.map2 (fun lb memo -> memo := Some (rec_env, lb.lbdef)) (snd lbs) memos in //tying the knot
             let body_env = List.fold_right (fun lb env -> (None, Clos(rec_env, lb.lbdef, BU.mk_ref None, false))::env)
                                (snd lbs) env in
             norm cfg body_env stack body
@@ -2482,7 +2461,7 @@ and rebuild (cfg:cfg) (env:env) (stack:stack) (t:term) : term =
                   rebuild cfg env_arg stack t
              else let stack = App(env, t, aq, r)::stack in
                   norm cfg env_arg stack tm
-        else begin match read_memo cfg m with
+        else begin match !m with
           | None ->
             if cfg.steps.hnf && not (is_partial_primop_app cfg t)
             then let arg = closure_as_term cfg env_arg tm in
@@ -2757,7 +2736,7 @@ and rebuild (cfg:cfg) (env:env) (stack:stack) (t:term) : term =
                 let env0 = env in
                 let env = List.fold_left
                       (fun env (bv, t) -> (Some (S.mk_binder bv),
-                                           Clos([], t, BU.mk_ref (Some (cfg, ([], t))), false))::env)
+                                           Clos([], t, BU.mk_ref (Some ([], t)), false))::env)
                       env s in
                 norm cfg env stack (guard_when_clause wopt b rest)
         in
