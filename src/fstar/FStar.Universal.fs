76417d62d4e1a6b141637ae0308b54d6e82b4aaa
Aseem Rastogi <aseemr@microsoft.com>
2021-03-26 08:00:13 +0000

    nit

--- src/fstar/FStar.Universal.fs
+++ src/fstar/FStar.Universal.fs
@@ -124,6 +124,7 @@ let init_env deps : TcEnv.env =
         TcTerm.universe_of
         TcTerm.tc_check_tot_or_gtot_term_maybe_fastpath
         TcTerm.universeof_fastpath
+        TcTerm.effectof_tot_or_gtot_term_fastpath
         solver
         Const.prims_lid
         (NBE.normalize
