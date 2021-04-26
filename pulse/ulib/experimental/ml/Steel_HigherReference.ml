1102d2ff8e6a5480bf30b2c1d0574235055c7959
Aseem Rastogi <aseemr@microsoft.com>
2021-04-26 10:15:04 +0530

    wip OCaml realizations for Steel

---
+++ pulse/ulib/experimental/ml/Steel_HigherReference.ml
@@ -0,0 +1,18 @@
+(*
+ * AR: This is unsound in the presence of concurrency, see PR#2276
+ *)
+type 'a ref = 'a FStar_ST.ref
+
+(*
+ * TODO: What about null and is_null?
+ *)
+
+let alloc = FStar_ST.alloc
+
+let read (perm:unit) (v:unit) (r:'a ref) = FStar_ST.read r
+
+let read_refine (perm:unit) (q:unit) (r:'a ref) = read () () r
+
+let write (v:unit) (r:'a ref) (x:'a) = FStar_ST.write r x
+
+let free (v:unit) (r:'a ref) = raise (Steel_Common.Steel_function_unsupported "free")
