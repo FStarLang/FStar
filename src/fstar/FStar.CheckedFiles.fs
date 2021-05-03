e3081cf686d062ae1150716ab2a4940f481ee8ce
Aseem Rastogi <aseemr@microsoft.com>
2021-05-03 10:20:35 +0000

    bump up the checked file no.

--- src/fstar/FStar.CheckedFiles.fs
+++ src/fstar/FStar.CheckedFiles.fs
@@ -43,7 +43,7 @@ module Dep     = FStar.Parser.Dep
  * detect when loading the cache that the version number is same
  * It needs to be kept in sync with prims.fst
  *)
-let cache_version_number = 34
+let cache_version_number = 35
 
 type tc_result = {
   checked_module: Syntax.modul; //persisted
