cd00ade26b2c1075b5fb924dac28d3a3f9e84650
Aymeric Fromherz <fromherz@cmu.edu>
2021-05-16 22:47:52 -0400

    Remove unneeded files for Steel AEC submission

--- ulib/experimental/ml/Steel_SpinLock.ml
+++
@@ -1,9 +0,0 @@
-type lock_t = Mutex.t
-
-type 'a lock = lock_t
-
-let new_lock (_:unit) : 'unit lock = Mutex.create ()
-
-let acquire (_:unit) (l:unit lock) = Mutex.lock l
-
-let release (_:unit) (l:unit lock) = Mutex.unlock l
