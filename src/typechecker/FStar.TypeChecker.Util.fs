f1de078c2f2addb27a56eaa4046f02d6ce6dec3a
Aseem Rastogi <aseemr@microsoft.com>
2021-06-04 08:34:19 +0000

    do not apply range argument when applying binds

--- src/typechecker/FStar.TypeChecker.Util.fs
+++ src/typechecker/FStar.TypeChecker.Util.fs
@@ -952,9 +952,7 @@ let mk_wp_bind env (m:lident) (ct1:comp_typ) (b:option<bv>) (ct2:comp_typ) (flag
     //we know it's total; indicate for the normalizer reduce it by adding  the TOTAL flag
     U.abs bs wp (Some (U.mk_residual_comp C.effect_Tot_lid None [TOTAL]))
   in
-  let r1 = S.mk (S.Tm_constant (FStar.Const.Const_range r1)) r1 in
   let wp_args = [
-    S.as_arg r1;
     S.as_arg t1;
     S.as_arg t2;
     S.as_arg wp1;
