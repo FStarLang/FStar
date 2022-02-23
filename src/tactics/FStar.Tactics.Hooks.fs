7456ef850f6905aa8cbace7ddacdad03c0090092
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2022-02-23 13:14:54 +0100

    Do not use resolve_implicits for smt_handle_goals

--- src/tactics/FStar.Tactics.Hooks.fs
+++ src/tactics/FStar.Tactics.Hooks.fs
@@ -383,9 +383,7 @@ let solve_implicits (env:Env.env) (tau:term) (imps:Env.implicits) : unit =
 (* Retrieves a tactic associated to a given attribute, if any *)
 let find_user_tac_for_attr env (a:term) : option<sigelt> =
   let hooks = Env.lookup_attr env PC.resolve_implicits_attr_string in
-  hooks |> BU.try_find
-              (fun hook ->
-                hook.sigattrs |> BU.for_some (U.attr_eq a))
+  hooks |> BU.try_find (fun _ -> true)
 
 (* This function takes an environment [env] and a goal [goal], and tries to run
    the tactic registered with the (handle_smt_goal) attribute, if any.
