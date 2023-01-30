c3914d55e544003ea79becd0b8181c1bd75b774b
Tahina Ramananandro <taramana@microsoft.com>
2023-01-30 15:01:33 -0800

    remove handwritten ulib files
    you can find them in the ocaml/ directory, outside of the
    generated subdirectories.

--- ulib/ml/FStar_All.ml
+++
@@ -1,9 +0,0 @@
-exception Failure = Failure
-let failwith x = raise (Failure x)
-let exit i = exit (Z.to_int i)
-let op_Bar_Greater (x : 'a) (f : ('a -> 'b)) : 'b = f x
-let op_Less_Bar  (f : ('a -> 'b)) (x : 'a) : 'b = f x
-let pipe_right a f = f a
-let pipe_left f a = f a
-let try_with f1 f2 = try f1 () with | e -> f2 e
-
--- ulib/ml/FStar_Date.ml
+++
@@ -1,7 +0,0 @@
-  type dateTime = DT of float
-  type timeSpan = TS of float
-  let now () = DT (Unix.gettimeofday())
-  let secondsFromDawn () = Int64.of_float (Unix.time()) |> Z.of_int64
-  let newTimeSpan d h m s = TS (((((float_of_int (Z.to_int d)) *. 24.0) +. (float_of_int (Z.to_int h))) *. 60.0 +. (float_of_int (Z.to_int m))) *. 60.0 +. (float_of_int (Z.to_int s)))
-  let addTimeSpan (DT(a)) (TS(b)) = DT (a +. b)
-  let greaterDateTime (DT(a)) (DT(b)) = a > b
