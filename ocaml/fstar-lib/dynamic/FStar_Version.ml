bf18af2c7a34e605cb5cb362624c63295f7b4b0a
Tahina Ramananandro <taramana@microsoft.com>
2023-02-01 09:09:49 -0800

    snap

--- ocaml/fstar-lib/dynamic/FStar_Version.ml
+++ ocaml/fstar-lib/dynamic/FStar_Version.ml
@@ -2,5 +2,5 @@ let dummy () = ();;
 FStar_Options._version := "2022.11.19~dev";;
 FStar_Options._platform := "Linux_x86_64";;
 FStar_Options._compiler := "OCaml 4.12.1";;
-FStar_Options._date := "2023-01-31 10:33:04 -0800 ";;
-FStar_Options._commit:= "ba0ec5f2c7815576a1fba7abecce965fdd6ec21e (dirty)";;
+FStar_Options._date := "2023-01-31 10:48:49 -0800 ";;
+FStar_Options._commit:= "6ddfc733ffccc6b5df5cabc2a7953b0c1c7b4c0b (dirty)";;
