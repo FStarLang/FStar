e2f334ae104fdfbe022edf236d5257a33287700e
Tahina Ramananandro <taramana@microsoft.com>
2023-02-06 11:30:12 -0800

    include extracted ulib/fs into binary package
    this is currently also building the ulibfs nuget package
    and including it into the binary package

--- ulib/install-ulib.sh
+++ ulib/install-ulib.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 set -x
-for f in *.fst *.fsti experimental/*.fst experimental/*.fsti .cache/*.checked ml/Makefile.realized ml/Makefile.include gmake/* legacy/*.fst legacy/*.fsti ; do
-    if [[ -e $f ]] ; then
+for f in *.fst *.fsti experimental/*.fst experimental/*.fsti .cache/*.checked ml/Makefile.realized ml/Makefile.include gmake/* legacy/*.fst legacy/*.fsti Makefile.extract.fsharp fs/* fs/VS/* ; do
+    if [[ -f $f ]] ; then
         "$INSTALL_EXEC" -m 644 -D -p $f $PREFIX/lib/fstar/$f
     fi
 done
