5d10f122fd34a153037e9447d98c16d947278c17
Tahina Ramananandro <taramana@microsoft.com>
2023-02-08 14:02:07 -0800

    minimal restore of nix flake

--- ulib/install-ulib.sh
+++ ulib/install-ulib.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 set -x
+
 for f in *.fst *.fsti experimental/*.fst experimental/*.fsti .cache/*.checked ml/Makefile.realized ml/Makefile.include gmake/* legacy/*.fst legacy/*.fsti Makefile.extract.fsharp fs/* fs/VS/* ; do
     if [[ -f $f ]] ; then
         "$INSTALL_EXEC" -m 644 -D -p $f $PREFIX/lib/fstar/$f
