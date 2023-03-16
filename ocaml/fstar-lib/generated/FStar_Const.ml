b24da2bf7ddb636c85e5fad19f54a2b43d7c667b
Aseem Rastogi <aseemr@microsoft.com>
2023-03-16 17:17:36 +0530

    snap

--- ocaml/fstar-lib/generated/FStar_Const.ml
+++ ocaml/fstar-lib/generated/FStar_Const.ml
@@ -101,6 +101,7 @@ let (eq_const : sconst -> sconst -> Prims.bool) =
            uu___ = uu___1) && (o1 = o2)
       | (Const_string (a, uu___), Const_string (b, uu___1)) -> a = b
       | (Const_reflect l1, Const_reflect l2) -> FStar_Ident.lid_equals l1 l2
+      | (Const_reify uu___, Const_reify uu___1) -> true
       | uu___ -> c1 = c2
 let rec (pow2 : FStar_BigInt.bigint -> FStar_BigInt.bigint) =
   fun x ->
