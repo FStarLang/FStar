b28c5d36a6fe71e767dcbe45a30b7120fdaa2b5c
Tahina Ramananandro <taramana@microsoft.com>
2023-04-08 21:28:01 -0700

    release.sh is only for subsequent artifacts, once the release is already created

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
 # This script is called by the release.yml GitHub Actions workflow
-# to create a release for Mac
+# to build and upload a release artifact for Mac or Windows.
+
+# This script MUST NOT be used to create a fresh new release from
+# scratch, but only to upload Mac or Windows binaries, ASSUMING that
+# a Linux release was already uploaded.
+# To create a new release, you should first start with Linux, by
+# building an image from .docker/release.Dockerfile
+
+# Ideally, instead of invoking these Dockerfiles and scripts by
+# yourself, you should be able to use the release.yaml GitHub Actions
+# workflow, which should create a new Linux release and upload Mac and
+# Windows binaries all at once.
 
 set -e
 set -x
@@ -12,8 +23,17 @@ if [[ -z "$FSTAR_HOST_HOME" ]] ; then
 fi
 cd "$FSTAR_HOST_HOME"
 
+# Strip the ~dev part of the version number
+dev='~dev'
+sed -i 's!'"$dev"'!!' version.txt
+
 # Build the package
 make -j6 package
+
+# Test the package if possible
+if [[ -z "$FSTAR_SKIP_PACKAGE_TEST" ]] ; then
+    .scripts/test_package.sh
+fi
 
 # Push the binary package to GitHub Releases
 .scripts/publish_release.sh
