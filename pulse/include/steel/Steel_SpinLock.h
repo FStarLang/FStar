44bf87df01f94524d5d8d12280ac4ef50387395e
Tahina Ramananandro <taramana@microsoft.com>
2023-05-01 15:19:26 -0700

    snap

--- pulse/include/steel/Steel_SpinLock.h
+++ pulse/include/steel/Steel_SpinLock.h
@@ -10,25 +10,25 @@
 #include <stdbool.h>
 #include "steel_types.h"
 
-extern bool Steel_SpinLock_uu___is_Lock(Steel_SpinLock_lock____ projectee);
+extern bool Steel_SpinLock_uu___is_Lock(Steel_SpinLock_lock____ *projectee);
 
-extern bool *Steel_SpinLock___proj__Lock__item__r(Steel_SpinLock_lock____ projectee);
+extern bool *Steel_SpinLock___proj__Lock__item__r(Steel_SpinLock_lock____ *projectee);
 
-extern void Steel_SpinLock___proj__Lock__item__i(Steel_SpinLock_lock____ projectee);
+extern void Steel_SpinLock___proj__Lock__item__i(Steel_SpinLock_lock____ *projectee);
 
-extern Steel_SpinLock_lock____ Steel_SpinLock_new_lock(void);
+extern void Steel_SpinLock_new_lock(Steel_SpinLock_lock____ *x0);
 
-extern void Steel_SpinLock_acquire(Steel_SpinLock_lock____ l);
+extern void Steel_SpinLock_acquire(Steel_SpinLock_lock____ *l);
 
-extern void Steel_SpinLock_release(Steel_SpinLock_lock____ l);
+extern void Steel_SpinLock_release(Steel_SpinLock_lock____ *l);
 
 typedef Steel_SpinLock_lock____ Steel_SpinLock_s_lock;
 
-extern Steel_SpinLock_lock____ Steel_SpinLock_new_s_lock(void);
+extern void Steel_SpinLock_new_s_lock(Steel_SpinLock_lock____ *x0);
 
-extern void Steel_SpinLock_s_acquire(Steel_SpinLock_lock____ l);
+extern void Steel_SpinLock_s_acquire(Steel_SpinLock_lock____ *l);
 
-extern void Steel_SpinLock_s_release(Steel_SpinLock_lock____ l);
+extern void Steel_SpinLock_s_release(Steel_SpinLock_lock____ *l);
 
 
 #define __Steel_SpinLock_H_DEFINED
