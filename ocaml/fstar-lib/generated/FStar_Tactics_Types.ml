549eac3742c0df96d326bd6143d969223616f59a
Aseem Rastogi <aseemr@microsoft.com>
2023-05-02 16:17:06 +0530

    snap

--- ocaml/fstar-lib/generated/FStar_Tactics_Types.ml
+++ ocaml/fstar-lib/generated/FStar_Tactics_Types.ml
@@ -477,3 +477,10 @@ let (uu___is_Both : unfold_side -> Prims.bool) =
   fun projectee -> match projectee with | Both -> true | uu___ -> false
 let (uu___is_Neither : unfold_side -> Prims.bool) =
   fun projectee -> match projectee with | Neither -> true | uu___ -> false
+type effect_label =
+  | E_Total 
+  | E_Ghost 
+let (uu___is_E_Total : effect_label -> Prims.bool) =
+  fun projectee -> match projectee with | E_Total -> true | uu___ -> false
+let (uu___is_E_Ghost : effect_label -> Prims.bool) =
+  fun projectee -> match projectee with | E_Ghost -> true | uu___ -> false
