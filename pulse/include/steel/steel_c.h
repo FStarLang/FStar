fc4a1dbdae6cae564b122c16b73a3da347ce8b64
Tahina Ramananandro <taramana@microsoft.com>
2023-07-13 08:33:56 -0700

    fix comment for void* support in steel_c.h

--- pulse/include/steel/steel_c.h
+++ pulse/include/steel/steel_c.h
@@ -1,8 +1,7 @@
 #ifndef __STEEL_C
 #define __STEEL_C
 
-// This file is a stopgap measure to avoid creating a Karamel branch.
-// Ideally, it should be empty, and there should be a Karamel AST entry for void*
+// SteelC support for void*
 
 typedef void* Steel_ST_C_Types_Base_void_ptr;
 typedef void* Steel_ST_C_Types_Array_array_void_ptr;
