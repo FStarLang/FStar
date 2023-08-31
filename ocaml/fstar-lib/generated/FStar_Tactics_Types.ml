1eb2b7ce1fd2fc2ebe4bff56182d9b8f1b8daa7c
Guido Martínez <mtzguido@gmail.com>
2023-08-31 10:49:52 -0700

    snap

--- ocaml/fstar-lib/generated/FStar_Tactics_Types.ml
+++ ocaml/fstar-lib/generated/FStar_Tactics_Types.ml
@@ -479,4 +479,3 @@ type ('g, 't0, 't1) subtyping_token = unit
 type ('g, 't0, 't1) equiv_token = unit
 type ('g, 'e, 'c) typing_token = unit
 type ('g, 'sc, 't, 'pats) match_complete_token = unit
-type issues = FStar_Issue.issue Prims.list
