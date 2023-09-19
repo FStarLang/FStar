c734e216b2276d8207dae47eaf34a1283f1d0424
Guido Martínez <mtzguido@gmail.com>
2023-09-18 18:08:55 -0700

    snap

--- pulse/src/ocaml/plugin/generated/Pulse_Typing_Combinators.ml
+++ pulse/src/ocaml/plugin/generated/Pulse_Typing_Combinators.ml
@@ -1205,4 +1205,5 @@ let rec (list_as_vprop :
   fun vps ->
     match vps with
     | [] -> Pulse_Syntax_Base.tm_emp
+    | hd::[] -> hd
     | hd::tl -> Pulse_Syntax_Base.tm_star hd (list_as_vprop tl)
