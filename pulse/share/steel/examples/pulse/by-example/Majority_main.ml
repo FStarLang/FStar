eee195d4fb20311628f38f3fdc9dfce9dd9e578c
Aseem Rastogi <aseemr@microsoft.com>
2024-01-11 15:38:51 +0000

    nits

--- pulse/share/steel/examples/pulse/by-example/Majority_main.ml
+++ pulse/share/steel/examples/pulse/by-example/Majority_main.ml
@@ -1,9 +1,9 @@
 let _ =
   let arr = Array.make 4 0 in
-  Array.set arr 0 0;
+  Array.set arr 0 1;
   Array.set arr 1 0;
   Array.set arr 2 0;
-  Array.set arr 3 1;
+  Array.set arr 3 0;
   let maj = PulseTutorial_Algorithms.majority () () arr (Stdint.Uint64.of_int 4) in
   match maj with
   | None -> print_string "No majority\n"
