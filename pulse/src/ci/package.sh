0cb91c794d62f03d7c8bb7e0c3364287717f72c7
Tahina Ramananandro <taramana@microsoft.com>
2024-01-12 10:46:49 -0800

    create a Steel binary package

---
+++ pulse/src/ci/package.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# build a F* package
+[[ -n "$FSTAR_HOME" ]]
+fstar_package_dir=$FSTAR_HOME/src/ocaml-output/fstar
+rm -rf "$fstar_package_dir"
+make -C "$FSTAR_HOME" package "$@"
+
+# assume current directory is $STEEL_HOME/src/ci
+export STEEL_HOME=$(cd `dirname $0`/../.. && pwd)
+
+# use the package to build Steel
+export FSTAR_HOME="$fstar_package_dir"
+export OTHERFLAGS='--admit_smt_queries true'
+make -C "$STEEL_HOME" "$@"
+
+# install Steel into the package directory
+export PREFIX="$FSTAR_HOME"
+make -C "$STEEL_HOME" install "$@"
+
+# create the archive package
+cd `dirname $0`
+[[ ! -e steel ]]
+mv "$PREFIX" steel
+rm -rf steel/share/fstar
+if [[ "$OS" = Windows_NT ]] ; then
+  zip -r -9 steel.zip steel
+else
+  tar czf steel.tgz steel
+fi
