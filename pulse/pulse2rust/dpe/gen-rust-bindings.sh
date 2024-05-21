88c562dd51c535319199d3414d37b9c0caafcba2
Tahina Ramananandro <taramana@microsoft.com>
2024-05-21 08:41:29 -0700

    why is the current directory not included?

--- pulse/pulse2rust/dpe/gen-rust-bindings.sh
+++ pulse/pulse2rust/dpe/gen-rust-bindings.sh
@@ -16,5 +16,5 @@ fi
 
 # generate EverCrypt*.rs and L0Core.rs
 DPE_OUTPUT_DIR=$PULSE_HOME/pulse2rust/dpe/src/generated
-bindgen -o $DPE_OUTPUT_DIR/evercrypt_gen.rs --allowlist-file '.*EverCrypt.*' _output/bindings.h --dynamic-loading "evercrypt" --dynamic-link-require-all -- -I _output
-bindgen -o $DPE_OUTPUT_DIR/l0core_gen.rs --allowlist-file '.*L0Core.*' _output/bindings.h --dynamic-loading "l0" --dynamic-link-require-all -- -I _output
+bindgen -o $DPE_OUTPUT_DIR/evercrypt_gen.rs --allowlist-file '.*EverCrypt.*' _output/bindings.h --dynamic-loading "evercrypt" --dynamic-link-require-all -- -I _output -I .
+bindgen -o $DPE_OUTPUT_DIR/l0core_gen.rs --allowlist-file '.*L0Core.*' _output/bindings.h --dynamic-loading "l0" --dynamic-link-require-all -- -I _output -I .
