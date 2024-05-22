d5e6848b370c2746ffe616ce40d0c1494eca3d92
Aseem Rastogi <aseemr@microsoft.com>
2024-05-22 04:21:12 +0000

    some comments etc.

--- pulse/pulse2rust/dpe/gen-rust-bindings-docker.sh
+++ pulse/pulse2rust/dpe/gen-rust-bindings-docker.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+#
+# Creates rust bindings for external dependencies of DPE
+#   using docker
+#
+
 set -e
 set -x
 
@@ -7,21 +13,20 @@ unset CDPATH
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 if  [[ -z "$PULSE_HOME" ]] ; then
-    # assume share/pulse/examples/dice/common/hacl-rust
     PULSE_HOME=$(realpath $(pwd)/../../)
 fi
 
-# regenerate EverCrypt*.h
-./gen-evercrypt-h.sh
+# regenerate EverCrypt*.h and L0Core.h
+./gen-external-h.sh
 
-# generate EverCrypt*.rs in a Docker image
+# generate evercrypt_gen.rs and l0core_gen in a Docker image
 DOCKER_IMAGE_NAME=haclrustbindingsimg"$$"
 echo $DOCKER_IMAGE_NAME
 docker build -t $DOCKER_IMAGE_NAME -f rust.Dockerfile .
 
 DPE_OUTPUT_DIR=$PULSE_HOME/pulse2rust/dpe/src/generated
 
-# copy everCrypt.rs from the Docker image
+# copy evercrypt_gen.rs and l0core_gen.rs from the Docker image
 DOCKER_CONTAINER_NAME=haclrustbindingscont"$$"
 docker create --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
 docker cp $DOCKER_CONTAINER_NAME:/usr/src/hacl/evercrypt_gen.rs $DPE_OUTPUT_DIR/
--- pulse/pulse2rust/dpe/gen-rust-bindings.sh
+++ pulse/pulse2rust/dpe/gen-rust-bindings.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+#
+# Generates rust bindings for external dependencies of DPE
+# It first invokes gen-external-h.sh to create .h files for
+#   the dependencies
+# And then uses the Rust bindgen tool to create their rust bindings
+#
+
 set -e
 set -x
 
@@ -7,14 +15,13 @@ unset CDPATH
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 if  [[ -z "$PULSE_HOME" ]] ; then
-    # assume share/pulse/examples/dice/common/hacl-rust
     PULSE_HOME=$(realpath $(pwd)/../../)
 fi
 
-# regenerate EverCrypt*.h
-./gen-evercrypt-h.sh
+# regenerate EverCrypt*.h and L0Core.h
+./gen-external-h.sh
 
-# generate EverCrypt*.rs and L0Core.rs
+# generate evercrypt_gen.rs and l0core_gen.rs
 DPE_OUTPUT_DIR=$PULSE_HOME/pulse2rust/dpe/src/generated
 bindgen -o $DPE_OUTPUT_DIR/evercrypt_gen.rs --allowlist-file '.*EverCrypt.*' _output/bindings.h --dynamic-loading "evercrypt" --dynamic-link-require-all -- -I _output -I .
 bindgen -o $DPE_OUTPUT_DIR/l0core_gen.rs --allowlist-file '.*L0Core.*' _output/bindings.h --dynamic-loading "l0" --dynamic-link-require-all -- -I _output -I .
