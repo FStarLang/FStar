9dadec3e879f5222d45232e588d7ea8875fb8153
Guido Martínez <mtzguido@gmail.com>
2024-08-22 17:11:28 -0700

    snap, mentions of ModifiesGen gone

--- ocaml/fstar-lib/generated/FStar_Modifies.ml
+++ ocaml/fstar-lib/generated/FStar_Modifies.ml
@@ -13,25 +13,12 @@ type ('s1, 's2) loc_aux_includes = Obj.t
 type ('l, 't, 'p) loc_aux_disjoint_buffer = Obj.t
 type ('l1, 'l2) loc_aux_disjoint = Obj.t
 type ('l, 'h1, 'h2) loc_aux_preserved = Obj.t
-let (cls : (unit, unit) aloc FStar_ModifiesGen.cls) =
-  FStar_ModifiesGen.Cls ((), (), (), (), (), (), (), (), (), ())
-type loc = ((unit, unit) aloc, unit) FStar_ModifiesGen.loc
-let (loc_none : loc) = FStar_ModifiesGen.loc_none cls
-type ('s1, 's2) loc_includes =
-  ((unit, unit) aloc, unit, unit, unit) FStar_ModifiesGen.loc_includes
-type ('s1, 's2) loc_disjoint =
-  ((unit, unit) aloc, unit, unit, unit) FStar_ModifiesGen.loc_disjoint
-type ('s, 'h1, 'h2) modifies =
-  ((unit, unit) aloc, unit, unit, unit, unit) FStar_ModifiesGen.modifies
-let (address_liveness_insensitive_locs : loc) =
-  FStar_ModifiesGen.address_liveness_insensitive_locs cls
-let (region_liveness_insensitive_locs : loc) =
-  FStar_ModifiesGen.region_liveness_insensitive_locs cls
-type ('h, 'ra) does_not_contain_addr =
-  (unit, unit) FStar_ModifiesGen.does_not_contain_addr
+type loc = unit
+
+type ('s1, 's2) loc_includes = unit
+type ('s1, 's2) loc_disjoint = unit
+type ('s, 'h1, 'h2) modifies = unit
+
+
+type ('h, 'ra) does_not_contain_addr = unit
 type ('uuuuu, 'uuuuu1) cloc_aloc = (unit, unit) aloc
-let (cloc_cls : (unit, unit) cloc_aloc FStar_ModifiesGen.cls) = cls
-let (cloc_of_loc :
-  loc -> ((unit, unit) cloc_aloc, unit) FStar_ModifiesGen.loc) = fun l -> l
-let (loc_of_cloc :
-  ((unit, unit) cloc_aloc, unit) FStar_ModifiesGen.loc -> loc) = fun l -> l
--- ocaml/fstar-lib/generated/FStar_ModifiesGen.ml
+++ ocaml/fstar-lib/generated/FStar_ModifiesGen.ml
@@ -1,20 +1,15 @@
 open Prims
 type aloc_t = unit
-type 'aloc cls =
-  | Cls of unit * unit * unit * unit * unit * unit * unit * unit * unit *
-  unit 
-let uu___is_Cls : 'aloc . 'aloc cls -> Prims.bool = fun projectee -> true
 type ('al, 'c) aloc =
   | ALoc of unit * Prims.nat * 'al FStar_Pervasives_Native.option 
-let uu___is_ALoc : 'al . 'al cls -> ('al, unit) aloc -> Prims.bool =
+let uu___is_ALoc : 'al . unit -> ('al, unit) aloc -> Prims.bool =
   fun c -> fun projectee -> true
 
-let __proj__ALoc__item__addr : 'al . 'al cls -> ('al, unit) aloc -> Prims.nat
-  =
+let __proj__ALoc__item__addr : 'al . unit -> ('al, unit) aloc -> Prims.nat =
   fun c ->
     fun projectee -> match projectee with | ALoc (region, addr, loc) -> addr
 let __proj__ALoc__item__loc :
-  'al . 'al cls -> ('al, unit) aloc -> 'al FStar_Pervasives_Native.option =
+  'al . unit -> ('al, unit) aloc -> 'al FStar_Pervasives_Native.option =
   fun c ->
     fun projectee -> match projectee with | ALoc (region, addr, loc) -> loc
 type ('a, 'b) i_restricted_g_t = unit
@@ -22,17 +17,9 @@ type 'regions addrs_dom = unit
 type ('regions, 'regionulivenessutags, 'r) non_live_addrs_codom = unit
 type ('regions, 'regionulivenessutags, 'nonuliveuaddrs,
   'r) live_addrs_codom = unit
-type ('al, 'c) loc' =
-  | Loc of unit * unit * unit * unit * unit 
-let uu___is_Loc : 'al . 'al cls -> ('al, unit) loc' -> Prims.bool =
-  fun c -> fun projectee -> true
+type ('aloc1, 'c) loc = unit
 
 
-type ('aloc1, 'c) loc = ('aloc1, unit) loc'
-
-
-let loc_none : 'a . 'a cls -> ('a, unit) loc =
-  fun c -> Loc ((), (), (), (), ())
 type ('t, 'tu, 'p, 'f1, 'f2) fun_set_equal = unit
 type ('al, 'c, 's1, 's2) loc_equal = Obj.t
 type ('al, 'c, 'b0, 'b) aloc_includes = unit
@@ -45,20 +32,13 @@ type ('al, 'c, 'l1, 'l2) loc_aux_disjoint = unit
 type ('al, 'c, 'l1, 'l2) loc_disjoint_region_liveness_tags = unit
 type ('al, 'c, 'l1, 'l2) loc_disjoint_addrs = unit
 type ('al, 'c, 'l1, 'l2) loc_disjoint_aux = unit
-type ('al, 'c, 'l1, 'l2) loc_disjoint' = unit
-type ('aloc1, 'c, 's1, 's2) loc_disjoint = unit
-let address_liveness_insensitive_locs : 'al . 'al cls -> ('al, unit) loc =
-  fun c -> Loc ((), (), (), (), ())
-let region_liveness_insensitive_locs : 'al . 'al cls -> ('al, unit) loc =
-  fun c -> Loc ((), (), (), (), ())
+type ('al, 'c, 'l1, 'l2) loc_disjoint = unit
 type ('al, 'c, 's, 'h1, 'h2) modifies_preserves_livenesses = unit
 type ('al, 'c, 's, 'h1, 'h2) modifies_preserves_mreferences = unit
 type ('al, 'c, 's, 'h1, 'h2) modifies_preserves_alocs = unit
 type ('al, 'c, 's, 'h1, 'h2) modifies_preserves_regions = unit
 type ('al, 'c, 's, 'h1, 'h2) modifies_preserves_not_unused_in = unit
-type ('al, 'c, 's, 'h1, 'h2) modifies' = unit
-type ('aloc1, 'c, 's, 'h1, 'h2) modifies = unit
-type ('h, 'ra) does_not_contain_addr' = unit
+type ('al, 'c, 's, 'h1, 'h2) modifies = unit
 type ('h, 'ra) does_not_contain_addr = unit
 type ('al, 'r, 'n) cls_union_aloc =
   | ALOC_FALSE of 'al 
@@ -104,17 +84,11 @@ type ('al, 'c, 'r, 'a, 'larger, 'smaller) cls_union_aloc_disjoint = unit
 type ('al, 'c, 'r, 'a, 'x, 'h, 'hu) cls_union_aloc_preserved = Obj.t
 type ('uuuuu, 'uuuuu1, 'uuuuu2) aloc_union =
   ('uuuuu, unit, unit) cls_union_aloc
-let cls_union :
-  'al . (Prims.bool -> 'al cls) -> ('al, unit, unit) aloc_union cls =
-  fun c -> Cls ((), (), (), (), (), (), (), (), (), ())
 
 
 type ('al, 'r, 'n) raise_aloc = 'al FStar_Universe.raise_t
-let raise_cls : 'al . 'al cls -> ('al, unit, unit) raise_aloc cls =
-  fun c -> Cls ((), (), (), (), (), (), (), (), (), ())
 let downgrade_aloc :
-  'al .
-    'al cls -> (('al, unit, unit) raise_aloc, unit) aloc -> ('al, unit) aloc
+  'al . unit -> (('al, unit, unit) raise_aloc, unit) aloc -> ('al, unit) aloc
   =
   fun c ->
     fun a ->
@@ -130,8 +104,7 @@ let downgrade_aloc :
                    (FStar_Universe.downgrade_val
                       (FStar_Pervasives_Native.__proj__Some__item__v x))))
 let upgrade_aloc :
-  'al .
-    'al cls -> ('al, unit) aloc -> (('al, unit, unit) raise_aloc, unit) aloc
+  'al . unit -> ('al, unit) aloc -> (('al, unit, unit) raise_aloc, unit) aloc
   =
   fun c ->
     fun a ->
@@ -146,23 +119,3 @@ let upgrade_aloc :
                  FStar_Pervasives_Native.Some
                    (FStar_Universe.raise_val
                       (FStar_Pervasives_Native.__proj__Some__item__v x))))
-let raise_loc :
-  'al .
-    'al cls -> ('al, unit) loc -> (('al, unit, unit) raise_aloc, unit) loc
-  =
-  fun c ->
-    fun l ->
-      let uu___ = l in
-      match uu___ with
-      | Loc (regions, region_liveness_tags, non_live_addrs, live_addrs, aux)
-          -> Loc ((), (), (), (), ())
-let lower_loc :
-  'al .
-    'al cls -> (('al, unit, unit) raise_aloc, unit) loc -> ('al, unit) loc
-  =
-  fun c ->
-    fun l ->
-      let uu___ = l in
-      match uu___ with
-      | Loc (regions, region_liveness_tags, non_live_addrs, live_addrs, aux)
-          -> Loc ((), (), (), (), ())
