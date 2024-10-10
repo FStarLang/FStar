af3cd9cd97db8fa4a4c1347e2a11b71f888c776a
Guido Martínez <mtzguido@gmail.com>
2024-10-10 08:44:50 -0700

    FStar_All.ml, FStar_Compiler_Effect.ml: Remove some footguns and unused code

--- ocaml/fstar-lib/FStar_All.ml
+++ ocaml/fstar-lib/FStar_All.ml
@@ -1,9 +1,7 @@
 exception Failure = Failure
 let failwith x = raise (Failure x)
 let exit i = exit (Z.to_int i)
-let op_Bar_Greater (x : 'a) (f : ('a -> 'b)) : 'b = f x
-let op_Less_Bar  (f : ('a -> 'b)) (x : 'a) : 'b = f x
-let pipe_right a f = f a
-let pipe_left f a = f a
-let try_with f1 f2 = try f1 () with | e -> f2 e
-
+(* Not used: handled specially by extraction. If used,
+   you will get all sorts of weird failures (e.g. an incomplete match
+   on f2!). *)
+(* let try_with f1 f2 = try f1 () with | e -> f2 e *)
