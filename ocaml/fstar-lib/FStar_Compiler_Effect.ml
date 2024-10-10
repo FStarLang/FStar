3fc28850bd5afe80d8417b243b88a44e2aa2d8de
Guido Martínez <mtzguido@gmail.com>
2024-10-10 11:23:45 -0700

    Remove more footguns in FStar_All and FStar_Compiler_Effect

--- ocaml/fstar-lib/FStar_Compiler_Effect.ml
+++ ocaml/fstar-lib/FStar_Compiler_Effect.ml
@@ -6,9 +6,9 @@ let op_Colon_Equals x y = x := y
 let alloc x = ref x
 let raise = raise
 let exit i = exit (Z.to_int i)
-exception Failure = Failure
-let failwith x = raise (Failure x)
+exception Failure = Failure (* NB: reusing OCaml's native Failure. *)
 (* Not used: handled specially by extraction. If used,
    you will get all sorts of weird failures (e.g. an incomplete match
    on f2!). *)
 (* let try_with f1 f2 = try f1 () with | e -> f2 e *)
+(* let failwith x = raise (Failure x) *)
