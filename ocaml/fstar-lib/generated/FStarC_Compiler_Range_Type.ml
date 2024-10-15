8900b78af5f11cb9ed0e65be55f5428155032803
Guido Martínez <mtzguido@gmail.com>
2024-10-15 11:39:28 -0700

    snap

--- ocaml/fstar-lib/generated/FStarC_Compiler_Range_Type.ml
+++ ocaml/fstar-lib/generated/FStarC_Compiler_Range_Type.ml
@@ -75,6 +75,30 @@ let (mk_rng : Prims.string -> pos -> pos -> rng) =
       fun end_pos -> { file_name = file_name1; start_pos; end_pos }
 let (mk_range : Prims.string -> pos -> pos -> range) =
   fun f -> fun b -> fun e -> let r = mk_rng f b e in range_of_rng r r
+let (string_of_file_name : Prims.string -> Prims.string) =
+  fun f ->
+    let uu___ =
+      let uu___1 = FStarC_Options_Ext.get "fstar:no_absolute_paths" in
+      uu___1 = "1" in
+    if uu___
+    then FStarC_Compiler_Util.basename f
+    else
+      (let uu___2 = FStarC_Options.ide () in
+       if uu___2
+       then
+         try
+           (fun uu___3 ->
+              match () with
+              | () ->
+                  let uu___4 =
+                    let uu___5 = FStarC_Compiler_Util.basename f in
+                    FStarC_Find.find_file uu___5 in
+                  (match uu___4 with
+                   | FStar_Pervasives_Native.None -> f
+                   | FStar_Pervasives_Native.Some absolute_path ->
+                       absolute_path)) ()
+         with | uu___3 -> f
+       else f)
 let (json_of_pos : pos -> FStarC_Json.json) =
   fun r ->
     FStarC_Json.JsonAssoc
@@ -85,13 +109,18 @@ let (json_of_rng : rng -> FStarC_Json.json) =
     let uu___ =
       let uu___1 =
         let uu___2 =
-          let uu___3 = json_of_pos r.start_pos in ("start_pos", uu___3) in
+          let uu___3 = string_of_file_name r.file_name in
+          FStarC_Json.JsonStr uu___3 in
+        ("file_name", uu___2) in
+      let uu___2 =
         let uu___3 =
-          let uu___4 =
-            let uu___5 = json_of_pos r.end_pos in ("end_pos", uu___5) in
-          [uu___4] in
-        uu___2 :: uu___3 in
-      ("file_name", (FStarC_Json.JsonStr (r.file_name))) :: uu___1 in
+          let uu___4 = json_of_pos r.start_pos in ("start_pos", uu___4) in
+        let uu___4 =
+          let uu___5 =
+            let uu___6 = json_of_pos r.end_pos in ("end_pos", uu___6) in
+          [uu___5] in
+        uu___3 :: uu___4 in
+      uu___1 :: uu___2 in
     FStarC_Json.JsonAssoc uu___
 let (json_of_range : range -> FStarC_Json.json) =
   fun r ->
