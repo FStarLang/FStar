50fcdf0f1aa60d97a2d8ec5bac6b330e211d2292
Guido Martínez <mtzguido@gmail.com>
2024-12-31 16:16:54 -0800

    Restore nix build plus CI (probably not ideally)

--- .nix/fstar.nix
+++ .nix/fstar.nix
@@ -1,31 +1,71 @@
-{ callPackage, fstar-dune, fstar-ulib, installShellFiles, lib, makeWrapper
-, stdenv, version, z3 }:
+{ callPackage, installShellFiles, lib, makeWrapper, buildDunePackage, version, z3, bash,
+    batteries,
+    menhir,
+    menhirLib,
+    pprint,
+    ppx_deriving,
+    ppx_deriving_yojson,
+    ppxlib,
+    process,
+    sedlex,
+    stdint,
+    yojson,
+    zarith,
+    memtrace,
+    mtime } :
 
-stdenv.mkDerivation {
+buildDunePackage {
   pname = "fstar";
   inherit version;
 
-  buildInputs = [ installShellFiles makeWrapper ];
+  duneVersion = "3";
 
-  src = lib.sourceByRegex ./.. [
-    ".common.mk"
-    "doc.*"
-    "examples.*"
-    "src(/ocaml-output(/Makefile)?)?"
-    "contrib.*"
-    "mk.*"
+  nativeBuildInputs = [ installShellFiles makeWrapper menhir ];
+
+  buildInputs = [
+    batteries
+    menhir
+    menhirLib
+    pprint
+    ppx_deriving
+    ppx_deriving_yojson
+    ppxlib
+    process
+    sedlex
+    stdint
+    yojson
+    zarith
+    memtrace
+    mtime
   ];
 
-  dontBuild = true;
+  enableParallelBuilding = true;
+
+  prePatch = ''
+    patchShebangs .scripts/*.sh
+    patchShebangs ulib/ml/app/ints/mk_int_file.sh
+
+    # The Makefile specifies bash, change it to Nix's bash.
+    sed -i 's,SHELL=.*,SHELL=${bash}/bin/bash,' Makefile
+  '';
+
+  src = lib.sourceByRegex ./.. [
+    "Makefile"
+    "src.*"
+    "mk.*"
+    "stage*.*"
+    "ulib.*"
+    "doc.*"
+    "version.txt"
+  ];
+
+  buildPhase = ''
+    export PATH="${z3}/bin:$PATH"
+    make -j$(nproc)
+  '';
 
   installPhase = ''
-    mkdir $out
-
-    CP="cp -r --no-preserve=mode"
-    $CP ${fstar-dune}/* $out
-    $CP ${fstar-ulib}/* $out
-
-    PREFIX=$out make -C src/ocaml-output install-sides
+    PREFIX=$out make install
 
     for binary in $out/bin/*
     do
