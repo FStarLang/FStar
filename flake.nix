e4cded6fefe9ddb150f873c646330e696de2789f
Guido Martínez <mtzguido@gmail.com>
2024-12-31 16:16:54 -0800

    Restore nix build plus CI (probably not ideally)

--- flake.nix
+++ flake.nix
@@ -13,15 +13,8 @@
         ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_14;
         z3 = pkgs.callPackage (import ./.nix/z3.nix) { };
         version = self.rev or "dirty";
-        fstar-dune = ocamlPackages.callPackage ./ocaml { inherit version; };
-        fstar-ulib = pkgs.callPackage ./nix/ulib.nix { inherit fstar-dune version z3; };
-        fstar = pkgs.callPackage ./.nix/fstar.nix {
-          inherit fstar-dune fstar-ulib version z3;
-        };
-        fstar-ocaml-snapshot =
-          pkgs.callPackage ./src { inherit fstar ocamlPackages version; };
-        fstar-bootstrap = pkgs.callPackage ./.nix/bootstrap.nix {
-          inherit fstar fstar-dune fstar-ocaml-snapshot fstar-ulib;
+        fstar = ocamlPackages.callPackage ./.nix/fstar.nix {
+          inherit version z3;
         };
         emacs = pkgs.writeScriptBin "emacs-fstar" ''
           #!${pkgs.stdenv.shell}
@@ -34,9 +27,7 @@
         '';
       in rec {
         packages = {
-          inherit z3 ocamlPackages;
-          inherit fstar fstar-dune fstar-ocaml-snapshot fstar-bootstrap;
-          inherit emacs;
+          inherit z3 fstar emacs;
           default = fstar;
         };
         apps.emacs = {
@@ -45,7 +36,7 @@
         };
         devShells.default = pkgs.mkShell {
           name = "${fstar.name}-dev";
-          inputsFrom = [ fstar fstar-dune ];
+          inputsFrom = [ fstar ];
           shellHook = ''
             export FSTAR_SOURCES_ROOT="$(pwd)"
             export PATH="$FSTAR_SOURCES_ROOT/bin/:$PATH"
