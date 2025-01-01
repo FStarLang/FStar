d0a8f12b21e7a93ea69985e400e7914ad04d929e
Guido Martínez <mtzguido@gmail.com>
2024-12-31 16:53:55 -0800

    stage.mk: use dune --release instead of custom flags
    This is simpler and more future- (and past-) proof.

--- mk/stage.mk
+++ mk/stage.mk
@@ -1,18 +1,6 @@
 FSTAR_ROOT ?= ..
 include $(FSTAR_ROOT)/mk/common.mk
 
-# We cannot simply use --release as that implies `--root .`, and we are explictly
-# changing it. Maybe we can just chdir instead?
-FSTAR_DUNE_RELEASE_OPTIONS :=
-FSTAR_DUNE_RELEASE_OPTIONS += --ignore-promoted-rules
-FSTAR_DUNE_RELEASE_OPTIONS += --no-config
-FSTAR_DUNE_RELEASE_OPTIONS += --profile release
-FSTAR_DUNE_RELEASE_OPTIONS += --always-show-command-line
-FSTAR_DUNE_RELEASE_OPTIONS += --promote-install-files
-FSTAR_DUNE_RELEASE_OPTIONS += --require-dune-project-file
-FSTAR_DUNE_RELEASE_OPTIONS += --ignore-lock-dir
-FSTAR_DUNE_RELEASE_OPTIONS += --default-target @install
-
 ifeq ($(V),)
 FSTAR_DUNE_OPTIONS += --no-print-directory
 FSTAR_DUNE_OPTIONS += --display=quiet
@@ -20,7 +8,7 @@ endif
 
 FSTAR_DUNE_BUILD_OPTIONS := $(FSTAR_DUNE_OPTIONS)
 ifeq ($(FSTAR_DUNE_RELEASE),1)
-FSTAR_DUNE_BUILD_OPTIONS += $(FSTAR_DUNE_RELEASE_OPTIONS)
+FSTAR_DUNE_BUILD_OPTIONS += --release
 endif
 
 .NOTPARALLEL:
@@ -30,16 +18,16 @@ endif
 _force:
 
 fstarc-bare: _force
-	dune build --root=dune $(FSTAR_DUNE_BUILD_OPTIONS) fstarc-bare
+	cd dune && dune build $(FSTAR_DUNE_BUILD_OPTIONS) fstarc-bare
 
 fstarc-full: _force
-	dune build --root=dune $(FSTAR_DUNE_BUILD_OPTIONS) fstarc-full
+	cd dune && dune build $(FSTAR_DUNE_BUILD_OPTIONS) fstarc-full
 
 libapp: _force
-	dune build --root=dune $(FSTAR_DUNE_BUILD_OPTIONS) libapp
+	cd dune && dune build $(FSTAR_DUNE_BUILD_OPTIONS) libapp
 
 libplugin: _force
-	dune build --root=dune $(FSTAR_DUNE_BUILD_OPTIONS) libplugin
+	cd dune && dune build $(FSTAR_DUNE_BUILD_OPTIONS) libplugin
 
 clean: _force
 	dune clean $(FSTAR_DUNE_OPTIONS) --root=dune
@@ -48,8 +36,8 @@ clean: _force
 CP=cp -p # preserve timestamps
 install: fstarc-bare fstarc-full libapp libplugin
 	# Seems to need one final build?
-	dune build $(FSTAR_DUNE_BUILD_OPTIONS) --root=dune
-	dune install $(FSTAR_DUNE_OPTIONS) --root=dune --prefix=$(CURDIR)/out
+	cd dune && dune build $(FSTAR_DUNE_BUILD_OPTIONS)
+	cd dune && dune install $(FSTAR_DUNE_OPTIONS) --prefix=$(abspath $(CURDIR)/out)
 	# Install library
 	$(CP) -t out/lib/fstar ulib/*.fst
 	$(CP) -t out/lib/fstar ulib/*.fsti
