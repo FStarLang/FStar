c1ec0e07ba363d8697e8a2d203bfb40a3929933a
Guido Martínez <mtzguido@gmail.com>
2025-01-02 09:58:05 -0800

    mk: tweak library install

--- mk/stage.mk
+++ mk/stage.mk
@@ -33,18 +33,15 @@ clean: _force
 	dune clean $(FSTAR_DUNE_OPTIONS) --root=dune
 	rm -rf out
 
-CP=cp -p # preserve timestamps
 install: fstarc-bare fstarc-full libapp libplugin
-	# Seems to need one final build?
+	@# Seems to need one final build?
 	cd dune && dune build $(FSTAR_DUNE_BUILD_OPTIONS)
 	cd dune && dune install $(FSTAR_DUNE_OPTIONS) --prefix=$(abspath $(CURDIR)/out)
-	# Install library
-	$(CP) -t out/lib/fstar ulib/*.fst
-	$(CP) -t out/lib/fstar ulib/*.fsti
-	$(CP) -t out/lib/fstar ulib/fstar.include
-	$(CP) -r -t out/lib/fstar ulib/experimental
-	$(CP) -r -t out/lib/fstar ulib/legacy
-	# Install checked files for the library
-	mkdir -p out/lib/fstar/.checked
-	$(CP) -r -t out/lib/fstar/.checked ulib.checked/*
-	# Should we make sure checked files have newer timestamp?
+	@# Install library
+	cp -H -p -r ulib out/lib/fstar/ulib
+	echo 'ulib' >> out/lib/fstar/fstar.include
+	rm -f out/lib/fstar/ulib/*.config.json
+	@# Install checked files for the library
+	mkdir -p out/lib/fstar/ulib/.checked
+	cp -p ulib.checked/* out/lib/fstar/ulib/.checked/
+	echo '.checked' >> out/lib/fstar/ulib/fstar.include
