d8d9b16c512f8003cbb904fe132cd7c06c0fefca
Guido Martínez <mtzguido@gmail.com>
2025-06-27 08:22:59 -0700

    Add expected code output for Null

--- pulse/lib/pulse/lib/ml/Pulse_Lib_Reference.ml
+++ pulse/lib/pulse/lib/ml/Pulse_Lib_Reference.ml
@@ -2,6 +2,11 @@ let op_Bang (r:'a ref) () () = !r
 let op_Colon_Equals (r:'a ref) (v:'a) () = r := v
 let alloc (v:'a) = ref v
 
-let __null = alloc 0
-let null : 'a ref = Obj.magic __null
-let is_null (r:'a ref) : bool = r == __null
+(* dummy null ref *)
+let __null : int ref = alloc 0
+
+let null : 'a. unit -> 'a ref =
+  fun () -> Obj.magic __null
+
+let is_null : 'a. 'a ref -> bool =
+  fun r -> r == Obj.magic __null
