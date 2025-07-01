9964371f08dfdd070a6451aeb301dfec124c820f
Théophile Wallez <theophile@wallez.net>
2025-07-01 11:39:46 +0200

    chore: update nixpkgs, revert sedlex override

--- .nix/z3_4_8_5.nix
+++ .nix/z3_4_8_5.nix
@@ -1,11 +1,36 @@
-{ fetchFromGitHub, z3 }:
+ { stdenv, lib
+, fetchzip
+, autoPatchelfHook
+}:
 
-z3.overrideAttrs (old: rec {
+stdenv.mkDerivation rec {
+  pname = "z3";
   version = "4.8.5";
-  src = fetchFromGitHub {
-    owner = "z3prover";
-    repo = "z3";
-    rev = "Z3-${version}";
-    sha256 = "ytG5O9HczbIVJAiIGZfUXC/MuYH7d7yLApaeTRlKXoc=";
+
+  src = fetchzip {
+    url = "https://github.com/Z3Prover/z3/releases/download/Z3-${version}/z3-${version}-x64-ubuntu-16.04.zip";
+    hash = "sha256-dgG4L77Y3+g10tO2pygmJ+XeGOhJrzuDxIzuZyJvMf0=";
   };
-})
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
+    stdenv.cc.cc.lib
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    cp -r bin $out
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "High-performance theorem prover and SMT solver";
+    mainProgram = "z3";
+    homepage = "https://github.com/Z3Prover/z3";
+    platforms = platforms.linux;
+  };
+}
