106b905dc6504238c3782583d67574138ea95189
Guido Martínez <mtzguido@gmail.com>
2025-08-07 00:17:58 -0700

    Update pulse2rust snapshots

--- pulse/pulse2rust/tests/src/pulsetutorial_algorithms.rs
+++ pulse/pulse2rust/tests/src/pulsetutorial_algorithms.rs
@@ -18,8 +18,8 @@ pub fn majority<A: Copy + PartialEq>(
     let votes_0 = votes[0];
     let mut cand = votes_0;
     while {
-        let vi = i;
-        vi < len
+        let __anf5610 = i;
+        __anf5610 < len
     } {
         let vi = i;
         let vk = k;
@@ -47,8 +47,8 @@ pub fn majority<A: Copy + PartialEq>(
         i = 0;
         k = 0;
         while {
-            let vi = i;
-            vi < len
+            let __anf37653 = i;
+            __anf37653 < len
         } {
             let vi = i;
             let vk1 = k;
