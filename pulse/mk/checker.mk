576cf7c64c3ed45f04c55cfa0a1367da78ee523a
Guido Martínez <mtzguido@gmail.com>
2025-08-12 19:35:42 -0700

    Use a custom tactic for finding non_informative proofs
    Calling tcresolve works, and allows us to use the typeclass system to
    extend the set of non-informative types, but comes at a cost. For once,
    tcresolve will try to apply local instances from the enviroment beofre
    attempting to apply global ones, which means traversing the entire
    environment and, for each binder, normalizing it and attempting to unify
    it with the goal.
    
    tcresolve should probably have smarter defaults, but, we also don't need
    to call it directly. This PR adds a custom tactic that has a fastpath
    for the common cases and only calls tcresolve if it could not solve the
    goal.

--- pulse/mk/checker.mk
+++ pulse/mk/checker.mk
@@ -4,7 +4,11 @@ CACHE_DIR := build/$(TAG).checked
 OUTPUT_DIR := build/$(TAG).ml
 CODEGEN := Plugin
 ROOTS := $(shell find $(SRC) -name '*.fst' -o -name '*.fsti')
+ROOTS += lib/common/Pulse.Lib.Tactics.fsti
+# ^ List files with plugins here
+
 FSTAR_OPTIONS += --already_cached 'Prims,FStar'
+FSTAR_OPTIONS += --include lib/common
 EXTRACT += --extract '-*,+Pulse,+PulseSyntaxExtension'
 DEPFLAGS += --already_cached 'Prims,FStar,FStarC'
 
