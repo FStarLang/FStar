a18ca5b0da64daa6b40951a28864ec14c1f58e45
Gabriel Ebner <gebner@gebner.org>
2026-02-04 15:32:01 -0800

    Add OCaml implementation for goto.

--- pulse/lib/pulse/lib/ml/Pulse_Lib_Dv.ml
+++ pulse/lib/pulse/lib/ml/Pulse_Lib_Dv.ml
@@ -5,3 +5,28 @@ let while_ cond body =
 
 val unreachable : unit -> 'a
 let unreachable () = unreachable ()
+
+(* TODO implement goto using OCaml 5 effects *)
+
+type goto_label_core = int
+type 'a goto_label = goto_label_core
+
+let new_goto_lbl : unit -> goto_label_core =
+  let ctr = ref 0 in
+  fun _ ->
+    let i = !ctr in
+    ctr := i + 1;
+    i
+
+exception Goto of goto_label_core * Obj.t
+
+let goto (lbl: 'a goto_label) (arg: 'a) : unit =
+  raise (Goto (lbl, arg))
+
+let forward_jump_label (body: 'a goto_label -> 'a) : 'a =
+  let lbl = new_goto_lbl () in
+  try
+    body lbl
+  with
+    | Goto (lbl', ret) when lbl' = lbl ->
+      ret
